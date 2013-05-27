d78b757315980d4c13cd13d29688101a7c48ef4d
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2013-05-27 04:03:13 +0000

    Added license headers and license text.
    git-svn-id: http://trac-hacks.org/svn@13245 7322e99d-02ea-0310-aa39-e9a107903beb

--- markdownmacro/trunk/setup.py
+++ markdownmacro/trunk/setup.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+#
+# Copyright (C) 2008 Douglas Clifton <dwclifton@gmail.com>
+# Copyright (C) 2012-2013 Ryan J Ollos <ryan.j.ollos@gmail.com>
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.
+#
 
 import os.path
 from setuptools import setup
@@ -11,11 +19,13 @@ setup(
 
     author = 'Douglas Clifton',
     author_email = 'dwclifton@gmail.com',
+    maintainer = 'Ryan J Ollos',
+    maintainer_email = 'ryan.j.ollos@gmail.com',
     description = 'Implements Markdown syntax WikiProcessor as a Trac macro.',
     long_description = open(os.path.join(os.path.dirname(__file__), 'README')).read(),
     keywords = '0.11 dwclifton processor macro wiki',
     url = 'http://trac-hacks.org/wiki/MarkdownMacro',
-    license = 'BSD',
+    license = 'BSD 3-Clause',
 
     entry_points = { 'trac.plugins': [ 'Markdown.macro = Markdown.macro' ] },
     classifiers = ['Framework :: Trac'],
