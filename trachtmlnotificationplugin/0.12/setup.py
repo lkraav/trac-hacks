39f2d013da641719c3026a07b2f08042c44b37ff
jun66j5 <jun66j5@7322e99d-02ea-0310-aa39-e9a107903beb>
2013-05-30 08:47:57 +0000

    trachtmlnotification: initial check-ins
    git-svn-id: http://trac-hacks.org/svn@13252 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ trachtmlnotificationplugin/0.12/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+extra = {}
+
+setup(
+    name = 'TracHtmlNotificationPlugin',
+    version = '0.12.0.1',
+    description = 'Send ticket notification with HTML part (t:#2625)',
+    license = 'BSD',  # the same as Trac
+    url = 'http://trac-hacks.org/wiki/TracHtmlNotificationPlugin',
+    author = 'Jun Omae',
+    author_email = 'jun66j5@gmail.com',
+    packages = find_packages(exclude=['*.tests*']),
+    package_data = {
+        'trachtmlnotification': ['templates/*.html'],
+    },
+    entry_points = {
+        'trac.plugins': [
+            'trachtmlnotification.notification = trachtmlnotification.notification',
+        ],
+    },
+    **extra)
