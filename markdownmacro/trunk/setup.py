5d02117824498834aa6f756ca6c23193d8e4e9ee
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2013-06-06 06:46:39 +0000

    PEP-0008 changes.
    git-svn-id: http://trac-hacks.org/svn@13265 7322e99d-02ea-0310-aa39-e9a107903beb

--- markdownmacro/trunk/setup.py
+++ markdownmacro/trunk/setup.py
@@ -13,21 +13,22 @@ import os.path
 from setuptools import setup
 
 setup(
-    name = 'TracMarkdownMacro',
-    packages = ['Markdown'],
-    version = '0.11.4',
+    name='TracMarkdownMacro',
+    packages=['Markdown'],
+    version='0.11.4',
 
-    author = 'Douglas Clifton',
-    author_email = 'dwclifton@gmail.com',
-    maintainer = 'Ryan J Ollos',
-    maintainer_email = 'ryan.j.ollos@gmail.com',
-    description = 'Implements Markdown syntax WikiProcessor as a Trac macro.',
-    long_description = open(os.path.join(os.path.dirname(__file__), 'README')).read(),
-    keywords = '0.11 dwclifton processor macro wiki',
-    url = 'http://trac-hacks.org/wiki/MarkdownMacro',
-    license = 'BSD 3-Clause',
+    author='Douglas Clifton',
+    author_email='dwclifton@gmail.com',
+    maintainer='Ryan J Ollos',
+    maintainer_email='ryan.j.ollos@gmail.com',
+    description='Implements Markdown syntax WikiProcessor as a Trac macro.',
+    long_description=open(
+        os.path.join(os.path.dirname(__file__), 'README')).read(),
+    keywords='0.11 dwclifton processor macro wiki',
+    url='http://trac-hacks.org/wiki/MarkdownMacro',
+    license='BSD 3-Clause',
 
-    entry_points = { 'trac.plugins': [ 'Markdown.macro = Markdown.macro' ] },
-    classifiers = ['Framework :: Trac'],
-    install_requires = ['Trac'],
+    entry_points={'trac.plugins': ['Markdown.macro = Markdown.macro']},
+    classifiers=['Framework :: Trac'],
+    install_requires=['Trac'],
 )
