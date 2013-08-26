1b434409776f0790c18851d9fb05e980b22613c0
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2013-08-26 12:22:32 +0000

    Moved license text to `COPYING` file. Fixes #11282.
    git-svn-id: http://trac-hacks.org/svn@13364 7322e99d-02ea-0310-aa39-e9a107903beb

--- condfieldsgenshiplugin/0.12/setup.py
+++ condfieldsgenshiplugin/0.12/setup.py
@@ -1,31 +1,12 @@
-# Copyright (c) 2008, Stephen Hansen
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 #
+# Copyright (c) 2008, Stephen Hansen <shansen@advpubtech.com>
+# Copyright (C) 2012-2013 Reinhard Wobst <rwobst@gmx.de>
 # All rights reserved.
 #
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-#
-#     * Redistributions of source code must retain the above copyright
-#       notice, this list of conditions and the following disclaimer.
-#     * Redistributions in binary form must reproduce the above copyright
-#       notice, this list of conditions and the following disclaimer in the
-#       documentation and/or other materials provided with the distribution.
-#     * Neither the name of the <ORGANIZATION> nor the names of its
-#       contributors may be used to endorse or promote products derived from
-#       this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
-# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-# ----------------------------------------------------------------------------
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.
 
 from setuptools import find_packages, setup
 
@@ -36,7 +17,7 @@ setup(
     author_email = 'rwobst@gmx.de',
     description = "CondFieldsGenshiPlugin for Trac >= 0.11 based on BlackMagicTicketTweaks",
     license = \
-    """Copyright (C) 2012, Reinhard Wobst. All rights reserved. Released under the 3-clause BSD license.""",
+    """Copyright (C) 2012-2013, Reinhard Wobst. All rights reserved. Released under the 3-clause BSD license.""",
     packages = find_packages(),
     package_data = {'condfieldsgenshi' : []},
     install_requires = ['trac>=0.11'],
