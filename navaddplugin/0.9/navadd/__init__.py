59476c70c3fcf9862269c97bcc4d8d3d810a8608
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2013-09-10 08:45:47 +0000

    Fixes #11248:  * File contained fixed tabs and spaces.  * Added license headers to file and 3-Clause BSD text.  * Changed entry point so that import in `__init__` isn't necessary.
    git-svn-id: http://trac-hacks.org/svn@13382 7322e99d-02ea-0310-aa39-e9a107903beb

--- navaddplugin/0.9/navadd/__init__.py
+++ navaddplugin/0.9/navadd/__init__.py
@@ -1,1 +1,1 @@
-from navadd import *
+
--- navaddplugin/0.9/setup.py
+++ navaddplugin/0.9/setup.py
@@ -1,13 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2006 Michael Renzmann <mrenzmann@otaku42.de>
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.
+
 from setuptools import setup
 
 setup(name='NavAdd',
       version='0.1',
       packages=['navadd'],
-      entry_points = {'trac.plugins': ['navadd = navadd']},
-      package_data={'navadd' : ['templates/*.cs', 'htdocs/js/*.js', 'htdocs/css/*.css']},
-      author = 'Michael Renzmann',
-      author_email = 'mrenzmann@otaku42.de',
-      description = 'A plugin for adding navigation items into one of the navigation bars.',
-      license = 'BSD',
-      keywords = 'trac navigation main meta',
-      url = 'http://trac-hacks.org/wiki/NavAddPlugin')
+      author='Michael Renzmann',
+      author_email='mrenzmann@otaku42.de',
+      license='3-Clause BSD',
+      url='http://trac-hacks.org/wiki/NavAddPlugin',
+      description='A plugin for adding navigation items into one of the navigation bars.',
+      keywords='trac navigation main meta',
+      entry_points={'trac.plugins': ['navadd.navadd = navadd.navadd']}
+)
