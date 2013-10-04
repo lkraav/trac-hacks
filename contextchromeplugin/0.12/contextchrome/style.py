2552319010b5ef29dbe0a8413b9c7f3325feb285
matobaa <matobaa@7322e99d-02ea-0310-aa39-e9a107903beb>
2013-10-04 23:15:36 +0000

    ContextChromePlugin.!TypeClassToTicket: Support for TagsPlugin.
    git-svn-id: http://trac-hacks.org/svn@13405 7322e99d-02ea-0310-aa39-e9a107903beb

--- contextchromeplugin/0.12/contextchrome/style.py
+++ contextchromeplugin/0.12/contextchrome/style.py
@@ -15,20 +15,32 @@ class TypeClassToTicket(Component):
     """ set css-class to type on ticket. """
     implements(ITemplateStreamFilter)
 
-    def filter_stream(self, req, method, filename, stream, data):
-        if filename != 'ticket.html':
-            return stream
-        if not 'ticket' in data:
-            return stream
-        ticket = data['ticket'].values
-        fields = self.config.getlist('ticket', 'decorate_fields')
-        value = ' '.join(['%s_is_%s' % (field, value.rstrip(' ').rstrip(',').replace('"', ''))
-                          for field in fields for value in ticket.get(field).split(' ') if field in ticket]
-                         + [ticket.get('type')]  # backward compatibility
-                         )
+    def get_resource_tags(self, req, resource):
+        try:
+            from tractags.wiki import WikiTagProvider
+            tag_provider = self.compmgr[WikiTagProvider]
+            return tag_provider.get_resource_tags(req, resource)
+        except:
+            return []
 
-        def add(name, event):
-            attrs = event[1][1]
-            values = attrs.get(name)
-            return values and ' '.join((values, value)) or value
-        return stream | Transformer('//body').attr('class', add)
+    def filter_stream(self, req, method, filename, stream, data):
+        value = None
+        if filename == 'ticket.html':
+            if not 'ticket' in data:
+                return stream
+            ticket = data['ticket'].values
+            fields = self.config.getlist('ticket', 'decorate_fields')
+            value = ' '.join(['%s_is_%s' % (field, value.rstrip(' ').rstrip(',').replace('"', ''))
+                              for field in fields for value in ticket.get(field).split(' ') if field in ticket]
+                             + [ticket.get('type')]  # backward compatibility
+                             )
+        if filename == 'wiki_view.html':
+            value = ' '.join(['tagged_as_%s' % tag for tag in self.get_resource_tags(req, data['context'].resource)])
+
+        if value:
+            def add(name, event):
+                attrs = event[1][1]
+                values = attrs.get(name)
+                return values and ' '.join((values, value)) or value
+            return stream | Transformer('//body').attr('class', add)
+        return stream
