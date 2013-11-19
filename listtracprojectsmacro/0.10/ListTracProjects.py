4b82d7e93b807a64ddaed27775fa9c92e0a3533f
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2013-11-19 06:24:31 +0000

    Replaced tabs with spaces.
    git-svn-id: http://trac-hacks.org/svn@13439 7322e99d-02ea-0310-aa39-e9a107903beb

--- listtracprojectsmacro/0.10/ListTracProjects.py
+++ listtracprojectsmacro/0.10/ListTracProjects.py
@@ -13,13 +13,13 @@
 
 
 """
-	List All Trac Projects in a scalable format.
-	
-	Administrators _must_ set the ENV_PARENT_DIR variable
-	in order for the macro to work properly. 
+    List All Trac Projects in a scalable format.
 
-	Sample call:
-	[[ListTracProjects()]]
+    Administrators _must_ set the ENV_PARENT_DIR variable
+    in order for the macro to work properly.
+
+    Sample call:
+    [[ListTracProjects()]]
 """
 
 import os
@@ -30,45 +30,45 @@ from trac.web.href import Href
 from trac.util.text import to_unicode
 
 def execute(hdf, txt, env):
-	### ATTENTION!! SET THIS VARIBALE
-	ENV_PARENT_DIR = '/usr/local/www/trac'
-	##
+    ### ATTENTION!! SET THIS VARIBALE
+    ENV_PARENT_DIR = '/usr/local/www/trac'
+    ##
 
-	# Create the index
-	# The index will only display links to the first letters
-	# of existing projects. e.g. if Apple, Charlie,
-	# and Zoo are the only projects under ENV_PARENT_DIR,
-	# the index will display "A : C : Z" for the index
-	str = '<div id="index">'
+    # Create the index
+    # The index will only display links to the first letters
+    # of existing projects. e.g. if Apple, Charlie,
+    # and Zoo are the only projects under ENV_PARENT_DIR,
+    # the index will display "A : C : Z" for the index
+    str = '<div id="index">'
 
-	proj_groups = get_projects(ENV_PARENT_DIR)
-	sorted_keys = proj_groups.keys()
-	sorted_keys.sort()
+    proj_groups = get_projects(ENV_PARENT_DIR)
+    sorted_keys = proj_groups.keys()
+    sorted_keys.sort()
 
-	for ltr in sorted_keys:
-		# This has to be done first so that it is at the top of the page
-		# optional: output number of projects in each category
-		# output sorted html index 
-		str += '<a href="#' + string.upper(ltr) + '">' + string.upper(ltr) + '</a> : '
-	str = str[:-3]		# remove unsightly trailing colon
-	str += '</div>'
-	
-	# Create alphabetized tables of projects
-	for grp in sorted_keys:
-		# Output link back to index
-		bigLtr = string.upper(grp)
-		str += '<a href="#index"><h2 id="' + bigLtr + '">' \
-			+ bigLtr + '</h2></a>'
-		# Output number of projects
-		str += '<table class="wiki">'
-		proj_groups[bigLtr].sort(lambda x, y: cmp(x['name'].lower(), y['name'].lower()))
-		for p in proj_groups[bigLtr]:
-			str += '<tr><td><a href="' + p['href'] + \
-				'" target="_new">' + p['name'] + \
-				'</a></td><td>' + p['description'] + '</td></tr>'
-		str += '</table>'
+    for ltr in sorted_keys:
+        # This has to be done first so that it is at the top of the page
+        # optional: output number of projects in each category
+        # output sorted html index
+        str += '<a href="#' + string.upper(ltr) + '">' + string.upper(ltr) + '</a> : '
+    str = str[:-3]		# remove unsightly trailing colon
+    str += '</div>'
 
-	return str
+    # Create alphabetized tables of projects
+    for grp in sorted_keys:
+        # Output link back to index
+        bigLtr = string.upper(grp)
+        str += '<a href="#index"><h2 id="' + bigLtr + '">' \
+            + bigLtr + '</h2></a>'
+        # Output number of projects
+        str += '<table class="wiki">'
+        proj_groups[bigLtr].sort(lambda x, y: cmp(x['name'].lower(), y['name'].lower()))
+        for p in proj_groups[bigLtr]:
+            str += '<tr><td><a href="' + p['href'] + \
+                '" target="_new">' + p['name'] + \
+                '</a></td><td>' + p['description'] + '</td></tr>'
+        str += '</table>'
+
+    return str
 
 # The code below is based on portions from trac.web.main.py
 # Authors:
@@ -78,30 +78,30 @@ def execute(hdf, txt, env):
 # It has been slightly modified by the current author
 # mostly to remove dependencies on the web frontend.
 def get_projects(ENV_PARENT_DIR):
-	href = Href('')
-	project_groups = {}
-	#projects = []
-	for env_name, env_path in get_environments(ENV_PARENT_DIR).items():
-	    try:
-		#env = _open_environment(env_path,
-		#                        run_once=environ['wsgi.run_once'])
-		env = open_environment(env_path)
-		proj = {
-		'name': env.project_name,
-		'description': env.project_description,
-		'href': href(env_name)
-		#'href': env_name
-		}
-	    except Exception, e:
-		proj = {'name': env_name, 'description': to_unicode(e)}
-	    # Add to the specified group based on first letter
-	    # ToDo: Does this work for numbers and symbols?
-	    if project_groups.has_key(string.upper(proj['name'][0])):
-		project_groups[string.upper(proj['name'][0])].append(proj)
-	    else:
-		# Initialize the new list
-		project_groups[string.upper(proj['name'][0])] = [proj]
-	return project_groups
+    href = Href('')
+    project_groups = {}
+    #projects = []
+    for env_name, env_path in get_environments(ENV_PARENT_DIR).items():
+        try:
+        #env = _open_environment(env_path,
+        #                        run_once=environ['wsgi.run_once'])
+        env = open_environment(env_path)
+        proj = {
+        'name': env.project_name,
+        'description': env.project_description,
+        'href': href(env_name)
+        #'href': env_name
+        }
+        except Exception, e:
+        proj = {'name': env_name, 'description': to_unicode(e)}
+        # Add to the specified group based on first letter
+        # ToDo: Does this work for numbers and symbols?
+        if project_groups.has_key(string.upper(proj['name'][0])):
+        project_groups[string.upper(proj['name'][0])].append(proj)
+        else:
+        # Initialize the new list
+        project_groups[string.upper(proj['name'][0])] = [proj]
+    return project_groups
 
 def get_environments(ENV_PARENT_DIR, warn=False):
     """Retrieve canonical environment name to path mapping.
