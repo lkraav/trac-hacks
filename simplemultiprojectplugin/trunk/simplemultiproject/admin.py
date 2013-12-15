e282d30fc9d30b94489e587f17e124328b139162
falkb <falkb@7322e99d-02ea-0310-aa39-e9a107903beb>
2013-12-15 00:11:46 +0000

    started 2 new features:  - close of projects (see #11377)  - user restriction for projects (see #11367)
    implemented
     * admin GUI
     * db extension
     * upgrade from plugin version 4 to 5 (two new fields 'restrict' (integer) and 'closed' (text) to table 'smp_project')
    
    git-svn-id: http://trac-hacks.org/svn@13474 7322e99d-02ea-0310-aa39-e9a107903beb

--- simplemultiprojectplugin/trunk/simplemultiproject/admin.py
+++ simplemultiprojectplugin/trunk/simplemultiproject/admin.py
@@ -12,6 +12,7 @@ from trac.util.translation import _
 from trac.perm import IPermissionRequestor
 from trac.admin.api import IAdminPanelProvider
 from trac.web.chrome import ITemplateProvider, add_notice
+from trac.util.datefmt import get_datetime_format_hint, from_utimestamp, to_utimestamp, user_time, parse_date
 
 # Model Class
 from simplemultiproject.model import *
@@ -35,23 +36,23 @@ class SmpAdminPanel(Component):
     def __init__(self):
         self.__SmpModel = SmpModel(self.env)
 
-    def add_project(self, name, summary, description):
+    def add_project(self, name, summary, description, closed, restrict):
         try:
             self.log.info("Simple Multi Project: Adding project %s" % (name))
-            self.__SmpModel.insert_project(name, summary, description)
+            self.__SmpModel.insert_project(name, summary, description, closed, restrict)
             return True
 
         except Exception, e:
             self.log.error("Add Project Error: %s" % (e, ))
             return False
 
-    def update_project(self, id, name, summary, description):
+    def update_project(self, id, name, summary, description, closed, restrict):
         try:
             # we have to rename the project in tickets custom field
             old_project_name = self.__SmpModel.get_project_name(id)
 
             self.log.info("Simple Multi Project: Modify project %s" % (name))
-            self.__SmpModel.update_project(id, name, summary, description)
+            self.__SmpModel.update_project(id, name, summary, description, closed, restrict)
 
             if old_project_name and old_project_name != name:
                 self.__SmpModel.update_custom_ticket_field(old_project_name, name)
@@ -69,16 +70,28 @@ class SmpAdminPanel(Component):
         projects_rows  = self.__SmpModel.get_all_projects()
         projects = []
         for row in sorted(projects_rows, key=itemgetter(1)):
-            projects.append({'id': row[0], 'name':row[1], 'summary': row[2], 'description': row[3]})
+            time_str = None
+            timestamp = row[4]
+            if (timestamp > 0):
+                time_str = from_utimestamp(timestamp)
+            projects.append({'id': row[0], 'name':row[1], 'summary': row[2], 'description': row[3], 'closed': time_str, 'restrict': row[5]})
 
         if path_info:
             if req.method == 'POST':
                 if req.args.get('modify'):
                     req.perm.require('PROJECT_ADMIN')
-                    if not self.update_project(req.args.get('id'), req.args.get('name'), req.args.get('summary'), req.args.get('description')):
+
+                    time = req.args.get('time', '')
+                    if time:
+                        time = user_time(req, parse_date, time, hint='datetime')
+                    else:
+                        time = None
+                    time = to_utimestamp(time)
+
+                    if not self.update_project(req.args.get('id'), req.args.get('name'), req.args.get('summary'), req.args.get('description'), time, req.args.get('restrict')):
                         self.log.error("SimpleMultiProject Error: Failed to added project '%s'" % (req.args.get('name'),))
                     else:
-                        add_notice(req, "'The project '%s' has been modify." % req.args.get('name'))
+                        add_notice(req, "'The project '%s' has been modified." % req.args.get('name'))
                         req.redirect(req.href.admin(category, page))
                 elif req.args.get('cancel'):
                     req.redirect(req.href.admin(category, page))
@@ -87,13 +100,20 @@ class SmpAdminPanel(Component):
             else:
                 for project in projects:
                     if project['id'] == int(path_info):
-                        data = {'view': 'detail', 'project': project}
+                        data = {'view': 'detail', 'project': project, 'datetime_hint': get_datetime_format_hint(), } # vars given to adminpanel.html
         else:
             if req.method == 'POST':
                 if req.args.get('add'):
                     req.perm.require('PROJECT_ADMIN')
                     if req.args.get('name') != '':
-                        if not self.add_project(req.args.get('name'), req.args.get('summary'), req.args.get('description')):
+                        time = req.args.get('time', '')
+                        if time:
+                            time = user_time(req, parse_date, time, hint='datetime')
+                        else:
+                            time = None
+                        time = to_utimestamp(time)
+
+                        if not self.add_project(req.args.get('name'), req.args.get('summary'), req.args.get('description'), time, req.args.get('restrict')):
                             self.log.error("SimpleMultiProject Error: Failed to added project '%s'" % (req.args.get('name'),))
                         else:
                             add_notice(req, "'The project '%s' has been added." % req.args.get('name'))
@@ -117,7 +137,7 @@ class SmpAdminPanel(Component):
                 else:
                     pass
             else:
-                data = {'view':'init', 'projects':projects, }
+                data = {'view':'init', 'projects':projects, 'datetime_hint': get_datetime_format_hint(), }
 
         return 'simplemultiproject_adminpanel.html', data
 
