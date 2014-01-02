34a6570efdca76d95215a85e03c6e281d345c41d
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2014-01-02 16:55:01 +0000

    1.0.1: Replace virtual script with a script and use `add_script_data` to pass data to the script. Refs #9764.
    git-svn-id: http://trac-hacks.org/svn@13537 7322e99d-02ea-0310-aa39-e9a107903beb

--- quietplugin/0.12/setup.py
+++ quietplugin/0.12/setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup, find_packages
 
 PACKAGE = 'TracQuiet'
-VERSION = '1.0.0'
+VERSION = '1.0.1'
 
 setup(
     name=PACKAGE, version=VERSION,
@@ -12,7 +12,6 @@ setup(
     author="Rob Guttman", author_email="guttman@alum.mit.edu",
     license='GPL', url='http://trac-hacks.org/wiki/TracQuietPlugin',
     packages = ['quiet'],
-    package_data = {'quiet':['templates/*.html','htdocs/*.js',
-                             'htdocs/*.css','htdocs/*.png']},
+    package_data = {'quiet':['htdocs/*.js','htdocs/*.css','htdocs/*.png']},
     entry_points = {'trac.plugins':['quiet.web_ui = quiet.web_ui']}
 )
