fefe437408e76c7cd91653e8c36eba4ae284062e
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2014-01-02 16:55:17 +0000

    1.0.1: Added license headers to files. The license text still needs to be added to a `COPYING` file.
    git-svn-id: http://trac-hacks.org/svn@13539 7322e99d-02ea-0310-aa39-e9a107903beb

--- quietplugin/0.12/setup.py
+++ quietplugin/0.12/setup.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+#
+# Copyright (C) 2011-2013 Rob Guttman <guttman@alum.mit.edu>
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.
 
 from setuptools import setup, find_packages
 
