aa834d7627818180eefb216f57a5f60f32bc9eda
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2014-01-09 22:56:14 +0000

    Added BSD 3-Clause license. Fixes #10502.
    git-svn-id: http://trac-hacks.org/svn@13563 7322e99d-02ea-0310-aa39-e9a107903beb

--- wantedpagesplugin/trunk/setup.py
+++ wantedpagesplugin/trunk/setup.py
@@ -1,3 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2006-2009 Justin Francis <jfrancis@justinfrancis.org>
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.
+
 from setuptools import setup
 
-setup(name="WantedPages", version="0.3",  packages=['wanted_pages'])
+setup(
+    name="WantedPages", 
+    version="0.3",
+    license="BSD 3-Clause",
+    packages=['wanted_pages']
+)
