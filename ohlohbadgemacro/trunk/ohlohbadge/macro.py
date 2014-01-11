c00cefea3995359ba74c5c93d1167f1f5fa81e14
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2014-01-11 19:39:10 +0000

    PEP-0008 changes and added license text.
    git-svn-id: http://trac-hacks.org/svn@13568 7322e99d-02ea-0310-aa39-e9a107903beb

--- ohlohbadgemacro/trunk/ohlohbadge/macro.py
+++ ohlohbadgemacro/trunk/ohlohbadge/macro.py
@@ -1,19 +1,26 @@
-# Created by Noah Kantrowitz on 2007-07-15.
-# Copyright (c) 2007 Noah Kantrowitz. All rights reserved.
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2007 Noah Kantrowitz <noah+pypi@coderanger.net>
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.
 
 from genshi.builder import tag 
 from trac.wiki.macros import WikiMacroBase
 from trac.wiki.formatter import system_message
 
+
 class OhlohBadgeMacro(WikiMacroBase):
-    """A small maco for showing Ohloh (http://ohloh.net) statistics badges."""
+    """A small macro for displaying Ohloh (http://ohloh.net)
+    statistics badges."""
     
     SCRIPT_LOCATION = 'http://www.ohloh.net/p/%s/widgets/project_thin_badge'
     
-    def expand_macro(self, formatter, name, content):
+    def expand_macro(self, formatter, name, content, args=None):
         content = content.strip()
         if not content.isdigit():
-            return system_message('Invalid Ohloh project ID', '%s is not a number'%content)
-        return tag.script('', src=self.SCRIPT_LOCATION%content, type='text/javascript')
-
-
+            return system_message('Invalid Ohloh project ID',
+                                  '%s is not a number' % content)
+        return tag.script('', src=self.SCRIPT_LOCATION % content,
+                          type='text/javascript')
--- ohlohbadgemacro/trunk/setup.py
+++ ohlohbadgemacro/trunk/setup.py
@@ -1,31 +1,33 @@
 #!/usr/bin/env python
-# -*- coding: iso-8859-1 -*-
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2007 Noah Kantrowitz <noah+pypi@coderanger.net>
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.
 
 from setuptools import setup
 
 setup(
-    name = 'TracOhlohBadge',
-    version = '1.0',
-    packages = ['ohlohbadge'],
-    #package_data = { 'ohlohbadge': ['templates/*.cs', 'htdocs/*.js', 'htdocs/*.css' ] },
-
-    author = 'Noah Kantrowitz',
-    author_email = 'noah+pypi@coderanger.net',
-    description = 'A Trac wiki macro to display Ohlo project badges.',
-    license = 'BSD',
-    keywords = 'trac plugin macro ohloh badge',
-    url = 'http://trac-hacks.org/wiki/OhlohBadgeMacro',
-    classifiers = [
+    name='TracOhlohBadge',
+    version='1.0',
+    packages=['ohlohbadge'],
+    author='Noah Kantrowitz',
+    author_email='noah+pypi@coderanger.net',
+    maintainer='Ryan J Ollos',
+    maintainer_email='ryan.j.ollos@gmail.com',
+    description='A Trac wiki macro to display Ohloh project badges.',
+    license='BSD 3-Clause',
+    keywords='trac plugin macro ohloh badge',
+    url='http://trac-hacks.org/wiki/OhlohBadgeMacro',
+    classifiers=[
         'Framework :: Trac',
     ],
-
-    install_requires = [],
-
-    entry_points = {
+    install_requires=[],
+    entry_points={
         'trac.plugins': [
             'ohlohbadge.macro = ohlohbadge.macro',
         ]
     },
 )
-
-
