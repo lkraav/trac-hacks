afa4734fb2223543b9abdc830f17dd3f75213c23
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2014-01-20 01:31:56 +0000

    0.3dev: PEP-0008 and other style changes, removed unused imports and fixed some misuses of the API.
    git-svn-id: http://trac-hacks.org/svn@13583 7322e99d-02ea-0310-aa39-e9a107903beb

--- ticketteamdispatcherplugin/0.11/ttd/admin.py
+++ ticketteamdispatcherplugin/0.11/ttd/admin.py
@@ -6,68 +6,53 @@
 # This software is licensed as described in the file COPYING, which
 # you should have received as part of this distribution.
 
-from trac.core import *
-from trac.web.chrome import ITemplateProvider
-from pkg_resources import resource_filename
-
-from tracusermanager.api import UserManager, User
 from trac.admin import IAdminPanelProvider
+from trac.core import Component, implements
+from trac.web.chrome import ITemplateProvider
+
+from tracusermanager.api import UserManager
+
 
 class TicketTeamDispatcherAdmin(Component):
     """
         Provides functions related to registration
     """
-
     implements(ITemplateProvider, IAdminPanelProvider)
 
-    # IAdminPageProvider methods
+    # IAdminPanelProvider methods
     def get_admin_panels(self, req):
         if req.perm.has_permission('TICKET_ADMIN'):
             yield ('ticket', 'Ticket System', 'ttd', 'Ticket Team Dispatcher')
-            
-    def getCaption(self):
-        return self.config.get('ticket-custom', 'ttd.label')
-        
-    def setCaption(self, caption):
-        self.config.set('ticket-custom', 'ttd.label', caption)
-        self.config.save()
-                
-    def getTeams(self):
-        return self.config.get('ticket-custom', 'ttd.options').split('|')   
-        
-    def setTeams(self, teams):
-        self.config.set('ticket-custom', 'ttd.options', '|'.join(teams))     
-        self.config.save()
         
     def render_admin_panel(self, req, category, page, path_info):
-        assert req.perm.has_permission('TICKET_ADMIN')
+        req.perm.require('TICKET_ADMIN')
 
         action = req.args.get('action')
         
         users = UserManager(self.env).get_active_users()
-        caption = self.getCaption()
-        teams = self.getTeams()
+        caption = self.get_caption()
+        teams = self.get_teams()
                 
         if action:
             # load data from post
             if action == 'rename':
                 caption = req.args.get('caption')
-                self.setCaption(caption)
+                self.set_caption(caption)
             elif action == 'add':
-                newTeam = req.args.get('newTeam')
+                new_team = req.args.get('newTeam')
                 
-                canAdd = True
+                can_add = True
                 for team in teams:
-                    if team == newTeam:
-                        canAdd = False
+                    if team == new_team:
+                        can_add = False
                         break
                 
-                if canAdd:
-                    teams.append(newTeam)
+                if can_add:
+                    teams.append(new_team)
                     for user in users:
-                        user[newTeam] = '0'
+                        user[new_team] = '0'
                         user.save()
-                    self.setTeams(teams)                
+                    self.set_teams(teams)
             elif action == 'up':
                 id = req.args.get('id')
                 i = teams.index(id)
@@ -75,7 +60,7 @@ class TicketTeamDispatcherAdmin(Component):
                     tmp = teams[i-1]
                     teams[i-1] = teams[i]
                     teams[i] = tmp
-                    self.setTeams(teams)                
+                    self.set_teams(teams)
             elif action == 'down':
                 id = req.args.get('id')
                 i = teams.index(id)
@@ -83,14 +68,14 @@ class TicketTeamDispatcherAdmin(Component):
                     tmp = teams[i+1]
                     teams[i+1] = teams[i]
                     teams[i] = tmp
-                    self.setTeams(teams)        
+                    self.set_teams(teams)
             elif action == 'delete':
                 id = req.args.get('id')
                 teams.remove(id)
                 for user in users:
                     del user[id]
                     user.save()
-                self.setTeams(teams)   
+                self.set_teams(teams)
             elif action == 'updateUsers':
                 for user in users:
                     for team in teams:
@@ -98,14 +83,33 @@ class TicketTeamDispatcherAdmin(Component):
                             user[team] = '1'
                         else:
                             user[team] = '0'
-                        
                     user.save()
-                                
-        return 'ttd_admin.html', {'teams' : teams, 'users' : users, 'caption' : caption}
 
-    # INavigationContributor
+        return 'ttd_admin.html', {
+            'teams' : teams,
+            'users' : users,
+            'caption' : caption
+        }
+
+    # INavigationContributor methods
     def get_templates_dirs(self):
+        from pkg_resources import resource_filename
         return [resource_filename(__name__, 'templates')]
 
     def get_htdocs_dirs(self):
         return []
+
+    # Internal methods
+    def get_caption(self):
+        return self.config.get('ticket-custom', 'ttd.label')
+
+    def set_caption(self, caption):
+        self.config.set('ticket-custom', 'ttd.label', caption)
+        self.config.save()
+
+    def get_teams(self):
+        return self.config.getlist('ticket-custom', 'ttd.options', sep='|')
+
+    def set_teams(self, teams):
+        self.config.set('ticket-custom', 'ttd.options', '|'.join(teams))
+        self.config.save()
