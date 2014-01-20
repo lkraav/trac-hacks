8823c79430670aa69e767e99c859e77e8e23ca9b
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2014-01-20 01:31:42 +0000

    0.3dev: Plugin does not have an `htdocs` directory, so `get_htdocs_dir` needs to return an empty list. Fixes #8587, #8594.
    git-svn-id: http://trac-hacks.org/svn@13579 7322e99d-02ea-0310-aa39-e9a107903beb

--- ticketteamdispatcherplugin/0.11/ttd/admin.py
+++ ticketteamdispatcherplugin/0.11/ttd/admin.py
@@ -97,22 +97,7 @@ class TicketTeamDispatcherAdmin(Component):
 
     # INavigationContributor
     def get_templates_dirs(self):
-        """
-            Return the absolute path of the directory containing the provided
-            templates
-        """
         return [resource_filename(__name__, 'templates')]
 
     def get_htdocs_dirs(self):
-        """
-        Return a list of directories with static resources (such as style
-        sheets, images, etc.)
-
-        Each item in the list must be a `(prefix, abspath)` tuple. The
-        `prefix` part defines the path in the URL that requests to these
-        resources are prefixed with.
-        
-        The `abspath` is the absolute path to the directory containing the
-        resources on the local file system.
-        """
-        return [('ttd', resource_filename(__name__, 'htdocs'))]
+        return []
