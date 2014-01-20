8d26ba2c11350f0e668da5e23bb006bc9540f1ce
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2014-01-20 01:31:45 +0000

    0.3dev: Specify install requirement in `setup`.
    git-svn-id: http://trac-hacks.org/svn@13580 7322e99d-02ea-0310-aa39-e9a107903beb

--- ticketteamdispatcherplugin/0.11/setup.py
+++ ticketteamdispatcherplugin/0.11/setup.py
@@ -1,7 +1,4 @@
 from setuptools import setup
-import pkg_resources
-
-pkg_resources.require("TracUserManagerPlugin>=0.4")
 
 PACKAGE = 'TicketTeamDispatcher'
 PACKAGE_SHORT = 'ttd'
@@ -15,10 +12,10 @@ setup(
     author_email = 'Alexander von Bremen-Kuehne',
     url = 'http://trac-hacks.org/wiki/TicketTeamDispatcherPlugin',
     license = 'GPLv2 or later',
-    description = 'Sends mails on ticket-creation to specified addresses according to the selected team.',                      
+    description = 'Sends mails on ticket-creation to specified addresses according to the selected team.',
+    install_requires = ['TracUserManagerPlugin'],
     entry_points = """
         [trac.plugins]
         %(pkg)s = %(pkg_s)s
     """ % {'pkg': PACKAGE, 'pkg_s' : PACKAGE_SHORT },
 )
-
