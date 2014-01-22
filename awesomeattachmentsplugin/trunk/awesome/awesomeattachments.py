b5cdce109e4d8ccf3447e79ccf4f31c4664532c2
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2014-01-22 03:26:27 +0000

    0.3dev: Fixed regression and various non-functional changes. Refs #9071.  * Import of `TracError` class was missing since [12185].  * PEP-0008 and other style changes.  * Removed unused (and malformed) CSS.
    git-svn-id: http://trac-hacks.org/svn@13603 7322e99d-02ea-0310-aa39-e9a107903beb

--- awesomeattachmentsplugin/trunk/awesome/awesomeattachments.py
+++ awesomeattachmentsplugin/trunk/awesome/awesomeattachments.py
@@ -7,40 +7,44 @@
 # you should have received as part of this distribution.
 #
 
-import unicodedata
 import os
+import unicodedata
 
 from trac.attachment import Attachment
-from trac.core import Component, implements
+from trac.core import Component, TracError, implements
 from trac.ticket.notification import TicketNotifyEmail
 from trac.ticket.web_ui import TicketModule
 from trac.util import get_reporter_id
 from trac.util.translation import _
-from trac.web.api import IRequestFilter, IRequestHandler
-from trac.web.chrome import (
-    Chrome, ITemplateProvider, ITemplateStreamFilter, add_link, add_script
-)
-from trac.web.href import Href
+from trac.web.api import IRequestFilter
+from trac.web.chrome import ITemplateProvider, add_link, add_script
+
 
 class AwesomeAttachments(Component):
   
     implements(IRequestFilter, ITemplateProvider)
   
-    ### methods for IRequestFilter
+    ### IRequestFilter methods
     def pre_process_request(self, req, handler):
         return handler
 
     def post_process_request(self, req, template, data, content_type):
-        if template == 'ticket.html' and req.path_info.rstrip() == '/newticket':
+        if template == 'ticket.html' and \
+                req.path_info.rstrip() == '/newticket':
             add_script(req, 'awesome/js/awesome.js')
-            add_link(req, 'image', req.href.chrome('awesome/images/add.png'), 'add', 'text/png', 'add-image')
-            add_link(req, 'image', req.href.chrome('awesome/images/delete.png'), 'delete', 'text/png', 'delete-image')
-            add_link(req, 'image', req.href.chrome('awesome/images/edit.png'), 'edit', 'text/png', 'edit-image')
+            add_link(req, 'image',
+                     req.href.chrome('awesome/images/add.png'),
+                     'add', 'text/png', 'add-image')
+            add_link(req, 'image',
+                     req.href.chrome('awesome/images/delete.png'),
+                     'delete', 'text/png', 'delete-image')
+            add_link(req, 'image',
+                     req.href.chrome('awesome/images/edit.png'),
+                     'edit', 'text/png', 'edit-image')
 
         return template, data, content_type
 
-  
-    ### methods for ITemplateProvider
+    ### ITemplateProvider methods
     def get_htdocs_dirs(self):
         from pkg_resources import resource_filename
         return [('awesome', resource_filename(__name__, 'htdocs'))]
@@ -65,14 +69,16 @@ class TicketUploadModule(TicketModule):
             raise TracError(_("Can't upload empty file"))
         
         max_size = self.env.config.get('attachment', 'max_size')
-        if max_size >= 0 and size > max_size:
-            raise TracError(_('Maximum attachment size: %(num)s bytes', num=max_size), _('Upload failed'))
+        if 0 <= max_size < size:
+            raise TracError(_('Maximum attachment size: %(num)s bytes',
+                              num=max_size), _("Upload failed"))
         
-        filename = unicodedata.normalize('NFC', unicode(upload.filename, 'utf-8'))
+        filename = unicodedata.normalize('NFC',
+                                         unicode(upload.filename, 'utf-8'))
         filename = filename.replace('\\', '/').replace(':', '/')
         filename = os.path.basename(filename)
         if not filename:
-            raise TracError(_('No file uploaded'))
+            raise TracError(_("No file uploaded"))
       
         attachment.description = description
         if 'author' in req.args:
@@ -93,11 +99,14 @@ class TicketUploadModule(TicketModule):
                 ticket #%s: %s""", ticket.id, e)
 
         # Redirect the user to the newly created ticket or add attachment
-        if(isinstance(req.args['attachment[]'], list)):
+        if isinstance(req.args['attachment[]'], list):
             for i in range(len(req.args['attachment[]'])):
-                self._create_attachment(req, ticket, req.args['attachment[]'][i], req.args['description[]'][i])
+                self._create_attachment(req, ticket,
+                                        req.args['attachment[]'][i],
+                                        req.args['description[]'][i])
         else:
-            self._create_attachment(req, ticket, req.args['attachment[]'], req.args['description[]'])
+            self._create_attachment(req, ticket, req.args['attachment[]'],
+                                    req.args['description[]'])
 
         if 'TICKET_VIEW' not in req.perm('ticket', ticket.id):
             req.redirect(req.href.newticket())
