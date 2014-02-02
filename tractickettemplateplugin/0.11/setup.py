5c30db4f8e146105355961d7391e170d01aaf602
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2014-02-02 08:29:10 +0000

    0.8: Bump version to 0.8. Added SVN revision to egg. Refs #6589, #6809, #7311, #7404, #7440, #8555, #8609, #9238,  #10858.
    git-svn-id: http://trac-hacks.org/svn@13633 7322e99d-02ea-0310-aa39-e9a107903beb

--- tractickettemplateplugin/0.11/setup.py
+++ tractickettemplateplugin/0.11/setup.py
@@ -19,7 +19,7 @@ except ImportError:
 
 setup(
     name = 'TracTicketTemplate',
-    version = '0.7',
+    version = '0.8',
     packages = ['tickettemplate'],
     package_data = { 'tickettemplate': [ '*.txt', 'templates/*.*', 'htdocs/*.*', 
         'tests/*.*', 'locale/*.*', 'locale/*/LC_MESSAGES/*.*' ] },
