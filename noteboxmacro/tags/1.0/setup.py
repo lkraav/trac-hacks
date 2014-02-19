4c9cec67948ee4eee17d22227de314f8467e5cf1
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2014-02-19 10:51:59 +0000

    Tagged version 1.0.
    git-svn-id: http://trac-hacks.org/svn@13676 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ noteboxmacro/tags/1.0/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2008-2009 Bernhard Gruenewaldt <trac@gruenewaldt.net>
+# Copyright (C) 2011-2012 Ryan J Ollos <ryan.j.ollos@gmail.com>
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.
+
+from setuptools import setup
+
+setup(
+    name = 'NoteBox',
+    version = 1.0,
+    packages = ['notebox'],
+    package_data = { 'notebox': ['htdocs/css/*.css', 'htdocs/*.png'] },
+    author = 'Bernhard Gruenewaldt',
+    author_email = 'trac@gruenewaldt.net',
+    maintainer = 'Ryan J Ollos',
+    maintainer_email = 'ryan.j.ollos@gmail.com',
+    url = 'http://trac-hacks.org/wiki/NoteBoxMacro',
+    description = 'Macro for rendering a notebox with an icon.',
+    entry_points={'trac.plugins': ['notebox = notebox.macro']},
+    keywords = 'trac macro notebox',
+    license = 'GPLv2+',
+)
