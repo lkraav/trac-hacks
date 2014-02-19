9dc4dc2653016e27acb25d1dc8e4435e6ec168fd
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2014-02-19 10:51:59 +0000

    Tagged version 1.0.
    git-svn-id: http://trac-hacks.org/svn@13676 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ noteboxmacro/tags/1.0/notebox/macro.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2008-2009 Bernhard Gruenewaldt <trac@gruenewaldt.net>
+# Copyright (C) 2011-2012 Ryan J Ollos <ryan.j.ollos@gmail.com>
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.
+
+from genshi.builder import tag
+from trac.core import implements
+from trac.web.chrome import ITemplateProvider, add_stylesheet
+from trac.wiki.api import parse_args
+from trac.wiki.formatter import format_to_html
+from trac.wiki.macros import WikiMacroBase
+
+
+class NoteBoxMacro(WikiMacroBase):
+    """Renders a small colored box with an icon and text.
+
+    To display a !NoteBox on a page, you must call the !NoteBox
+    macro and pass the ''style'' and ''text'' as arguments. The
+    text may contain wiki formatting, however it is not possible
+    to embed other wiki macros within the macro. Also, commas must
+    be escaped with a backslash.
+
+    A third optional argument allows the ''width'' of the !NoteBox
+    to be specified. The ''width'' can be specified in pixels
+    as a percent of the page width. The default width is 70%.
+
+    The following styles are available: '''warn''', '''tip'''
+    and '''note'''.
+
+    Examples:
+    {{{
+    [[NoteBox(warn,If you don't run `update` before `commit`\, your checkin may fail.)]]
+    [[NoteBox(tip,The !NoteBox macro can bring '''attention''' to text within a page.,50%)]]
+    [[NoteBox(note,More styles may be added in a ''future'' release.,350px)]]
+    }}}
+
+    [[NoteBox(warn,If you don't run `update` before `commit`\, your checkin may fail.)]]
+
+    [[NoteBox(tip,The !NoteBox macro can bring '''attention''' to text within a page.,50%)]]
+
+    [[NoteBox(note,More styles may be added in a ''future'' release.,350px)]]
+    """
+
+    implements(ITemplateProvider)
+
+    def expand_macro(self, formatter, name, content):
+        add_stylesheet(formatter.req, 'notebox/css/notebox.css')
+        args, kwargs = parse_args(content)
+        width = len(args) > 2 and args[2].strip() or '70%'
+        return tag.div(format_to_html(self.env, formatter.context,
+                                      args[1].strip()),
+                       class_='notebox-%s' % (args[0].strip(),),
+                       style='width: %s' % (width,))
+
+    # ITemplateProvider
+    def get_htdocs_dirs(self):
+        from pkg_resources import resource_filename
+        return [('notebox', resource_filename(__name__, 'htdocs'))]
+
+    def get_templates_dirs(self):
+        return []
