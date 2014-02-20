d6876f3e5268c493c3dc75713698f8341863eaad
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2014-02-20 07:36:36 +0000

    Compatibility for Trac 0.12 and later.
    git-svn-id: http://trac-hacks.org/svn@13700 7322e99d-02ea-0310-aa39-e9a107903beb

--- flexiblereporternotificationplugin/0.11/flexiblereporternotification/api.py
+++ flexiblereporternotificationplugin/0.11/flexiblereporternotification/api.py
@@ -1,22 +1,19 @@
-# -*- coding: utf-8 -*-
-
-from trac.ticket.api import ITicketActionController, TicketSystem
-from trac.ticket.default_workflow import ConfigurableTicketWorkflow
 import trac.ticket.notification as note
 
+
 def get_recipients(self, tktid):
-    
+
     notify_reporter = self.config.getbool('notification',
                                           'always_notify_reporter')
     notify_owner = self.config.getbool('notification',
                                        'always_notify_owner')
     notify_updater = self.config.getbool('notification',
                                          'always_notify_updater')
-    for_reporting = self.config.getlist('notification', 'reporter_states')
-    
+    reporter_states = self.config.getlist('notification', 'reporter_states')
+
     ccrecipients = self.prev_cc
     torecipients = []
-    
+
     # Harvest email addresses from the cc, reporter, owner and status fields
     cursor = self.db.cursor()
     cursor.execute("SELECT cc,reporter,owner,status FROM ticket WHERE id=%s",
@@ -28,31 +25,43 @@ def get_recipients(self, tktid):
         self.owner = row[2]
         self.status = row[3]
         if notify_reporter:
-            for S in for_reporting:
-                if self.status == S:
+            for state in reporter_states:
+                if self.ticket.values.get('status') == state:
                     torecipients.append(row[1])
         if notify_owner:
             torecipients.append(row[2])
-        
+
+    # Harvest email addresses from the author field of ticket_change(s)
     if notify_updater:
         cursor.execute("SELECT DISTINCT author,ticket FROM ticket_change "
                        "WHERE ticket=%s", (tktid,))
         for author, ticket in cursor:
             torecipients.append(author)
-                
+
     # Suppress the updater from the recipients
     updater = None
-    cursor = self.db.cursor()
     cursor.execute("SELECT author FROM ticket_change WHERE ticket=%s "
                    "ORDER BY time DESC LIMIT 1", (tktid,))
     for updater, in cursor:
         break
     else:
-        cursor.execute("SELECT reporter FROM ticket WHERE id=%s", (tktid,))
+        cursor.execute("SELECT reporter FROM ticket WHERE id=%s",
+                       (tktid,))
+        for updater, in cursor:
+            break
 
-    for updater, in cursor:
-        break
-    
+    if not notify_updater:
+        filter_out = True
+        if notify_reporter and (updater == self.reporter):
+            filter_out = False
+        if notify_owner and (updater == self.owner):
+            filter_out = False
+        if filter_out:
+            torecipients = [r for r in torecipients if r and r != updater]
+    elif updater:
+        torecipients.append(updater)
+
     return torecipients, ccrecipients
+
 
 note.TicketNotifyEmail.get_recipients = get_recipients
