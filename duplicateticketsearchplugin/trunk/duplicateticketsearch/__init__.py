57401801eb5d2c7dc91319433d41b9431d7751d9
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2014-03-17 04:36:39 +0000

    1.0.2: Coding style changes and added license headers to Python source files.
    git-svn-id: http://trac-hacks.org/svn@13760 7322e99d-02ea-0310-aa39-e9a107903beb

--- duplicateticketsearchplugin/trunk/duplicateticketsearch/__init__.py
+++ duplicateticketsearchplugin/trunk/duplicateticketsearch/__init__.py
@@ -1,32 +1,40 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-import pkg_resources
+#
+# Copyright (C) 2009-2012 Greg MacLellan
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.
 
 from trac.core import *
 from trac.web import ITemplateStreamFilter
 from trac.web.chrome import add_script, add_stylesheet, ITemplateProvider
 
+
 class DuplicateTicketSearch(Component):
     implements(ITemplateProvider, ITemplateStreamFilter)
 
     # ITemplateProvider methods
-        
+
     def get_htdocs_dirs(self):
-        return [('duplicateticketsearch', pkg_resources.resource_filename(__name__, 'htdocs'))]
+        import pkg_resources import resource_filename
+        return [('duplicateticketsearch',
+                 resource_filename(__name__, 'htdocs'))]
 
     def get_templates_dirs(self):
         return []
 
 
     # ITemplateStreamFilter methods
-    
+
     def filter_stream(self, req, method, filename, stream, data):
-    
+
         if filename == 'ticket.html':
             ticket = data.get('ticket')
-            if ticket and not ticket.id:   # only add for new tickets
-                add_script(req, 'duplicateticketsearch/js/tracDupeSearch.js')                
-                add_stylesheet(req, 'duplicateticketsearch/css/tracDupeSearch.css')
-            
-        return stream
+            if ticket and not ticket.id:  # only add for new tickets
+                add_script(req, 'duplicateticketsearch/js/tracDupeSearch.js')
+                add_stylesheet(req,
+                               'duplicateticketsearch/css/tracDupeSearch.css')
 
+        return stream
--- duplicateticketsearchplugin/trunk/setup.py
+++ duplicateticketsearchplugin/trunk/setup.py
@@ -1,21 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+#
+# Copyright (C) 2009-2012 Greg MacLellan
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.
 
 from setuptools import setup
 
 setup(
-    name = 'DuplicateTicketSearch', 
-    version = '1.0.2',
-    author = 'gregmac',
-    license = 'BSD',
-    description = 'Adds XMLRPC-based check from ticket entry page for potential duplicate tickets.',
-    keywords = 'trac duplicate ticket search plugin',
-    url = 'http://trac-hacks.org/wiki/DuplicateTicketSearchPlugin',
-    packages = ['duplicateticketsearch'],
-    package_data = { 'duplicateticketsearch' : ['htdocs/css/*.css', 'htdocs/js/*'] },
-    entry_points = """
+    name='DuplicateTicketSearch',
+    version='1.0.2',
+    author='gregmac',
+    license='BSD',
+    description='Adds XMLRPC-based check from ticket entry page for potential duplicate tickets.',
+    keywords='trac duplicate ticket search plugin',
+    url='http://trac-hacks.org/wiki/DuplicateTicketSearchPlugin',
+    packages=['duplicateticketsearch'],
+    package_data={
+        'duplicateticketsearch': ['htdocs/css/*.css', 'htdocs/js/*']
+    },
+    entry_points="""
         [trac.plugins]
         DuplicateTicketSearch = duplicateticketsearch
     """,
-    install_requires = ['TracXMLRPC'],  
+    install_requires=['TracXMLRPC'],
 )
