0287d1f0a5c4a6934cc6c1b35fa0100128c7471c
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2014-03-18 00:07:57 +0000

    0.2.0dev: PEP-0008 changes.
    git-svn-id: http://trac-hacks.org/svn@13771 7322e99d-02ea-0310-aa39-e9a107903beb

--- queuesplugin/0.12/setup.py
+++ queuesplugin/0.12/setup.py
@@ -4,14 +4,16 @@
 from setuptools import setup, find_packages
 
 PACKAGE = 'TracQueues'
-VERSION = '0.1.0'
+VERSION = '0.2.0'
 
 setup(
     name=PACKAGE, version=VERSION,
     description='Manages ticket queues via drag-and-drop',
     author="Rob Guttman", author_email="guttman@alum.mit.edu",
     license='GPL', url='http://trac-hacks.org/wiki/TracQueuesPlugin',
-    packages = ['queues'],
-    package_data = {'queues':['templates/*.html','htdocs/*.css','htdocs/*.js']},
-    entry_points = {'trac.plugins':['queues.web_ui = queues.web_ui']}
+    packages=['queues'],
+    package_data={
+        'queues': ['templates/*.html', 'htdocs/*.css', 'htdocs/*.js']
+    },
+    entry_points = {'trac.plugins': ['queues.web_ui = queues.web_ui']}
 )
