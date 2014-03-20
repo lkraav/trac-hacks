dcfaea3060d0846c45b53a456c9c1128d440cc71
cauly <cauly@7322e99d-02ea-0310-aa39-e9a107903beb>
2014-03-20 13:35:15 +0000

    add license info
    git-svn-id: http://trac-hacks.org/svn@13782 7322e99d-02ea-0310-aa39-e9a107903beb

--- fieldoftableplugin/trunk/FieldOfTable/FieldOfTable.py
+++ fieldoftableplugin/trunk/FieldOfTable/FieldOfTable.py
@@ -1,4 +1,10 @@
-# -*- coding: utf8 -*- 
+# -*- coding: utf8 -*-
+#
+# Copyright (C) Cauly Kan, mail: cauliflower.kan@gmail.com
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.
 
 '''
 Created on 2014-03-19 
--- fieldoftableplugin/trunk/setup.py
+++ fieldoftableplugin/trunk/setup.py
@@ -1,4 +1,10 @@
 # -*- coding: utf8 -*- 
+#
+# Copyright (C) Cauly Kan, mail: cauliflower.kan@gmail.com
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.
 
 '''
 Created on 2014-03-19 
@@ -13,6 +19,7 @@ setup(
     packages=find_packages(exclude=['*.tests*']),
     author='Cauly Kan',
     author_email='cauliflower.kan@gmail.com',
+    license = "BSD 3-Clause",
     entry_points = {
         'trac.plugins': [
             'tracfieldoftableplugin = FieldOfTable',
