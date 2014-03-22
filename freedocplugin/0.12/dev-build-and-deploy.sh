15648f7d9f139a10f5e2af3213667548a1494918
rickvanderzwet <rickvanderzwet@7322e99d-02ea-0310-aa39-e9a107903beb>
2014-03-22 14:50:50 +0000

    Updating dev script to match new location and make more generic
    git-svn-id: http://trac-hacks.org/svn@13787 7322e99d-02ea-0310-aa39-e9a107903beb

--- freedocplugin/0.12/dev-build-and-deploy.sh
+++ freedocplugin/0.12/dev-build-and-deploy.sh
@@ -3,11 +3,13 @@
 # License: BSDLike
 # Rick van der Zwet <info@rickvanderzwet.nl>
 
-TARGET=${1:-/usr/local/www/trac/tracs/personal/plugins/}
+TARGET=${1:-/usr/local/www/rickvanderzwet.nl/trac/tracs/personal/plugins/}
 WEBSERVER_USER=${2:-www}
 
 rm dist/*
 python setup.py bdist_egg
-sudo rm -v $TARGET/TracFreeDoc*
+
+NAME=`basename *.egg-info .egg-info`
+sudo rm -v $TARGET/$NAME-*
 sudo -u www cp dist/* $TARGET 
 
