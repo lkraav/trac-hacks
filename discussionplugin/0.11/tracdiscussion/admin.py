a89eba851070441ead75415719695e7cb2831722
hasienda <hasienda@7322e99d-02ea-0310-aa39-e9a107903beb>
2014-04-29 11:04:51 +0000

    DiscussionPlugin: Cleanup for the admin panel provider, refs #6783.
    git-svn-id: http://trac-hacks.org/svn@13885 7322e99d-02ea-0310-aa39-e9a107903beb

--- discussionplugin/0.11/tracdiscussion/admin.py
+++ discussionplugin/0.11/tracdiscussion/admin.py
@@ -1,36 +1,32 @@
 # -*- coding: utf-8 -*-
 
-# Standard imports.
 import time
 
-# Trac imports.
-from trac.core import *
+from trac.admin import IAdminPanelProvider
+from trac.core import Component, implements
 from trac.mimeview import Context
+from trac.perm import IPermissionRequestor
 from trac.web.chrome import add_stylesheet
 from trac.wiki import wiki_to_html, wiki_to_oneliner
 
-# Trac interfaces.
-from trac.perm import IPermissionRequestor
-from trac.admin import IAdminPanelProvider
+from tracdiscussion.api import DiscussionApi
 
-# Local imports.
-from tracdiscussion.api import *
 
 class DiscussionWebAdmin(Component):
+    """[opt] Implements discussion plugin administration method access
+       via web-UI.
     """
-        The webadmin module implements discussion plugin administration
-        via WebAdminPlugin.
-    """
+
     implements(IAdminPanelProvider)
 
-    # IAdminPageProvider
+    # IAdminPageProvider methods
+
     def get_admin_panels(self, req):
         if req.perm.has_permission('DISCUSSION_ADMIN'):
             yield ('discussion', 'Discussion System', 'group', 'Forum Groups')
             yield ('discussion', 'Discussion System', 'forum', 'Forums')
 
     def render_admin_panel(self, req, category, page, path_info):
-        # Prepare request object.
         if page == 'forum':
             if not req.args.has_key('group'):
                 req.args['group'] = '-1'
@@ -40,20 +36,19 @@ class DiscussionWebAdmin(Component):
             if path_info:
                 req.args['group'] = path_info
 
-        # Create request context.
+        # Create context with additional arguments prepared before.
         context = Context.from_request(req)
         context.realm = 'discussion-admin'
 
-        # Process request.
-        api = self.env[DiscussionApi]
+        # Process admin panel request.
+        api = DiscussionApi(self.env)
         template, data = api.process_discussion(context)
 
         if context.redirect_url:
             # Redirect request if needed.
             href = req.href(context.redirect_url[0]) + context.redirect_url[1]
-            self.log.debug("Redirecting to %s" % (href))
-            req.redirect(req.href('discussion', 'redirect', redirect_url =
-              href))
+            self.log.debug("Redirecting to %s" % href)
+            req.redirect(req.href('discussion', 'redirect',
+                                  redirect_url=href))
         else:
-            # Return template and data.
             return template, data
