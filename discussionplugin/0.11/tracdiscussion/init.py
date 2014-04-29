14f136a53d2ad1483abec2c7091eb96686f7d313
hasienda <hasienda@7322e99d-02ea-0310-aa39-e9a107903beb>
2014-04-29 17:01:47 +0000

    DiscussionPlugin: Clean-up for environment setup participant, refs #6783.
    Most notably passing a db cursor outside of the scope of the constructing
    method or function shall be avoided, because it causes rather nasty,
    hard-to-debug issues. Rephrasing 'db' to 'schema' for clarity.
    
    We do not handle exceptions on db upgrade but let Trac deal with it instead,
    enabling it to better care itself for side-effects on other setup participants.
    Introducing some INFO logging gives a bit more insight behind the scene of
    running upgrade actions.
    
    git-svn-id: http://trac-hacks.org/svn@13887 7322e99d-02ea-0310-aa39-e9a107903beb

--- discussionplugin/0.11/tracdiscussion/init.py
+++ discussionplugin/0.11/tracdiscussion/init.py
@@ -1,57 +1,69 @@
 # -*- coding: utf-8 -*-
 
-# Trac imports.
-from trac.core import *
+from trac.core import Component, TracError, implements
 from trac.db import *
-
-# Trac interfaces.
 from trac.env import IEnvironmentSetupParticipant
 
-# Last discussion database shcema version.
-last_db_version = 5
+# Current discussion database schema version.
+schema_version = 5
+
 
 class DiscussionInit(Component):
-    """
-        Initialise database and environment for discussion component.
-    """
+    """[main] Initialises database and environment for discussion storage."""
+
     implements(IEnvironmentSetupParticipant)
 
-    # IEnvironmentSetupParticipant.
+    # IEnvironmentSetupParticipant methods
+
     def environment_created(self):
         pass
 
     def environment_needs_upgrade(self, db):
-        cursor = db.cursor()
-
-        # Is database up to date?
-        return self._get_db_version(cursor) != last_db_version
+        # Provide a db handle. A prepared cursor could get out-of-scope.
+        schema_ver = self._get_schema_version(db)
+        if schema_ver == schema_version:
+            return False
+        elif schema_ver > schema_version:
+            raise TracError("A newer plugin version has been installed "
+                            "before, but downgrading is unsupported.")
+        self.log.info("TracDiscussion database schema version is %d, "
+                      "should be %d"  % (schema_ver, schema_version))
+        return True
 
     def upgrade_environment(self, db):
+        """Each schema version should have its own upgrade module, named
+        upgrades/dbN.py, where 'N' is the version number (int).
+        """
+        schema_ver = self._get_schema_version(db)
+
         cursor = db.cursor()
+        # Always perform incremental upgrades.
+        for i in range(schema_ver + 1, schema_version + 1):
+            script_name  = 'db%i' % i
+            try:
+                upgrades = __import__('tracdiscussion.db', globals(),
+                                      locals(), [script_name])
+                script = getattr(upgrades, script_name)
+            except AttributeError:
+                raise TracError("No upgrade module for version %(num)i "
+                                "(%(version)s.py)", num=i, version=name)
+            script.do_upgrade(self.env, cursor)
 
-        # Get current database schema version.
-        db_version = self._get_db_version(cursor)
-
-        # Perform incremental upgrades.
-        for I in range(db_version + 1, last_db_version + 1):
-            script_name  = 'db%i' % (I)
-            module = __import__('tracdiscussion.db.%s' % (script_name),
-            globals(), locals(), ['do_upgrade'])
-            module.do_upgrade(self.env, cursor)
-
-        # Finish database changes.
+        self.log.info("Upgraded TracDiscussion db schema from version "
+                      "%d to %d" % (schema_ver, schema_version))
         db.commit()
 
-    def _get_db_version(self, cursor):
-        # ToDo: Replace with exception-free code here.
-        try:
-            sql = ("SELECT value "
-                   "FROM system "
-                   "WHERE name='discussion_version'")
-            self.log.debug(sql)
-            cursor.execute(sql)
-            for row in cursor:
-                return int(row[0])
-            return 0
-        except:
-            return 0
+    # Internal methods
+
+    def _get_schema_version(self, db=None):
+        """Return the current schema version for this plugin."""
+        if not db:
+            db = self.env.get_db_cnx()
+        cursor = db.cursor()
+        cursor.execute("""
+            SELECT value
+              FROM system
+             WHERE name='discussion_version'
+        """)
+        row = cursor.fetchone()
+        return row and int(row[0]) or 0
