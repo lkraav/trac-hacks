76df0b441d2abed1c4ead8ab3c6b2241c909ee3a
hasienda <hasienda@7322e99d-02ea-0310-aa39-e9a107903beb>
2014-05-06 21:27:49 +0000

    DiscussionPlugin: Cleanup spam filter adapter component, refs #11706.
    git-svn-id: http://trac-hacks.org/svn@13910 7322e99d-02ea-0310-aa39-e9a107903beb

--- discussionplugin/0.11/tracdiscussion/spamfilter.py
+++ discussionplugin/0.11/tracdiscussion/spamfilter.py
@@ -2,59 +2,64 @@
 #
 # Copyright (C) 2009-2010 Radek Bartoň <blackhex@post.cz>
 # Copyright (C) 2012-2014 Ryan J Ollos <ryan.j.ollos@gmail.com>
+# Copyright (C) 2014 Steffen Hoffmann <hoff.st@web.de>
 #
 # This software is licensed as described in the file COPYING, which
 # you should have received as part of this distribution.
 #
 
-# Spamfilter imports.
+from trac.core import Component, implements
 from trac.util import arity
+
 from tracspamfilter.api import RejectContent
-try: #SpamFilter < 0.7
+try:
+    # SpamFilter < 0.7
     from tracspamfilter.api import FilterSystem
-except ImportError: #SpamFilter 0.7+
+except ImportError:
+    # SpamFilter 0.7+
     from tracspamfilter.filtersystem import FilterSystem
 
-# Local imports.
-from tracdiscussion.api import *
+from tracdiscussion.api import IDiscussionFilter
+
 
 class DiscussionSpamFilter(Component):
+    """[extra] Implements an adapter for Trac Spam Filtering.
+
+    Deny creation of topics and messages with bad content using methods
+    provided by [http://trac.edgewall.org/wiki/SpamFilter TracSpamFilter].
     """
-        The spam filtering component implements adapter for SpamFilterPluging
-        which denies to create topics of messages with bad content.
-    """
+
     implements(IDiscussionFilter)
 
-    # IDiscussionFilter methods.
+    # IDiscussionFilter methods
 
     def filter_topic(self, context, topic):
-        # Test topic for spam.
         try:
-            self._spam_test(context.req, topic['author'], [(None,
-                topic['author']), (None, topic['subject']), (None,
-                topic['body'])], context.req.remote_addr)
+            self._spam_test(context.req, topic['author'],
+                            [(None, topic['author']),
+                             (None, topic['subject']),
+                             (None, topic['body'])],
+                            context.req.remote_addr)
         except RejectContent, error:
-            # Topic contains spam.
+            # Topic contains SPAM.
             return False, error.message
-
-        # Topic is fine.
         return True, topic
 
     def filter_message(self, context, message):
-        # Test message for spam.
         try:
-            self._spam_test(context.req, message['author'], [(None,
-                message['author']), (None, message['body'])],
-                context.req.remote_addr)
+            self._spam_test(context.req, message['author'],
+                            [(None, message['author']),
+                             (None, message['body'])],
+                            context.req.remote_addr)
         except RejectContent, error:
-            # Message contains spam.
+            # Message contains SPAM.
             return False, error.message
-
-        # Message is fine.
         return True, message
 
     def _spam_test(self, req, author, changes, ip):
-        if arity(FilterSystem.test) == 4: #SpamFilter < 0.3.2 or >= 0.7.0
+        if arity(FilterSystem.test) == 4:
+            # SpamFilter < 0.3.2 or >= 0.7.0
             FilterSystem(self.env).test(req, author, changes)
-        else: #SpamFilter >= 0.3.2 or < 0.7.0
+        else:
+            # SpamFilter >= 0.3.2 or < 0.7.0
             FilterSystem(self.env).test(req, author, changes, ip)
