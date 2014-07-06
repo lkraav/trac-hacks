536728f24146ee683e029dd51506b49f3fd218a5
psuter <psuter@7322e99d-02ea-0310-aa39-e9a107903beb>
2014-07-06 08:53:42 +0000

    FoldMacroProcessorMacro: Add macro parameter `tag` to switch the title's HTML tag type. Thanks to Tobias Hommel. (fix #11846)
    git-svn-id: http://trac-hacks.org/svn@13999 7322e99d-02ea-0310-aa39-e9a107903beb

--- foldmacroprocessormacro/FoldMacro.py
+++ foldmacroprocessormacro/FoldMacro.py
@@ -3,16 +3,34 @@ from trac.core import *
 from trac.wiki.macros import WikiMacroBase
 from trac.wiki.formatter import format_to_html
 
+
 class FoldMacro(WikiMacroBase):
     """Expands to a foldable section.
-      
-      The `title` Wiki processor parameter is the header.
-      The Wiki processor content is the folded wiki text.
-      """
+    
+    The Wiki processor content is the folded wiki text.
+    
+    Wiki processor parameters:
+    * `title`: The text in the folding header.
+    * `tag`: The HTML element used for the title (Default: span)
+    
+    Example:
+    {{{
+        {{{#!Fold title="A title for the folded section" tag=h2
+        This section of the wiki page is folded up and can be expanded by
+        clicking on the title.
+
+        This can contain any ''formatted'' **wiki** content, including macros
+        and nested Fold sections.
+        }}}
+    }}}
+    """
 
     def expand_macro(self, formatter, name, content, args):
-        html_title = tag.h1(args.get('title', 'Use {{{#!Fold title="Your title"}}}'), class_="foldable")
-        html_content = format_to_html(self.env, formatter.context, content)
+        title_text = args.get('title', 'Use {{{#!Fold title="Your title"}}}')
+        title_tag = args.get('tag', 'span')
+        title_tag_function = getattr(tag, title_tag)
+        title_html = title_tag_function(title_text, class_="foldable")
+        content_html = format_to_html(self.env, formatter.context, content)
         return tag.div(
-                  tag(html_title,
-                      tag.div(html_content)))
+                  tag(title_html,
+                      tag.div(content_html)))
