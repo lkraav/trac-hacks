b89a540b54ee7aa1c29e876de942b4514fc50d7c
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2014-07-25 11:33:11 +0000

    Changed license to 3-Clause BSD with permission of author. Refs #11832.
    git-svn-id: http://trac-hacks.org/svn@14030 7322e99d-02ea-0310-aa39-e9a107903beb

--- queuesplugin/0.12/setup.py
+++ queuesplugin/0.12/setup.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+#
+# Copyright (C) 2011-2012 Rob Guttman <guttman@alum.mit.edu>
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.
+#
 
 from setuptools import setup, find_packages
 
@@ -10,7 +17,7 @@ setup(
     name=PACKAGE, version=VERSION,
     description='Manages ticket queues via drag-and-drop',
     author="Rob Guttman", author_email="guttman@alum.mit.edu",
-    license='GPL', url='http://trac-hacks.org/wiki/QueuesPlugin',
+    license='3-Clause BSD', url='http://trac-hacks.org/wiki/QueuesPlugin',
     packages=['queues'],
     package_data={
         'queues': ['templates/*.html', 'htdocs/*.css', 'htdocs/*.js']
