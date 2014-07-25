f56fd4a5c80f15fdd18927e0a81e5590f858420a
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2014-07-25 11:34:51 +0000

    Changed license to 3-Clause BSD with permission of author. Refs #11832.
    git-svn-id: http://trac-hacks.org/svn@14032 7322e99d-02ea-0310-aa39-e9a107903beb

--- dynamicvariablesplugin/0.12/dynvars/web_ui.py
+++ dynamicvariablesplugin/0.12/dynvars/web_ui.py
@@ -1,8 +1,18 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2011-2012 Rob Guttman <guttman@alum.mit.edu>
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.
+#
+
 import re
 from trac.core import *
 from trac.web.chrome import ITemplateProvider, add_script
 from trac.web.main import IRequestFilter, IRequestHandler
 from trac.ticket import TicketSystem, Milestone
+
 
 class DynamicVariablesModule(Component):
     implements(IRequestHandler, ITemplateProvider, IRequestFilter)
--- dynamicvariablesplugin/0.12/setup.py
+++ dynamicvariablesplugin/0.12/setup.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+#
+# Copyright (C) 2011-2012 Rob Guttman <guttman@alum.mit.edu>
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.
+#
 
 from setuptools import setup, find_packages
 
@@ -10,7 +17,8 @@ setup(
     name=PACKAGE, version=VERSION,
     description='Default and convert report dynamic variables to pulldowns',
     author="Rob Guttman", author_email="guttman@alum.mit.edu",
-    license='GPL', url='http://trac-hacks.org/wiki/TracQueuesPlugin',
+    license='3-Clause BSD',
+    url='http://trac-hacks.org/wiki/DynamicVariablesPlugin',
     packages = ['dynvars'],
     package_data = {'dynvars':['templates/*.html','htdocs/*.js']},
     entry_points = {'trac.plugins':['dynvars.web_ui = dynvars.web_ui']}
