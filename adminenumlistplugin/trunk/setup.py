64ea806c7d6b2b725462ada63952948436854523
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2014-08-25 06:12:51 +0000

    2.0: Changed plugin name to TracAdminEnumListPlugin since it will be published to PyPI.
    git-svn-id: http://trac-hacks.org/svn@14121 7322e99d-02ea-0310-aa39-e9a107903beb

--- adminenumlistplugin/trunk/setup.py
+++ adminenumlistplugin/trunk/setup.py
@@ -12,7 +12,7 @@
 
 from setuptools import find_packages, setup
 
-setup(name='AdminEnumListPlugin',
+setup(name='TracAdminEnumListPlugin',
       version='2.0',
       description="Adds drag-and-drop reordering to admin panel enum lists.",
       author='Stepan Riha',
