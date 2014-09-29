8f3da498dbf6f5ef5b8dee949244f5204c1fc722
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2014-09-29 19:51:16 +0000

    1.0dev: Append table of revisions in an ITemplateStreamFilter call rather than an AJAX callback.
    The //Change History// is initially collapsed.
    
    Refs #11821.
    
    
    git-svn-id: http://trac-hacks.org/svn@14186 7322e99d-02ea-0310-aa39-e9a107903beb

--- tracticketchangelogplugin/1.0/ticketlog/web_ui.py
+++ tracticketchangelogplugin/1.0/ticketlog/web_ui.py
@@ -7,34 +7,31 @@
 # you should have received as part of this distribution.
 #
 
-import cgi
 import re
-import urllib
 from pkg_resources import resource_filename
 try:
     import json
 except ImportError:
     import simplejson as json
 
+from genshi.filters.transform import Transformer
 from trac.config import IntOption, Option
 from trac.core import *
 from trac.mimeview.api import Context
 from trac.perm import IPermissionRequestor
 from trac.versioncontrol.api import RepositoryManager
-from trac.web.api import IRequestHandler, ITemplateStreamFilter, RequestDone
-from trac.web.chrome import (Chrome, ITemplateProvider, add_script,
-                             add_stylesheet)
+from trac.web.api import ITemplateStreamFilter
+from trac.web.chrome import Chrome, ITemplateProvider, add_stylesheet
 from trac.wiki.formatter import format_to_oneliner
 from trac.util.datefmt import format_datetime, from_utimestamp, user_time
 from trac.util.text import shorten_line
 
-from i18n_domain import _, N_, add_domain, gettext, tag_
+from i18n_domain import add_domain
 
 
 class TicketlogModule(Component):
 
-    implements(IPermissionRequestor, IRequestHandler, ITemplateProvider,
-               ITemplateStreamFilter)
+    implements(IPermissionRequestor, ITemplateProvider, ITemplateStreamFilter)
 
     max_message_length = IntOption('ticketlog', 'log_message_maxlength',
         doc="""Maximum length of log message to display.""")
@@ -59,31 +56,6 @@ class TicketlogModule(Component):
     def get_htdocs_dirs(self):
         return [('ticketlog', resource_filename(__name__, 'htdocs'))]
 
-    # IRequestHandler methods
-
-    def match_request(self, req):
-        return req.path_info.startswith('/ticketlog')
-
-    def process_request(self, req):
-        req.perm.require('TICKETLOG_VIEW')
-
-        if req.path_info.startswith('/ticketlog/query'):
-            # query revisions of ticket
-            result = {
-                'msg_query_err': _("Error in querying changelogs."),
-                'msg_tkt_rev_head': _("Commit History"),
-                'data': self._handle_ticketlog_query(req),
-                'status': '1',
-            }
-            self._send_response(req, json.dumps(result))
-
-        return 'ticketlog.html', {
-            'gettext': gettext,
-            '_': _,
-            'tag_': tag_,
-            'N_': N_,
-        }, None
-
     # ITemplateStreamFilter methods
 
     def filter_stream(self, req, method, filename, stream, data):
@@ -91,107 +63,63 @@ class TicketlogModule(Component):
                 and req.path_info.startswith('/ticket/') \
                 and 'TICKETLOG_VIEW' in req.perm:
             add_stylesheet(req, 'ticketlog/ticketlog.css')
-            add_script(req, 'ticketlog/json2.js')
-            add_script(req, 'ticketlog/ticketlog.js')
-
+            ticket_id = req.args.get('id')
+            revisions = self._get_ticket_revisions(req, ticket_id)
+            template = Chrome(self.env).load_template('ticketlog.html') \
+                                       .generate(revisions=revisions)
+            stream |= Transformer('//div[@id="ticket"]').after(template)
         return stream
 
     # Internal methods
 
-    def _handle_ticketlog_query(self, req):
-        """handler for query ticket revisions """
-        jsonstr = urllib.unquote(req.read())
-        req.args.update(json.loads(jsonstr))
-
-        # query ticket revisions
-        ticket_id = req.args.get('ticket_id')
-        return {
-            'ticket_id': ticket_id,
-            'headers': [
-                _('Changeset'),
-                _('Author'),
-                _('Time'),
-                _('ChangeLog')
-            ],
-            'header_width': [
-                '5em',
-                '6em',
-                '8em',
-                '',
-            ],
-            'revisions': self._get_ticket_revisions(req, ticket_id),
-        }
-
     def _get_ticket_revisions(self, req, ticket_id):
-        """ get ticket revisions """
         revisions = []
 
         if not ticket_id:
             return revisions
 
-        db = self.env.get_db_cnx()
-        cursor = db.cursor()
-
-        cursor.execute("""
-            SELECT p.value, v.rev, v.author, v.time, v.message
-              FROM revision v
-              LEFT JOIN repository p
-                ON v.repos = p.id AND p.name='name'
-              WHERE message LIKE %s
-        """, ('%#' + ticket_id + '%',))
-            
-        rows = cursor.fetchall()
-
         p = re.compile(self.log_pattern % ticket_id, re.M + re.S + re.U)
 
         intermediate = {}
-        for row in rows:
-            repos_name, rev, author, timestamp, message = row
-            repos = RepositoryManager(self.env).get_repository(repos_name)
-            rev = repos.normalize_rev(rev)
+        for repos_name, rev, author, timestamp, message in self.env.db_query(
+                """
+                SELECT p.value, v.rev, v.author, v.time, v.message
+                FROM revision v
+                  LEFT JOIN repository p
+                  ON v.repos = p.id AND p.name='name'
+                WHERE message LIKE %s
+                """, ('%#' + ticket_id + '%',)):
 
             if not p.match(message):
                 continue
 
-            link = '%s/%s' % (rev, repos_name)
+            repos = RepositoryManager(self.env).get_repository(repos_name)
+            rev = repos.normalize_rev(rev)
+            link = str(rev)
+            if repos_name:
+                link += '/%s' % repos_name
             # Using (rev, author, time, message) as the key
             # If branches from the same repo are under Trac system
             # Only one changeset will be in the ticket changelog
             intermediate[(rev, author, timestamp, message)] = link
 
+        ctxt = Context.from_request(req)
         for key in intermediate:
             rev, author, timestamp, message = key
-            revision = {
-                'rev': rev,
-                'author': Chrome(self.env).format_author(req, author),
-                'time': user_time(req, format_datetime,
-                                  from_utimestamp(timestamp)),
-            }
             if self.max_message_length \
                     and len(message) > self.max_message_length:
                 message = shorten_line(message, self.max_message_length)
-            message = cgi.escape(message)
-            ctxt = Context.from_request(req)
-            revision['message'] = format_to_oneliner(self.env, ctxt, message)
-            revision['link'] = intermediate[key]
+            rev_link = "[changeset:%s]" % intermediate[key]
+            revision = {
+                'link': intermediate[key],
+                'rev': format_to_oneliner(self.env, ctxt, rev_link),
+                'author': Chrome(self.env).format_author(req, author),
+                'time': user_time(req, format_datetime,
+                                  from_utimestamp(timestamp)),
+                'message': format_to_oneliner(self.env, ctxt, message),
+            }
             revisions.append(revision)
 
         revisions.sort(key=lambda r: r['time'], reverse=True)
 
         return revisions
-
-    def _send_response(self, req, message):
-        """ send response and stop request handling
-        """
-        req.send_response(200)
-        req.send_header('Cache-control', 'no-cache')
-        req.send_header('Expires', 'Fri, 01 Jan 1999 00:00:00 GMT')
-        req.send_header('Content-Type', 'text/plain' + ';charset=utf-8')
-        req.send_header('Content-Length', len(isinstance(message, unicode)
-                                          and message.encode('utf-8')
-                                          or message))
-        req.end_headers()
-
-        if req.method != 'HEAD':
-            req.write(message)
-        raise RequestDone
