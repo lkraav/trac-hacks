9ef4b85f0f5f10c417879f209840a3e95e883d4d
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2014-11-04 12:21:12 +0000

    Replaced tabs with spaces.
    git-svn-id: http://trac-hacks.org/svn@14236 7322e99d-02ea-0310-aa39-e9a107903beb

--- sqlquerymacro/0.11/sqlquery/macro.py
+++ sqlquerymacro/0.11/sqlquery/macro.py
@@ -11,95 +11,93 @@ from pymills.db import Connection
 from pymills.datatypes import OrderedDict
 from pymills.table import Table, Header, Row, Cell
 
-__all__ = ["SqlQueryMacro"]
 
 class SqlQueryMacro(Component):
-	"""
-	A macro to execute an SQL query against a
-	configured database and render the results
-	in a table.
+    """
+    A macro to execute an SQL query against a
+    configured database and render the results
+    in a table.
 
-	Example:
-	{{{
-	[[SQL(SELECT foo FROM bar)]]
-	}}}
-	"""
-	
-	implements(IWikiMacroProvider, ITemplateProvider)
+    Example:
+    {{{
+    [[SQL(SELECT foo FROM bar)]]
+    }}}
+    """
 
-	uri = Option("sqlquery", "uri", "sqlite://:memory:",
-		"""Database URI to connect to and use for SQL Queries""")
-	
-	# IWikiMacroProvider methods
-	def get_macros(self):
-		yield "SQL"
-		
-	def get_macro_description(self, name):
-		return inspect.getdoc(self.__class__)
+    implements(IWikiMacroProvider, ITemplateProvider)
 
-	def expand_macro(self, formatter, name, args):
-		if args is None:
-			return "No query defined!"
+    uri = Option("sqlquery", "uri", "sqlite://:memory:",
+        """Database URI to connect to and use for SQL Queries""")
 
-		sql = str(args).strip()
+    # IWikiMacroProvider methods
+    def get_macros(self):
+        yield "SQL"
 
-		db = Connection(self.uri)
-		try:
-			records = db.do(sql)
-		finally:
-			db.rollback()
-			db.close()
+    def get_macro_description(self, name):
+        return inspect.getdoc(self.__class__)
 
-		if records:
-			headers = []
-			fields = [x for x in records[0].keys() if not x.startswith("__") and not x.endswith("__")]
-			for field in fields:
-				headers.append(Header(field))
+    def expand_macro(self, formatter, name, args):
+        if args is None:
+            return "No query defined!"
 
-			groups = OrderedDict()
-			for record in records:
-				style = ""
+        sql = str(args).strip()
 
-				if record.has_key("__color__"):
-					style += "background-color: %s" % record["__color__"]
-					record.remove("__color__")
-				
-				if record.has_key("__group__"):
-					group = record["__group__"]
-					record.remove("__group__")					
-					row = Row([Cell(v) for v in record.values()],
-						style=style)
-					if not groups.has_key(group):
-						groups[group] = []
-					groups[group].append(row)
-				else:
-					row = Row([Cell(v) for v in record.values()],
-						style=style)
-					if not groups.has_key(None):
-						groups[None] = []
-					groups[None].append(row)
+        db = Connection(self.uri)
+        try:
+            records = db.do(sql)
+        finally:
+            db.rollback()
+            db.close()
 
-			s = StringIO()
-			for group, rows in groups.iteritems():
-				t = Table(headers, rows, cls="wiki")
-				t.refresh()
-				if group:
-					s.write("=== %s ===\n" % group)
-				s.write("{{{\n")
-				s.write("#!html\n")
-				s.write(t.toHTML())
-				s.write("\n}}}\n")
+        if records:
+            headers = []
+            fields = [x for x in records[0].keys() if not x.startswith("__") and not x.endswith("__")]
+            for field in fields:
+                headers.append(Header(field))
 
-			v = s.getvalue()
-			s.close()
-			return wiki_to_html(v, self.env, formatter.req)
-		else:
-			return "No results"
+            groups = OrderedDict()
+            for record in records:
+                style = ""
 
-	# ITemplateProvider methods
-	def get_templates_dirs(self):
-		return []
-		
-	def get_htdocs_dirs(self):
-		return []
-	
+                if "__color__" in record:
+                    style += "background-color: %s" % record["__color__"]
+                    record.remove("__color__")
+
+                if "__group__" in record:
+                    group = record["__group__"]
+                    record.remove("__group__")
+                    row = Row([Cell(v) for v in record.values()],
+                              style=style)
+                    if group not in groups:
+                        groups[group] = []
+                    groups[group].append(row)
+                else:
+                    row = Row([Cell(v) for v in record.values()],
+                              style=style)
+                    if None not in groups:
+                        groups[None] = []
+                    groups[None].append(row)
+
+            s = StringIO()
+            for group, rows in groups.iteritems():
+                t = Table(headers, rows, cls="wiki")
+                t.refresh()
+                if group:
+                    s.write("=== %s ===\n" % group)
+                s.write("{{{\n")
+                s.write("#!html\n")
+                s.write(t.toHTML())
+                s.write("\n}}}\n")
+
+            v = s.getvalue()
+            s.close()
+            return wiki_to_html(v, self.env, formatter.req)
+        else:
+            return "No results"
+
+    # ITemplateProvider methods
+    def get_templates_dirs(self):
+        return []
+
+    def get_htdocs_dirs(self):
+        return []
