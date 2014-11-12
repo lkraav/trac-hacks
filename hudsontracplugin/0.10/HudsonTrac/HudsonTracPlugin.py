9447c0197e92392eb0614b21f731fb0e2723331c
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2014-11-12 07:12:51 +0000

    Fixed error in [14251]. Remove `target` attribute when `display_in_new_tab` is `False`. Refs #12036.
    git-svn-id: http://trac-hacks.org/svn@14252 7322e99d-02ea-0310-aa39-e9a107903beb

--- hudsontracplugin/0.10/HudsonTrac/HudsonTracPlugin.py
+++ hudsontracplugin/0.10/HudsonTrac/HudsonTracPlugin.py
@@ -250,9 +250,10 @@ class HudsonTracPlugin(Component):
 
     def get_navigation_items(self, req):
         if self.nav_url and req.perm.has_permission('BUILD_VIEW'):
+            print self.disp_tab
             yield ('mainnav', 'builds',
                    tag.a('Builds', href=self.nav_url,
-                         target='hudson' if self.disp_tab else ''))
+                         target='hudson' if self.disp_tab else None))
 
     # ITemplateProvider methods
     def get_templates_dirs(self):
