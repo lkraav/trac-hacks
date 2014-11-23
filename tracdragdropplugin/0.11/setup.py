2fba77e7ad04aba4d9638688893362202ecbe515
jun66j5 <jun66j5@7322e99d-02ea-0310-aa39-e9a107903beb>
2014-11-23 13:21:27 +0000

    TracDragDropPlugin: call `setuptools.setup()` only if run `setup.py` as a script
    git-svn-id: http://trac-hacks.org/svn@14282 7322e99d-02ea-0310-aa39-e9a107903beb

--- tracdragdropplugin/0.11/setup.py
+++ tracdragdropplugin/0.11/setup.py
@@ -3,24 +3,29 @@
 
 from setuptools import setup, find_packages
 
-setup(
-    name = 'TracDragDrop',
-    version = '0.11.0.10',
-    description = 'Add drag-and-drop attachments feature to Trac',
-    license = 'BSD', # the same as Trac
-    url = 'http://trac-hacks.org/wiki/TracDragDropPlugin',
-    author = 'Jun Omae',
-    author_email = 'jun66j5@gmail.com',
-    install_requires = ['Trac>=0.11,!=0.12-dev,<0.12'],
-    packages = find_packages(exclude=['*.tests*']),
-    package_data = {
-        'tracdragdrop' : [
-            'htdocs/*.js', 'htdocs/*.css', 'htdocs/*.gif', 'templates/*.html',
-        ],
-    },
-    entry_points = {
-        'trac.plugins': [
-            'tracdragdrop.web_ui = tracdragdrop.web_ui',
-        ],
-    }
-)
+def main():
+    setup(
+        name = 'TracDragDrop',
+        version = '0.11.0.10',
+        description = 'Add drag-and-drop attachments feature to Trac',
+        license = 'BSD', # the same as Trac
+        url = 'http://trac-hacks.org/wiki/TracDragDropPlugin',
+        author = 'Jun Omae',
+        author_email = 'jun66j5@gmail.com',
+        install_requires = ['Trac>=0.11,!=0.12-dev,<0.12'],
+        packages = find_packages(exclude=['*.tests*']),
+        package_data = {
+            'tracdragdrop': [
+                'htdocs/*.js', 'htdocs/*.css', 'htdocs/*.gif',
+                'templates/*.html',
+            ],
+        },
+        entry_points = {
+            'trac.plugins': [
+                'tracdragdrop.web_ui = tracdragdrop.web_ui',
+            ],
+        }
+    )
+
+if __name__ == '__main__':
+    main()
