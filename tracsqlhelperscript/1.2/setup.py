5199a464756dfe394651b47fa8d31404668db6fc
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2014-12-05 10:07:30 +0000

    0.3.0: New branch compatible with Trac 1.1.2 and later. Fixes #12090.
    git-svn-id: http://trac-hacks.org/svn@14299 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ tracsqlhelperscript/1.2/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2009 Jeff Hammel <jhammel@openplans.org>
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.
+#
+
+from setuptools import setup, find_packages
+
+version = '0.3.0'
+
+setup(name='TracSQLHelper',
+      version=version,
+      description="SQL Helper functions for Trac",
+      long_description="""SQL Helper functions for Trac
+                       """,
+      classifiers=[],
+      keywords='SQL Trac',
+      author='Jeff Hammel',
+      author_email='jhammel@openplans.org',
+      url='http://trac-hacks.org/wiki/k0s',
+      license='BSD 3-Clause',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=['Trac'],
+      entry_points="""
+      """,
+      )
