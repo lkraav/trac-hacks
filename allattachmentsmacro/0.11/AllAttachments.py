c8406f1726faecf1e6426613eedb2e467decebcf
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2015-01-18 07:54:32 +0000

    PEP-0008 coding style changes.
    git-svn-id: https://trac-hacks.org/svn@14369 7322e99d-02ea-0310-aa39-e9a107903beb

--- allattachmentsmacro/0.11/AllAttachments.py
+++ allattachmentsmacro/0.11/AllAttachments.py
@@ -2,35 +2,37 @@
 #
 # Copyright (C) 2008 Daan van Etten <daan@stuq.nl>
 # All rights reserved.
-# This work is licensed under the Creative Commons Attribution-Noncommercial-Share Alike 3.0 License.
-# To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/3.0/ or send 
-# a letter to Creative Commons, 171 Second Street, Suite 300, San Francisco, California, 94105, USA.
+# This work is licensed under the Creative Commons
+# Attribution-Noncommercial-Share Alike 3.0 License.
+# To view a copy of this license, visit
+# http://creativecommons.org/licenses/by-nc-sa/3.0/ or send
+# a letter to Creative Commons, 171 Second Street, Suite 300
+# San Francisco, California, 94105, USA.
 
-from trac.wiki.macros import WikiMacroBase, parse_args
+from trac.wiki.macros import WikiMacroBase
 from genshi.builder import tag
 from trac.util.text import pretty_size
 from trac.resource import ResourceNotFound
 from trac.attachment import Attachment
 
-revison = "$Rev$"
+revision = "$Rev$"
 url = "$URL$"
+
 
 class AllAttachmentsMacro(WikiMacroBase):
     """Shows all attachments on the Trac site.
 
        The first argument is the filter for which attachments to show.
-       The filter can have the value 'ticket' or 'wiki'. Omitting the filter argument
-       shows all attachments.
+       The filter can have the value 'ticket' or 'wiki'. Omitting the filter
+       argument shows all attachments.
 
        Examples:
 
        {{{
-           [[AllAttachments()]]                   # Show all attachments
-           [[AllAttachments(ticket)]]             # Show the attachments that are linked to tickets
-           [[AllAttachments(wiki)]]               # Show the attachments that are linked to wiki pages
+           [[AllAttachments()]]       # Show all attachments
+           [[AllAttachments(ticket)]] # Show attachments linked to tickets
+           [[AllAttachments(wiki)]]   # Show attachments linked to wiki pages
        }}}
-
-       ''Created by Daan van Etten (http://stuq.nl/software/trac/AllAttachmentsMacro)''
     """
 
     def expand_macro(self, formatter, name, content):
@@ -41,14 +43,12 @@ class AllAttachmentsMacro(WikiMacroBase):
                 attachment_type = argv[0]
 
         db = self.env.get_db_cnx()
-        if db == None:
-           return "No DB connection"
-        
-        attachmentFormattedList=""
+        if db is None:
+            return "No DB connection"
 
         cursor = db.cursor()
 
-        if attachment_type == None or attachment_type == "":
+        if attachment_type is None or attachment_type == "":
             cursor.execute("SELECT type,id,filename,size,time,"
                            "description,author,ipnr FROM attachment")
         else:
@@ -56,18 +56,26 @@ class AllAttachmentsMacro(WikiMacroBase):
                            "description,author,ipnr FROM attachment "
                            "WHERE type=%s", (attachment_type, ))
         
-        formatters={"wiki": formatter.href.wiki, "ticket": formatter.href.ticket}
-        types={"wiki": "", "ticket": "ticket "}
+        formatters = {
+            'wiki': formatter.href.wiki,
+            'ticket': formatter.href.ticket
+        }
+        types = {
+            'wiki': '',
+            'ticket': 'ticket '
+        }
 
         return tag.ul(
-                      [tag.li(
-                          tag.a(filename, href=formatter.href.attachment(type + "/" + id + "/" + filename)), 
-                          " (", tag.span(pretty_size(size), title=size), ") - added by ",
-                          tag.em(author), " to ",
-                          tag.a(types[type] + " " + id, href=formatters[type](id)), " ")
-                    for type,id,filename,size,time,description,author,ipnr in cursor if self._has_perm(type, id, filename, formatter.context)])
-
-        return attachmentFormattedList
+            [tag.li(
+                tag.a(filename, href=formatter.href.attachment(type + '/' +
+                                                               id + '/' +
+                                                               filename)),
+                " (", tag.span(pretty_size(size), title=size), ") - added by ",
+                tag.em(author), " to ",
+                tag.a(types[type] + ' ' + id, href=formatters[type](id)), ' ')
+             for type, id, filename, size, time, description, author, ipnr
+             in cursor
+             if self._has_perm(type, id, filename, formatter.context)])
 
     def _has_perm(self, parent_realm, parent_id, filename, context):
         try:
