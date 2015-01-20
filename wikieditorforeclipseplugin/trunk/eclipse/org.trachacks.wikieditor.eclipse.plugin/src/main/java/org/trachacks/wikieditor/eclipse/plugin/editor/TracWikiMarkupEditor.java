5e7fd0a3b6b4fbbfa693e982c59cf076c0ebafb5
ivangsa <ivangsa@7322e99d-02ea-0310-aa39-e9a107903beb>
2015-01-20 09:37:11 +0000

    #11849 Fix Compatibility with WikiText 2.0 API changes
    git-svn-id: https://trac-hacks.org/svn@14374 7322e99d-02ea-0310-aa39-e9a107903beb

--- wikieditorforeclipseplugin/trunk/eclipse/org.trachacks.wikieditor.eclipse.plugin/src/main/java/org/trachacks/wikieditor/eclipse/plugin/editor/TracWikiMarkupEditor.java
+++ wikieditorforeclipseplugin/trunk/eclipse/org.trachacks.wikieditor.eclipse.plugin/src/main/java/org/trachacks/wikieditor/eclipse/plugin/editor/TracWikiMarkupEditor.java
@@ -4,20 +4,17 @@
 package org.trachacks.wikieditor.eclipse.plugin.editor;
 
 import java.nio.charset.Charset;
+import java.util.Arrays;
+import java.util.List;
 
 import org.eclipse.jface.text.source.ISourceViewer;
 import org.eclipse.jface.text.source.IVerticalRuler;
 import org.eclipse.jface.viewers.Viewer;
-import org.eclipse.mylyn.wikitext.core.WikiText;
-import org.eclipse.mylyn.wikitext.ui.editor.MarkupSourceViewerConfiguration;
+import org.eclipse.mylyn.wikitext.core.parser.markup.MarkupLanguage;
 import org.eclipse.mylyn.wikitext.ui.editor.WikiTextSourceEditor;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.CTabFolder;
 import org.eclipse.swt.custom.CTabItem;
-import org.eclipse.swt.events.KeyAdapter;
-import org.eclipse.swt.events.KeyEvent;
-import org.eclipse.swt.events.MouseAdapter;
-import org.eclipse.swt.events.MouseEvent;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.widgets.Composite;
@@ -35,11 +32,13 @@ public class TracWikiMarkupEditor extends WikiTextSourceEditor {
 	public static final String DEFAULT_ENCODING = (Charset.isSupported(UTF8)) ? UTF8 : Charset.defaultCharset().displayName();
 	
 	private static final String TRACWIKI_MARKUP_LANGUAGE = "TracWiki";
+	private static final List<String> WIKITEXT_TEST_CLASSNAMES = Arrays.asList("org.eclipse.mylyn.wikitext.core.WikiText", "org.eclipse.mylyn.wikitext.ui.WikiText");
 
 	private CTabFolder tabFolder;
 	private CTabItem sourceTab;	
 	private CTabItem previewTab;
 	private TracWikiPreview preview;
+
 	
 	public TracWikiMarkupEditor() {
 		super();
@@ -49,9 +48,37 @@ public class TracWikiMarkupEditor extends WikiTextSourceEditor {
 
 
 
+	private MarkupLanguage getMarkupLanguage(String name) {
+	    // Support for WikiText 2.0+ see
+	    // http://help.eclipse.org/luna/topic/org.eclipse.mylyn.wikitext.help.ui/help/Upgrading-From-Mylyn-WikiText-1-x-to-2-x.html?cp=41_1_7_1#APIChangesin2.0
+	    
+	    Class wikiTextClass = null;
+	    for (String className : WIKITEXT_TEST_CLASSNAMES) {
+	        try {
+	            wikiTextClass = getClass().forName(className);
+	        } catch (ClassNotFoundException e1) {
+	            // next
+	        }
+	        if (wikiTextClass != null) {
+	            break;
+	        }
+	    }
+
+	    if (wikiTextClass == null) {
+                throw new RuntimeException("WikiText class not found in either " + WIKITEXT_TEST_CLASSNAMES);
+	    }
+
+	    try {
+	        return (MarkupLanguage) wikiTextClass.getMethod("getMarkupLanguage", String.class).invoke(null, name);
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	        throw new RuntimeException(e);
+	    }
+	}
+
 	@Override
 	protected ISourceViewer createSourceViewer(Composite parent, IVerticalRuler ruler, int styles) {
-		setMarkupLanguage(WikiText.getMarkupLanguage(TRACWIKI_MARKUP_LANGUAGE));
+		setMarkupLanguage(getMarkupLanguage(TRACWIKI_MARKUP_LANGUAGE));
 
 		tabFolder = new CTabFolder(parent, SWT.BOTTOM);
 		ISourceViewer viewer = super.createSourceViewer(tabFolder, ruler, styles);
