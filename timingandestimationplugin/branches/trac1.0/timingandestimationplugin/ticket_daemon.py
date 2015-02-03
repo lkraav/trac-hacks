be00d82b8d34e4f1f17020eb67afd598ca5ddf07
bobbysmith007 <bobbysmith007@7322e99d-02ea-0310-aa39-e9a107903beb>
2015-02-03 19:48:37 +0000

    Attempt at fixing concurrency errors in the ticket_daemon
    Replaces stateful work with a handful of sql statements that will
    behave correctly regardless of ordering in the case of concurrency
    
    re #12173
    
    
    
    
    git-svn-id: https://trac-hacks.org/svn@14398 7322e99d-02ea-0310-aa39-e9a107903beb

--- timingandestimationplugin/branches/trac1.0/timingandestimationplugin/ticket_daemon.py
+++ timingandestimationplugin/branches/trac1.0/timingandestimationplugin/ticket_daemon.py
@@ -24,48 +24,56 @@ except Exception:
 
 def save_custom_field_value( db, ticket_id, field, value ):
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM ticket_custom "
-                   "WHERE ticket=%s and name=%s", (ticket_id, field))
-    if cursor.fetchone():
-        cursor.execute("UPDATE ticket_custom SET value=%s "
-                       "WHERE ticket=%s AND name=%s",
-                       (value, ticket_id, field))
-    else:
+    cursor.execute("UPDATE ticket_custom SET value=%s "
+                   "WHERE ticket=%s AND name=%s", (value, ticket_id, field))
+
+    if cursor.rowcount==0:
         cursor.execute("INSERT INTO ticket_custom (ticket,name, "
-                       "value) VALUES(%s,%s,%s)",
-                       (ticket_id, field, value))
+                       "value) VALUES(%s,%s,%s)", (ticket_id, field, value))
 
-DONTUPDATE = "DONTUPDATE"
+def update_totalhours_custom( db, ticket_id):
+    cursor = db.cursor()
+    sumSql = """
+       (SELECT SUM( CASE WHEN newvalue = '' OR newvalue IS NULL THEN 0
+                         ELSE CAST( newvalue AS DECIMAL ) END ) as total 
+          FROM ticket_change
+         WHERE ticket=%s and field='hours')  """
+    cursor.execute("UPDATE ticket_custom SET value="+sumSql+
+                   "WHERE ticket=%s AND name='totalhours'",
+               (ticket_id,ticket_id))
+    if cursor.rowcount==0:
+        cursor.execute("INSERT INTO ticket_custom (name, value, ticket) "+
+                       "VALUES('totalhours',"+sumSql+",%s)",
+                       (ticket_id,ticket_id))
 
-def save_ticket_change( db, ticket_id, author, change_time, field, oldvalue, newvalue, log, dontinsert=False):
-    """tries to save a ticket change,
-
-       dontinsert means do not add the change if it didnt already exist
+def insert_totalhours_changes( db, ticket_id):
+    cursor = db.cursor()
+    sumSql = """
+       INSERT INTO ticket_change (ticket, author, time, field, oldvalue, newvalue)
+       SELECT ticket, author, time, 'totalhours',  
+               (SELECT SUM( CASE WHEN newvalue = '' OR newvalue IS NULL THEN 0
+                           ELSE CAST( newvalue AS DECIMAL ) END ) as total
+               FROM ticket_change as guts 
+               WHERE guts.ticket = ticket_change.ticket AND guts.field='hours'
+                 AND guts.time < ticket_change.time
+              ) as oldvalue, 
+              (SELECT SUM( CASE WHEN newvalue = '' OR newvalue IS NULL THEN 0
+                           ELSE CAST( newvalue AS DECIMAL ) END ) as total
+               FROM ticket_change as guts 
+               WHERE guts.ticket = ticket_change.ticket AND guts.field='hours'
+                 AND guts.time <= ticket_change.time
+              ) as newvalue
+          FROM ticket_change
+         WHERE ticket=%s and field='hours'
+           AND NOT EXISTS( SELECT ticket
+                             FROM ticket_change as guts 
+                            WHERE guts.ticket=ticket_change.ticket
+                              AND guts.author=ticket_change.author
+                              AND guts.time=ticket_change.time
+                              AND field='totalhours')
     """
-    if type(change_time) == datetime.datetime:
-        change_time = to_timestamp(change_time)
-    cursor = db.cursor();
-    sql = """SELECT * FROM ticket_change
-             WHERE ticket=%s and author=%s and time=%s and field=%s"""
+        cursor.execute(sql, (ticket_id))
 
-    cursor.execute(sql, (ticket_id, author, change_time, field))
-    if cursor.fetchone():
-        if oldvalue == DONTUPDATE:
-            cursor.execute("""UPDATE ticket_change  SET  newvalue=%s
-                       WHERE ticket=%s and author=%s and time=%s and field=%s""",
-                           ( newvalue, ticket_id, author, change_time, field))
-
-        else:
-            cursor.execute("""UPDATE ticket_change  SET oldvalue=%s, newvalue=%s
-                       WHERE ticket=%s and author=%s and time=%s and field=%s""",
-                           (oldvalue, newvalue, ticket_id, author, change_time, field))
-    else:
-        if oldvalue == DONTUPDATE:
-            oldvalue = '0'
-        if not dontinsert:
-            cursor.execute("""INSERT INTO ticket_change  (ticket,time,author,field, oldvalue, newvalue)
-                        VALUES(%s, %s, %s, %s, %s, %s)""",
-                           (ticket_id, change_time, author, field, oldvalue, newvalue))
 
 class TimeTrackingTicketObserver(Component):
     implements(ITicketChangeListener)
@@ -73,79 +81,29 @@ class TimeTrackingTicketObserver(Component):
         pass
 
     def watch_hours(self, ticket):
-
-        def readTicketValue(name, tipe, default=0):
-            if ticket.values.has_key(name):
-                return tipe(ticket.values[name] or default)
-            else:
-                val = dbhelper.get_first_row(
-                    self.env, "SELECT * FROM ticket_custom where ticket=%s and name=%s",
-                    ticket.id, name)
-                if val:
-                    return tipe(val[2] or default)
-                return default
-
-
-        hours = readTicketValue("hours", convertfloat)
-        totalHours = readTicketValue("totalhours", convertfloat)
-
-        
         ticket_id = ticket.id
-        cl = ticket.get_changelog()
-
-        self.log.debug("found hours: "+str(hours ));
-        #self.log.debug("Dir_ticket:"+str(dir(ticket)))
-        #self.log.debug("ticket.values:"+str(ticket.values))
-        #self.log.debug("changelog:"+str(cl))
-
-        most_recent_change = None
-        if cl:
-            most_recent_change = cl[-1];
-            change_time = most_recent_change[0]
-            author = most_recent_change[1]
-        else:
-            change_time = ticket.time_created
-            author = ticket.values["reporter"]
-
         @self.env.with_transaction()
         def fn(db):
-            ## SAVE estimated hour
-            estimatedhours = readTicketValue("estimatedhours", convertfloat)
-            self.log.debug("found Estimated hours:"+str(estimatedhours))
-            save_ticket_change( db, ticket_id, author, change_time, "estimatedhours",
-                                DONTUPDATE, str(estimatedhours), self.log, True)
-            save_custom_field_value( db, ticket.id, "estimatedhours", str(estimatedhours))
-            #######################
+            save_custom_field_value( db, ticket_id, "hours", '0')
+            insert_totalhours_changes( db, ticket_id )
+            update_totalhours_custom ( db, ticket_id )
 
-
-            ## If our hours changed
-            if not hours == 0:
-                newtotal = str(totalHours+hours)
-                save_ticket_change( db, ticket_id, author, change_time, "hours",
-                                    '0.0', str(hours), self.log)
-                save_ticket_change( db, ticket_id, author, change_time, "totalhours",
-                                    str(totalHours), str(newtotal), self.log)
-                save_custom_field_value( db, ticket_id, "hours", '0')
-                save_custom_field_value( db, ticket_id, "totalhours", str(newtotal) )
-            ########################
-
-    # END of watch_hours
 
     def ticket_created(self, ticket):
         """Called when a ticket is created."""
         self.watch_hours(ticket)
 
-
     def ticket_changed(self, ticket, comment, author, old_values):
-        """Called when a ticket is modified.
+        """Called when a ticket is modified."""
+        self.watch_hours(ticket)
 
-        `old_values` is a dictionary containing the previous values of the
-        fields that have changed.
-        """
+    def ticket_change_deleted(ticket, cdate, changes):
+        """called when a ticket change is deleted"""
         self.watch_hours(ticket)
 
     def ticket_deleted(self, ticket):
         """Called when a ticket is deleted."""
+        pass
 
 
 class TimeTrackingTicketValidator(Component):
