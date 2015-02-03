72d2ff7b593958cbdad7835f83ed088492aa488b
bobbysmith007 <bobbysmith007@7322e99d-02ea-0310-aa39-e9a107903beb>
2015-02-03 21:45:49 +0000

    new ticket hours bugs fix
    git-svn-id: https://trac-hacks.org/svn@14404 7322e99d-02ea-0310-aa39-e9a107903beb

--- timingandestimationplugin/branches/trac1.0/timingandestimationplugin/ticket_daemon.py
+++ timingandestimationplugin/branches/trac1.0/timingandestimationplugin/ticket_daemon.py
@@ -90,7 +90,10 @@ class TimeTrackingTicketObserver(Component):
 
     def ticket_created(self, ticket):
         """Called when a ticket is created."""
-        self.watch_hours(ticket)
+        hours = convertfloat(ticket['hours'])
+        if hours > 0:
+            ticket['hours']=hours
+            ticket.save_changes(ticket['reporter'])
 
     def ticket_changed(self, ticket, comment, author, old_values):
         """Called when a ticket is modified."""
