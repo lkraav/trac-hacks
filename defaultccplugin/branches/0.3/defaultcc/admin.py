0c2af83f12ccc0fe0898fdd6de4c0d025f08a714
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2015-03-22 19:36:14 +0000

    Created a branch for further development of 0.3dev.
    git-svn-id: https://trac-hacks.org/svn@14501 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ defaultccplugin/branches/0.3/defaultcc/admin.py
@@ -0,0 +1,165 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2009 ERCIM
+# Copyright (C) 2009 Jean-Guilhem Rouel <jean-guilhem.rouel@ercim.org>
+# Copyright (C) 2009 Vivien Lacourba <vivien.lacourba@ercim.org>
+# Copyright (C) 2012 Ryan J Ollos <ryan.j.ollos@gmail.com>
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.
+#
+
+from genshi.builder import tag
+from genshi.filters import Transformer
+from trac.core import Component, TracError, implements
+from trac.db import Column, DatabaseManager, Index, Table
+from trac.env import IEnvironmentSetupParticipant
+from trac.resource import ResourceNotFound
+from trac.ticket import model
+from trac.web.api import IRequestFilter, ITemplateStreamFilter
+
+from defaultcc.model import DefaultCC
+
+
+class DefaultCCAdmin(Component):
+    """Allows to setup a default CC list per component through the component
+    admin UI.
+    """
+
+    implements(IEnvironmentSetupParticipant, ITemplateStreamFilter,
+               IRequestFilter)
+
+    SCHEMA = [
+        Table('component_default_cc', key='name')[
+            Column('name'),
+            Column('cc'),
+            Index(['name']),
+        ]
+    ]
+
+    # IEnvironmentSetupParticipant methods
+
+    def environment_created(self):
+        self._upgrade_db(self.env.get_db_cnx())
+
+    def environment_needs_upgrade(self, db):
+        cursor = db.cursor()
+        try:
+            cursor.execute("SELECT COUNT(*) FROM component_default_cc")
+            cursor.fetchone()
+            return False
+        except:
+            return True
+
+    def upgrade_environment(self, db):
+        self._upgrade_db(db)
+
+    def _upgrade_db(self, db):
+        try:
+            db_backend = DatabaseManager(self.env)._get_connector()[0]
+            cursor = db.cursor()
+            for table in self.SCHEMA:
+                for stmt in db_backend.to_sql(table):
+                    self.log.debug(stmt)
+                    cursor.execute(stmt)
+                    db.commit()
+        except Exception, e:
+            self.log.error(e, exc_info=True)
+            raise TracError(str(e))
+
+    # IRequestFilter methods
+
+    def pre_process_request(self, req, handler):
+        if 'TICKET_ADMIN' in req.perm and req.method == 'POST' \
+                and req.path_info.startswith('/admin/ticket/components'):
+            if req.args.get('save') and req.args.get('name'):
+                old_name = req.args.get('old_name')
+                new_name = req.args.get('name')
+                old_cc = DefaultCC(self.env, old_name)
+                new_cc = DefaultCC(self.env, new_name)
+                new_cc.cc = req.args.get('defaultcc', '')
+                if old_name == new_name:
+                    old_cc.delete()
+                    new_cc.insert()
+                else:
+                    try:
+                        model.Component(self.env, new_name)
+                    except ResourceNotFound:
+                        old_cc.delete()
+                        new_cc.insert()
+            elif req.args.get('add') and req.args.get('name'):
+                name = req.args.get('name')
+                try:
+                    model.Component(self.env, name)
+                except ResourceNotFound:
+                    cc = DefaultCC(self.env, name)
+                    cc.name = name
+                    cc.cc = req.args.get('defaultcc', '')
+                    cc.insert()
+            elif req.args.get('remove'):
+                if req.args.get('sel'):
+                    # If only one component is selected, we don't receive
+                    # an array, but a string preventing us from looping in
+                    # that case.
+                    if isinstance(req.args.get('sel'), basestring):
+                        cc = DefaultCC(self.env, req.args.get('sel'))
+                        cc.delete()
+                    else:
+                        for name in req.args.get('sel'):
+                            cc = DefaultCC(self.env, name)
+                            cc.delete()
+        return handler
+
+    def post_process_request(self, req, template, data, content_type):
+        if template == 'admin_components.html' and 'components' in data:
+            # Prior to Trac 1.0.2-r11919, components was a generator and
+            # expanding the generator causes the table to not be rendered
+            data['components'] = list(data['components'])
+        return template, data, content_type
+
+    # ITemplateStreamFilter methods
+
+    def filter_stream(self, req, method, filename, stream, data):
+        if 'TICKET_ADMIN' in req.perm and \
+                req.path_info.startswith('/admin/ticket/components'):
+            if data.get('component'):
+                cc = DefaultCC(self.env, data.get('component').name)
+                filter = Transformer('//form[@id="modcomp"]/fieldset'
+                                     '/div[@class="field"][2]')
+                filter = filter.after(tag.div("Default CC:",
+                                              tag.br(),
+                                              tag.input(type='text',
+                                                        name='defaultcc',
+                                                        value=cc.cc),
+                                              class_='field')) \
+                               .before(tag.input(type='hidden',
+                                                 name='old_name',
+                                                 value=cc.name))
+                return stream | filter
+            else:
+                filter = Transformer('//form[@id="addcomponent"]'
+                                     '/fieldset/div[@class="buttons"]')
+                stream |= filter.before(tag.div("Default CC:",
+                                                tag.br(),
+                                                tag.input(type='text',
+                                                          name='defaultcc'),
+                                                class_='field'))
+
+                default_ccs = DefaultCC.select(self.env)
+
+                stream |= Transformer('//table[@id="complist"]/thead/tr') \
+                          .append(tag.th('Default CC'))
+
+                for i, comp in enumerate(data.get('components')):
+                    if comp.name in default_ccs:
+                        default_cc = default_ccs[comp.name]
+                    else:
+                        default_cc = ''
+                    filter = Transformer('//table[@id="complist"]'
+                                         '/tbody/tr[%d]' % (i + 1))
+                    stream |= filter.append(tag.td(default_cc,
+                                                   class_='defaultcc'))
+                return stream
+
+        return stream
---
+++ defaultccplugin/branches/0.3/defaultcc/model.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2009 ERCIM
+# Copyright (C) 2009 Jean-Guilhem Rouel <jean-guilhem.rouel@ercim.org>
+# Copyright (C) 2009 Vivien Lacourba <vivien.lacourba@ercim.org>
+# Copyright (C) 2012 Ryan J Ollos <ryan.j.ollos@gmail.com>
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.
+#
+
+import re
+
+class DefaultCC(object):
+    """Class representing components' default CC lists
+    """
+
+    def __init__(self, env, name=None, db=None):
+        self.env = env
+        self.name = name
+        self.cc = None
+        if name:
+            if not db:
+                db = self.env.get_db_cnx()
+            cursor = db.cursor()
+            cursor.execute("""
+                SELECT cc FROM component_default_cc WHERE name=%s
+                """, (name,))
+            row = cursor.fetchone()
+            if row:
+                self.cc = row[0] or None
+
+    def delete(self, db=None):
+        if not db:
+            db = self.env.get_db_cnx()
+            handle_ta = True
+        else:
+            handle_ta = False
+
+        cursor = db.cursor()
+        self.env.log.info("Deleting component %s's default CC" % self.name)
+        cursor.execute("""
+            DELETE FROM component_default_cc WHERE name=%s
+            """, (self.name,))
+
+        if handle_ta:
+            db.commit()
+
+    def insert(self, db=None):
+        assert self.name, "Cannot create default CC for a component" \
+                          " without a name"
+        if not db:
+            db = self.env.get_db_cnx()
+            handle_ta = True
+        else:
+            handle_ta = False
+
+        cursor = db.cursor()
+        self.env.log.debug("Creating %s's default CC" % self.name)
+        cursor.execute("""
+            INSERT INTO component_default_cc (name,cc) VALUES (%s,%s)
+            """, (self.name, _fixup_cc_list(self.cc)))
+
+        if handle_ta:
+            db.commit()
+
+    @classmethod
+    def select(cls, env, db=None):
+        if not db:
+            db = env.get_db_cnx()
+        cursor = db.cursor()
+        cursor.execute("""
+            SELECT name,cc FROM component_default_cc ORDER BY name
+            """)
+        res = {}
+        for name, cc in cursor:
+            res[name] = cc
+
+        return res
+
+def _fixup_cc_list(cc_value):
+    """Fix up cc list separators and remove duplicates."""
+    # Copied from trac.ticket.model
+    cclist = []
+    for cc in re.split(r'[;,\s]+', cc_value):
+        if cc and cc not in cclist:
+            cclist.append(cc)
+    return ', '.join(cclist)
