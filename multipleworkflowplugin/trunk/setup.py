8554112033666f49199fff78f6868678dd8149e0
Cinc-th <Cinc-th@7322e99d-02ea-0310-aa39-e9a107903beb>
2015-05-02 11:18:27 +0000

    Added description, homepage and maintainer information.
    git-svn-id: https://trac-hacks.org/svn@14564 7322e99d-02ea-0310-aa39-e9a107903beb

--- multipleworkflowplugin/trunk/setup.py
+++ multipleworkflowplugin/trunk/setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Copyright (C) 2009 ???
+# Copyright (C) 2009
 # All rights reserved.
 #
 # This software is licensed as described in the file COPYING, which
@@ -10,8 +10,12 @@ from setuptools import find_packages, setup
 
 setup(
     name='MultipleWorkflowPlugin',
+    description='Ticket workflows based on ticket type.',
     version='1.0',
+    url='http://trac-hacks.org/wiki/MultipleWorkflowPlugin',
     license="New BSD",
+    author='ermal',
+    maintainer='Cinc-th',
     packages=find_packages(exclude=['*.tests*']),
     entry_points="""
         [trac.plugins]
