5a319381f1e55c3ac70027f78ca47b587a6ade33
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2015-05-04 18:37:40 +0000

    0.12.2: Fixed regression in [14562]. Refs #12217, #12299.
    Only check that custom field doesn't match an existing field when creating the field, not when saving changes to the field.
    
    
    git-svn-id: https://trac-hacks.org/svn@14567 7322e99d-02ea-0310-aa39-e9a107903beb

--- customfieldadminplugin/0.11/customfieldadmin/api.py
+++ customfieldadminplugin/0.11/customfieldadmin/api.py
@@ -113,8 +113,8 @@ class CustomFields(Component):
         # (if modify it should already be deleted)
         if create and self.config.get('ticket-custom', cfield['name']):
             raise TracError(_("Can not create as field already exists."))
-        if [f for f in TicketSystem(self.env).fields
-              if f['name'] == cfield['name']]:
+        if create and [f for f in TicketSystem(self.env).fields
+                       if f['name'] == cfield['name']]:
                 raise TracError(_("Can't create a custom field with the "
                                   "same name as a built-in field."))
 
