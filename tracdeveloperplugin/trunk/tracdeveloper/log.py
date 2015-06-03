f7639a7df549d7bed3c9211df71915985955c671
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2015-06-03 21:03:44 +0000

    0.3.0dev: Compatibility with Trac >= 1.1.2.
    * `DeveloperLogModule` was not functional for Trac >= 1.1.2.
    * Don't show log for `/chrome` request.
    * Don't show log for Ajax request.
    * Don't append `TracDeveloperHandler` unless user has `TRAC_DEVELOP` permission.
    
    Thanks to Jun Omae for help with the patch. Fixes #11905. 
    
    
    git-svn-id: https://trac-hacks.org/svn@14627 7322e99d-02ea-0310-aa39-e9a107903beb

--- tracdeveloperplugin/trunk/tracdeveloper/log.py
+++ tracdeveloperplugin/trunk/tracdeveloper/log.py
@@ -3,20 +3,19 @@
 import logging
 
 from trac.core import *
-from trac.web.api import IRequestHandler, ITemplateStreamFilter
-from trac.web.chrome import add_script
-from trac.util.datefmt import to_datetime
+from trac.web.api import IRequestFilter, ITemplateStreamFilter
+from trac.web.chrome import Chrome, add_script
 from genshi.core import END
 from genshi.builder import tag
 
 
 class TracDeveloperHandler(logging.Handler):
     """A custom logging handler to implement the TracDeveloper log console."""
-    
+
     def __init__(self):
         logging.Handler.__init__(self)
         self.buf = []
-    
+
     def emit(self, record):
         self.buf.append(record)
 
@@ -24,46 +23,47 @@ class TracDeveloperHandler(logging.Handler):
 class DeveloperLogModule(Component):
     """A plugin to display the Trac log."""
 
-    implements(IRequestHandler, ITemplateStreamFilter)
-    
-    def __init__(self):
-        # Force us to the front so we can override the default.
-        from trac.core import ComponentMeta
-        ComponentMeta._registry[IRequestHandler].remove(DeveloperLogModule)
-        ComponentMeta._registry[IRequestHandler].insert(0, DeveloperLogModule)
-    
-    # IRequestHandler methods
-    def match_request(self, req):
-        if not req.path_info.startswith('/chrome'):
-            hdlr = TracDeveloperHandler()
-            hdlr.setFormatter(self.log._trac_handler.formatter)
-            req._tracdeveloper_hdlr = hdlr
-            self.log.addHandler(hdlr)
-        return False
+    implements(IRequestFilter, ITemplateStreamFilter)
 
-    def process_request(self, req):
-        pass
+    def __init__(self):
+        self.log_handler = TracDeveloperHandler()
+        self.log_handler.setFormatter(self.log._trac_handler.formatter)
+        self.log.addHandler(self.log_handler)
+
+    # IRequestFilter methods
+
+    def pre_process_request(self, req, handler):
+        if not isinstance(handler, Chrome) and \
+                req.get_header('X-Requested-With') != 'XMLHttpRequest' and \
+                'TRAC_DEVELOP' in req.perm:
+            if self.log_handler not in self.log.handlers:
+                self.log.addHandler(self.log_handler)
+            req._tracdeveloper_hdlr = self.log_handler
+        return handler
+
+    def post_process_request(self, req, template, data, content_type):
+        return template, data, content_type
 
     # ITemplateStreamFilter methods
     def filter_stream(self, req, method, filename, stream, data):
         if not hasattr(req, '_tracdeveloper_hdlr'):
             return stream
-        
+
         if method != 'xhtml':
             req._tracdeveloper_hdlr.formatter = None
             del req._tracdeveloper_hdlr.buf[:]
             self.log.removeHandler(req._tracdeveloper_hdlr)
             del req._tracdeveloper_hdlr
             return stream
-        
+
         add_script(req, 'developer/js/log.js')
-        
+
         def fn(stream):
             for kind, data, pos in stream:
                 if kind is END and data.localname == 'body':
                     first_time = req._tracdeveloper_hdlr.buf \
                                  and req._tracdeveloper_hdlr.buf[0].created
-                    
+
                     elm = tag.div(tag.table(tag.thead(tag.tr(
                         tag.th('Time'),
                         tag.th('Module'),
