fc57219652a8cb0de9fc318c5da4f133b7d96e25
jun66j5 <jun66j5@7322e99d-02ea-0310-aa39-e9a107903beb>
2015-06-11 17:36:06 +0000

    TracMigratePlugin: uses `pkg_resources.require()` rather than `install_requires` in `setup.py`
    git-svn-id: https://trac-hacks.org/svn@14657 7322e99d-02ea-0310-aa39-e9a107903beb

--- tracmigrateplugin/0.12/setup.py
+++ tracmigrateplugin/0.12/setup.py
@@ -7,13 +7,13 @@ extra = {}
 
 setup(
     name = 'TracMigratePlugin',
-    version = '0.12.0.3',
+    version = '0.12.0.4',
     description = '',
     license = 'BSD', # the same as Trac
     url = 'http://trac-hacks.org/wiki/TracMigratePlugin',
     author = 'Jun Omae',
     author_email = 'jun66j5@gmail.com',
-    install_requires = ['Trac >= 0.12'],
+    install_requires = ['Trac'],
     packages = find_packages(exclude=['*.tests*']),
     test_suite = 'tracmigrate.tests.suite',
     entry_points = {
