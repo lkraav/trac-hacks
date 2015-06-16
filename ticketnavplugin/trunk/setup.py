b1621500eec0c93b220e2872857ea017db77bd39
framay <framay@7322e99d-02ea-0310-aa39-e9a107903beb>
2015-06-16 04:59:02 +0000

    fixes #12386: added license
    git-svn-id: https://trac-hacks.org/svn@14674 7322e99d-02ea-0310-aa39-e9a107903beb

--- ticketnavplugin/trunk/setup.py
+++ ticketnavplugin/trunk/setup.py
@@ -1,3 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2010-2015 Franz Mayer Gefasoft AG
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.
+
 from setuptools import find_packages, setup
 
 setup(
@@ -6,6 +15,7 @@ setup(
     author = 'Franz Mayer, Gefasoft AG',
     author_email = 'franz.mayer@gefasoft.de',
     description = 'Some small changes in ticket-view, like having a navigation frame, sorting milestone and version, etc.',
+	license = "BSD 3-Clause",
     url = 'http://www.gefasoft-muenchen.de',
     download_url = 'http://trac-hacks.org/wiki/TicketNavPlugin',
     packages=find_packages(exclude=['*.tests*']),
