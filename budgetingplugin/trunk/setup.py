ac9b4279fce95cafe0a2bcdab7e9e1e593cd7eba
framay <framay@7322e99d-02ea-0310-aa39-e9a107903beb>
2015-06-16 04:37:48 +0000

    fixes #12223: added license
    git-svn-id: https://trac-hacks.org/svn@14669 7322e99d-02ea-0310-aa39-e9a107903beb

--- budgetingplugin/trunk/setup.py
+++ budgetingplugin/trunk/setup.py
@@ -1,3 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2012-2015 Franz Mayer Gefasoft AG
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.
+
 from setuptools import find_packages, setup
 
 # name can be any name.  This name will be used to create the .egg file.
@@ -9,6 +18,7 @@ setup(
     author = 'Gefasoft AG, Franz Mayer',
     author_email = 'franz.mayer@gefasoft.de', 
     description = 'Possibility to add budgeting information (estimation, cost, user) to tickets',
+	license = "BSD 3-Clause",
     url = 'http://trac-hacks.org/wiki/BudgetingPlugin',
     download_url = 'https://trac-hacks.org/wiki/BudgetingPlugin',
     packages=find_packages(exclude=['*.tests*']),
--- budgetingplugin/trunk/ticketbudgeting/ticketbudgeting.py
+++ budgetingplugin/trunk/ticketbudgeting/ticketbudgeting.py
@@ -1,3 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2012-2015 Franz Mayer Gefasoft AG
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.
+
 from genshi import HTML
 from genshi.builder import tag
 from genshi.filters import Transformer
