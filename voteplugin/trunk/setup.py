58fa1fec5338196da9c25c12527d6bbfb3c63b76
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2015-06-26 03:55:46 +0000

    0.4.0dev: Enforce Trac >= 1.0.
    git-svn-id: https://trac-hacks.org/svn@14763 7322e99d-02ea-0310-aa39-e9a107903beb

--- voteplugin/trunk/setup.py
+++ voteplugin/trunk/setup.py
@@ -26,7 +26,7 @@ except ImportError:
 
 setup(
     name='TracVote',
-    version='0.3.0',
+    version='0.4.0',
     packages=find_packages(exclude=['*.tests']),
     package_data={
         'tracvote': [
@@ -44,7 +44,7 @@ setup(
 
     test_suite = 'tracvote.tests.suite',
     zip_safe=True,
-    install_requires = ['Trac >= 0.11'],
+    install_requires = ['Trac'],
     url='http://trac-hacks.org/wiki/VotePlugin',
     description='A plugin for voting on Trac resources.',
     entry_points = {'trac.plugins': ['tracvote = tracvote']},
