f877e1750d938867d2a9bf7c1ab7db3298a65b18
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2015-07-11 23:29:22 +0000

    Updates `djangoauth.py` from file previously attached to wiki page.
    git-svn-id: https://trac-hacks.org/svn@14791 7322e99d-02ea-0310-aa39-e9a107903beb

--- djangoauthintegration/0.9/djangoauth/djangoauth.py
+++ djangoauthintegration/0.9/djangoauth/djangoauth.py
@@ -20,9 +20,7 @@ class DjangoAuthPlugin(Component):
         if req.remote_user and (req.remote_user != "anonymous"):
             authname = req.remote_user
         elif req.incookie.has_key('sessionid'):
-            cookie = req.incookie['sessionid']
-            sid = cookie.OutputString()[10:-1]
-            authname = self._get_name_from_django(sid)
+            authname = self._get_name_from_django(req, req.incookie['sessionid'])
         else:
             authname = req.remote_user
 
@@ -45,34 +43,38 @@ class DjangoAuthPlugin(Component):
                     self.config.get('djangoauth', 'login_url', '#'))
 
     # private methods
-    def _get_name_from_django(self, sessionid):
+    def _get_name_from_django(self, req, sessionid):
         settings = self.config.get('djangoauth', 'django_settings_module')
-        
         os.environ['DJANGO_SETTINGS_MODULE'] = settings
         from django.contrib.sessions.models import Session
         from django.contrib.auth.models import User
-        session = Session.objects.get(pk=sessionid)
+        #req.django_sess = 'value is '+sessionid.value
+        try:
+            session = Session.objects.get(pk=sessionid.value)
+        except Session.DoesNotExist:
+            pass
+        else:
      
-        # Check for stale session
-        if session.expire_date > datetime.datetime.now():
-            data = session.get_decoded()
+            # Check for stale session
+            if session.expire_date > datetime.datetime.now():
+                data = session.get_decoded()
 
-            if data.has_key('_auth_user_id'):
-                try:
-                    user = User.objects.get(pk=data['_auth_user_id'])
-                except User.DoesNotExist:
-                    return None
+                if data.has_key('_auth_user_id'):
+                    try:
+                        user = User.objects.get(pk=data['_auth_user_id'])
+                    except User.DoesNotExist:
+                        return None
 
-                # Check user perms
-                if user.is_active:
+                    # Check user perms
+                    if user.is_active:
 
-                    if self.config.getbool('djangoauth', 'use_django_perms'):
-                        # make user object available for perm checks later
-                        self.user = user
+                        if self.config.getbool('djangoauth', 'use_django_perms'):
+                            # make user object available for perm checks later
+                            self.user = user
 
-                    # Update session expire_date
-                    # session.expire_date = datetime.datetime.now()
-                    # session.save() # OperationalError: readonly db??
+                        # Update session expire_date
+                        # session.expire_date = datetime.datetime.now()
+                        # session.save() # OperationalError: readonly db??
 
                     return user.username
         return None
@@ -97,9 +99,14 @@ class DjangoAuthPlugin(Component):
         req.write(str(dir(req))+ '\n')
         req.write('incookie: ' + str(req.incookie) + '\n')
         req.write('outcookie: ' + str(req.outcookie) + '\n')
-        req.write('perm: ' + str((req.perm.permissions(),req.perm.perms)) + '\n')
+        try:
+            req.write('perm: ' + str((req.perm.permissions(),req.perm.perms)) + '\n')
+        except AttributeError:
+            pass
         req.write('remote_user: ' + str(req.remote_user) + '\n')
         req.write('server_name: ' + str(req.server_name) + '\n')
         req.write('session: ' + str(req.session) + '\n')
+        if req.django_sess:
+            req.write('django-sess: ' + str(req.django_sess) + '\n') 
         if req.authname:
             req.write('authname: ' + str(req.authname) + '\n') 
