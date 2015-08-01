bfcfc820fa5f4399f2572a7597920a7b0bcad16c
walnut <walnut@7322e99d-02ea-0310-aa39-e9a107903beb>
2015-08-01 19:33:09 +0000

    - update release version (2.5.3).
    git-svn-id: https://trac-hacks.org/svn@14853 7322e99d-02ea-0310-aa39-e9a107903beb

--- childticketsplugin/trunk/setup.py
+++ childticketsplugin/trunk/setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 # use package name as entry_points
 setup(
     name = 'Tracchildtickets',
-    version = 'dev',
+    version = '2.5.3',
     packages = find_packages(),
     author = 'Mark Ryan',
     author_email = 'fatrascal@bigfoot.com@gmail.com',
