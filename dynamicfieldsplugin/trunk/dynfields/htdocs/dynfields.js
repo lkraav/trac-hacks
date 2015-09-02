0af3647f0779a6a3fbf8003fa427bae460143a97
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2015-09-02 17:58:34 +0000

    2.2.0dev: Move functions inside document ready handler. Refs #9606.
    No performance change is expected.
    
    git-svn-id: https://trac-hacks.org/svn@14896 7322e99d-02ea-0310-aa39-e9a107903beb

--- dynamicfieldsplugin/trunk/dynfields/htdocs/dynfields.js
+++ dynamicfieldsplugin/trunk/dynfields/htdocs/dynfields.js
@@ -1,74 +1,73 @@
-var get_selector = function(field_name){
-    var selector = '#field-'+field_name;
-    if (field_name == 'owner'){
-        if (jQuery(selector).length != 1){
-            if (jQuery('#action_assign_reassign_owner').length){
-                selector = '#action_assign_reassign_owner';
-            } else {
-                selector = '#action_reassign_reassign_owner';
+jQuery(document).ready(function($) {
+
+    var get_selector = function(field_name) {
+        var selector = '#field-' + field_name;
+        if (field_name == 'owner'){
+            if ($(selector).length != 1) {
+                if ($('#action_assign_reassign_owner').length) {
+                    selector = '#action_assign_reassign_owner';
+                } else {
+                    selector = '#action_reassign_reassign_owner';
+                }
+            }
+        } else if (field_name == 'resolution') {
+            if ($(selector).length != 1){
+                selector = '#action_resolve_resolve_resolution';
             }
         }
-    } else if (field_name == 'resolution'){
-        if (jQuery(selector).length != 1){
-            selector = '#action_resolve_resolve_resolution';
+        return selector;
+    };
+
+    var apply_rules = function(){
+        var input = $(this);
+        setup_triggers();
+
+        // execute the rule lifecycle...
+
+        // setup each rule
+        $.each(triggers, function(trigger, specs) {
+            $.each(specs, function(i, spec) {
+                spec.rule.setup(input, spec);
+            });
+        });
+
+        // apply each rule
+        $.each(triggers, function(trigger, specs) {
+            $.each(specs, function(i, spec){
+                if (input.attr('id') != specs.selector.slice(1))
+                    return;
+                spec.rule.apply(input, spec);
+            });
+        });
+
+        // complete each rule
+        $.each(triggers, function(trigger, specs) {
+            $.each(specs, function(i, spec){
+                if (input.attr('id') != specs.selector.slice(1))
+                    return;
+                spec.rule.complete(input, spec);
+            });
+        });
+    };
+
+    var setup_triggers = function() {
+        if (window.dynfields_rules == undefined)
+            window.dynfields_rules = {};
+
+        for (var prop in triggers) {
+            triggers[prop].selector = get_selector(prop);
+            triggers[prop].forEach(function(spec) {
+                spec.rule = window.dynfields_rules[spec.rule_name];
+            });
         }
-    }
-    return selector;
-};
-
-var setup_triggers = function(){
-    if (window.dynfields_rules == undefined)
-        window.dynfields_rules = {};
-
-    for (var prop in triggers) {
-        triggers[prop].selector = get_selector(prop);
-        for (var i in triggers[prop]) {
-            var spec = triggers[prop][i];
-            spec.rule = window.dynfields_rules[spec.rule_name];
-        }
-    }
-};
-
-var apply_rules = function(){
-    var input = $(this);
-    setup_triggers();
-
-    // execute the rule lifecycle...
-
-    // setup each rule
-    jQuery.each(triggers, function(trigger,specs){
-        jQuery.each(specs, function(i,spec){
-            spec.rule.setup(input, spec);
-        });
-    });
-
-    // apply each rule
-    jQuery.each(triggers, function(trigger,specs){
-        jQuery.each(specs, function(i,spec){
-            if (input.attr('id') != specs.selector.slice(1))
-                return;
-            spec.rule.apply(input, spec);
-        });
-    });
-
-    // complete each rule
-    jQuery.each(triggers, function(trigger,specs){
-        jQuery.each(specs, function(i,spec){
-            if (input.attr('id') != specs.selector.slice(1))
-                return;
-            spec.rule.complete(input, spec);
-        });
-    });
-};
-
-jQuery(document).ready(function($){
+    };
 
     setup_triggers(); // trigger fields
 
-    if (window.location.pathname.match(/\/query$/)){
+    if (window.location.pathname.match(/\/query$/)) {
         // hide all "hide_always" fields
-        jQuery.each(triggers, function(trigger,specs){
-            jQuery.each(specs, function(i,spec){
+        $.each(triggers, function(trigger,specs) {
+            $.each(specs, function(i,spec) {
                 spec.rule.query(spec);
             });
         });
@@ -76,14 +75,14 @@ jQuery(document).ready(function($){
         var inputs = [];
 
         // collect all input fields that trigger rules
-        jQuery.each(triggers, function(trigger,specs){
-            var input = jQuery(specs.selector).get(0);
+        $.each(triggers, function(trigger, specs) {
+            var input = $(specs.selector).get(0);
             inputs.push(input);
         });
-        inputs = jQuery.unique(inputs);
+        inputs = $.unique(inputs);
 
         // attach change event to each input and trigger first change
-        jQuery.each(inputs, function(obj){
+        $.each(inputs, function(obj) {
             $(this).change(apply_rules).change();
         });
 
