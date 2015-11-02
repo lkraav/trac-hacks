7ac271a7a10223dca0788619ce73c3c09d007418
Cinc-th <Cinc-th@7322e99d-02ea-0310-aa39-e9a107903beb>
2015-11-02 09:37:22 +0000

    Bump version and mark as development release.
    git-svn-id: https://trac-hacks.org/svn@15019 7322e99d-02ea-0310-aa39-e9a107903beb

--- simplemultiprojectplugin/trunk/setup.py
+++ simplemultiprojectplugin/trunk/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='SimpleMultiProject',
-    version='0.5.0',
+    version='0.5.1',
     packages=['simplemultiproject'],
     package_data={
         'simplemultiproject': [
