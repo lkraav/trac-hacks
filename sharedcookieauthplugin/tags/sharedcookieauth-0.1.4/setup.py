7036de5b85747fceeaa94897a5f630c050ac5215
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2015-11-27 05:28:06 +0000

    0.1.4: Tag version 0.1.4
    Refs #5566.
    
    
    git-svn-id: https://trac-hacks.org/svn@15071 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ sharedcookieauthplugin/tags/sharedcookieauth-0.1.4/setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+
+version='0.1.4'
+
+setup(name='SharedCookieAuth',
+      version=version,
+      description="share cookies between trac projects in the same directory",
+      author='Jeff Hammel',
+      author_email='jhammel@openplans.org',
+      maintainer='Lars Wireen',
+      maintainer_email='lw@agitronic.se',
+      url='http://trac-hacks.org/wiki/SharedCookieAuthPlugin',
+      keywords='trac plugin',
+      license="GPL",
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests*']),
+      include_package_data=True,
+      package_data={ 'sharedcookieauth': ['templates/*', 'htdocs/*'] },
+      zip_safe=False,
+      entry_points = """
+      [trac.plugins]
+      sharedcookieauth = sharedcookieauth
+      """,
+      )
+
