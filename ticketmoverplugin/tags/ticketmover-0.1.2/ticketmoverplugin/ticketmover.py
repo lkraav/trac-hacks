edf926762209a00afae1f54ac08d7d43e69f2906
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2015-12-10 22:43:32 +0000

    0.1.2: Tag version 0.1.2
    This version should be used for Trac < 1.0.
    
    Refs #11114, #12608.
    
    
    git-svn-id: https://trac-hacks.org/svn@15108 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ ticketmoverplugin/tags/ticketmover-0.1.2/ticketmoverplugin/ticketmover.py
@@ -0,0 +1,111 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2009 Jeff Hammel <jhammel@openplans.org>
+# Copyright (C) 2013 Ryan J Ollos <ryan.j.ollos@gmail.com>
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.
+#
+
+import os
+import shutil
+
+from trac.config import Option
+from trac.core import Component
+from trac.env import open_environment
+from trac.perm import PermissionSystem
+from trac.ticket import Ticket
+
+from tracsqlhelper import get_all_dict
+from tracsqlhelper import insert_row_from_dict
+
+
+class TicketMover(Component):
+
+    permission = Option('ticket', 'move_permission', 'TICKET_ADMIN',
+                        """permission needed to move tickets between
+                           Trac projects""")
+
+    ### Internal methods
+
+    def projects(self, user):
+        base_path, _project = os.path.split(self.env.path)
+        _projects = [p for p in os.listdir(base_path)
+                     if p != _project]
+        projects = {}
+        for project in _projects:
+            path = os.path.join(base_path, project)
+            try:
+                env = open_environment(path, use_cache=True)
+            except:
+                continue
+            perm = PermissionSystem(env)
+            if self.permission in perm.get_user_permissions(user):
+                projects[project] = env
+
+        return projects
+        
+    def move(self, ticket_id, author, env, delete=False):
+        """
+        move a ticket to another environment
+        
+        env: environment to move to
+        """
+        tables = {'attachment': 'id',
+                  'ticket_change': 'ticket'}
+
+        # open the environment if it is a string
+        if isinstance(env, basestring):
+            base_path, _project = os.path.split(self.env.path)
+            env = open_environment(os.path.join(base_path, env),
+                                   use_cache=True)
+
+        # get the old ticket
+        old_ticket = Ticket(self.env, ticket_id)
+
+        # make a new ticket from the old ticket values
+        new_ticket = Ticket(env)
+        new_ticket.values = old_ticket.values.copy()
+        new_ticket.insert(when=old_ticket.time_created)
+
+        # copy the changelog and attachment DBs
+        for table, _id in tables.items():
+            for row in get_all_dict(self.env,
+                                    "SELECT * FROM %s WHERE %s=%%s"
+                                    % (table, _id), str(ticket_id)):
+                row[_id] = new_ticket.id
+                insert_row_from_dict(env, table, row)
+
+        # copy the attachments
+        src_attachment_dir = os.path.join(self.env.path, 'attachments',
+                                          'ticket', str(ticket_id))
+        if os.path.exists(src_attachment_dir):
+            dest_attachment_dir = os.path.join(env.path, 'attachments',
+                                               'ticket')
+            if not os.path.exists(dest_attachment_dir):
+                os.makedirs(dest_attachment_dir)
+            dest_attachment_dir = os.path.join(dest_attachment_dir,
+                                               str(new_ticket.id))
+            shutil.copytree(src_attachment_dir, dest_attachment_dir)
+
+        # note the previous location on the new ticket
+        new_ticket.save_changes(author, 'moved from %s'
+                                        % self.env.abs_href('ticket',
+                                                            ticket_id))
+
+        # location of new ticket
+        new_location = env.abs_href.ticket(new_ticket.id)
+
+        if delete:
+            old_ticket.delete()
+        else:
+            # close old ticket and point to new one
+            old_ticket['status'] = u'closed'
+            old_ticket['resolution'] = u'moved'
+            old_ticket.save_changes(author, u'moved to %s' % new_location)
+
+        if env.config['trac'].get('base_url'):
+            return new_location
+        else:
+            return None
---
+++ ticketmoverplugin/tags/ticketmover-0.1.2/ticketmoverplugin/web_ui.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2009 Jeff Hammel <jhammel@openplans.org>
+# Copyright (C) 2013 Ryan J Ollos <ryan.j.ollos@gmail.com>
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.
+#
+
+from trac.core import Component, TracError, implements
+from trac.util.translation import _
+from trac.web.api import IRequestHandler
+from trac.web.chrome import Chrome, ITemplateProvider
+
+from ticketmoverplugin.ticketmover import TicketMover
+from ticketsidebarprovider import ITicketSidebarProvider
+
+
+class TicketMoverSidebar(Component):
+
+    implements(ITicketSidebarProvider, ITemplateProvider)
+
+    ### ITicketSidebarProvider methods
+
+    def enabled(self, req, ticket):
+        if not self.config['ticket'].get('move_permission') in req.perm or \
+                not ticket.exists:
+            return False
+        tm = TicketMover(self.env)
+        projects = tm.projects(req.authname)
+        self.log.debug(_("TicketMover SidebarProvider is %(status)s.",
+                         status=['enabled', 'disabled'][bool(projects)]))
+        return bool(projects)
+
+    def content(self, req, ticket):
+        tm = TicketMover(self.env)
+        projects = tm.projects(req.authname)
+        chrome = Chrome(self.env)
+        template = chrome.load_template('ticketmover-sidebar.html')
+        data = {'projects': projects,
+                'req': req,
+                'ticket': ticket}
+        return template.generate(**data)
+
+    ### ITemplateProvider methods
+
+    def get_htdocs_dirs(self):
+        return []
+
+    def get_templates_dirs(self):
+        from pkg_resources import resource_filename
+        return [resource_filename(__name__, 'templates')]
+
+
+class TicketMoverHandler(Component):
+
+    implements(IRequestHandler)
+
+    ### IRequestHandler methods
+
+    def match_request(self, req):
+        return req.method == 'POST' and \
+                    req.path_info.rstrip('/') == '/ticket/move'
+
+    def process_request(self, req):
+        req.perm.require(self.config['ticket'].get('move_permission'))
+        project = req.args['project']
+
+        tm = TicketMover(self.env)
+        new_location = tm.move(req.args['ticket'], req.authname,
+                               project, 'delete' in req.args)
+
+        if 'delete' in req.args:
+            if new_location:
+                req.redirect(new_location)
+            else:
+                raise TracError(_("Can't redirect to project %(project)s "
+                                  "after moving ticket because \"base_url\" "
+                                  "is not set for that project.",
+                                  project=project))
+        req.redirect(req.href.ticket(req.args['ticket']))
