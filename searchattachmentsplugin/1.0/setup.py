6dd7a024066aaffa2ab9d313b6fd8c17b5fe06b5
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2016-01-26 18:12:25 +0000

    0.2dev: Fix indentation using `reindent.py`
    git-svn-id: https://trac-hacks.org/svn@15177 7322e99d-02ea-0310-aa39-e9a107903beb

--- searchattachmentsplugin/1.0/setup.py
+++ searchattachmentsplugin/1.0/setup.py
@@ -4,17 +4,16 @@
 from setuptools import setup
 
 PACKAGE = 'TracSearchAttachmentsPlugin'
-VERSION = '0.1'
+VERSION = '0.2'
 
-setup(	name=PACKAGE,
-      	author='Yannick Deltroo',
-	author_email='deltroo@gmail.com',
-	license='BSD',
-	url='http://trac-hacks.org/wiki/SearchAttachmentsPlugin',
-	version=VERSION,
-	description='Text integral search in trac attachments',
-	long_description="""This trac plugin uses external tools to index and search attachments""",
-	packages=['searchattachments'],
-	entry_points={'trac.plugins': '%s = searchattachments' % PACKAGE},
+setup(  name=PACKAGE,
+        author='Yannick Deltroo',
+        author_email='deltroo@gmail.com',
+        license='BSD',
+        url='http://trac-hacks.org/wiki/SearchAttachmentsPlugin',
+        version=VERSION,
+        description='Text integral search in trac attachments',
+        long_description="""This trac plugin uses external tools to index and search attachments""",
+        packages=['searchattachments'],
+        entry_points={'trac.plugins': '%s = searchattachments' % PACKAGE},
 )
-
