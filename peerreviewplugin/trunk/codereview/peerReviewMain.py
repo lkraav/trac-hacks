2e7dc4791d7e8ed8f0fa23046e8a30cb4f089605
Cinc-th <Cinc-th@7322e99d-02ea-0310-aa39-e9a107903beb>
2016-02-07 09:44:44 +0000

    Improvements to timeline event rendering.
    git-svn-id: https://trac-hacks.org/svn@15230 7322e99d-02ea-0310-aa39-e9a107903beb

--- peerreviewplugin/trunk/codereview/peerReviewMain.py
+++ peerreviewplugin/trunk/codereview/peerReviewMain.py
@@ -1,5 +1,7 @@
 #
 # Copyright (C) 2005-2006 Team5
+# Copyright (C) 2016 Cinc-th
+#
 # All rights reserved.
 #
 # This software is licensed as described in the file COPYING.txt, which
@@ -7,13 +9,13 @@
 #
 # Author: Team5
 #
-
 # Provides functionality for main page
 # Works with peerReviewMain.html
 
 import itertools
 
 from trac.core import Component, implements
+from trac.wiki.formatter import format_to_html
 from trac.perm import IPermissionRequestor
 from trac.resource import *
 from trac.timeline.api import ITimelineEventProvider
@@ -34,24 +36,27 @@ def add_ctxt_nav_items(req):
     add_ctxtnav(req, _("Search Code Reviews"), "peerReviewSearch", _("Search Code Reviews"))
 
 
-class MainReviewModule(Component):
+class PeerReviewReviewMain(Component):
     implements(INavigationContributor, IRequestHandler, ITemplateProvider,
                IPermissionRequestor, ITimelineEventProvider)
-        
+
     # INavigationContributor methods
+
     def get_active_navigation_item(self, req):
         return 'peerReviewMain'
-                
+
     def get_navigation_items(self, req):
         if 'CODE_REVIEW_DEV' in req.perm:
             yield ('mainnav', 'peerReviewMain',
                    Markup('<a href="%s">Peer Review</a>') % req.href.peerReviewMain())
 
     # IPermissionRequestor methods
+
     def get_permission_actions(self):
         return ['CODE_REVIEW_DEV', ('CODE_REVIEW_MGR', ['CODE_REVIEW_DEV'])]
 
     # IRequestHandler methods
+
     def match_request(self, req):
         if 'CODE_REVIEW_DEV' in req.perm:
             return req.path_info == '/peerReviewMain'
@@ -66,9 +71,6 @@ class MainReviewModule(Component):
             data['manager'] = True
         else:
             data['manager'] = False
-
-        db = self.env.get_read_db()
-        dbBack = dbBackend(db)
 
         all_reviews = [rev for rev in Review.select(self.env) if rev.status != "Closed"]
         rev_by_reviewer = [rev for rev in Review.select_by_reviewer(self.env, req.authname) if rev.status != "Closed"]
@@ -108,6 +110,7 @@ class MainReviewModule(Component):
         return 'peerReviewMain.html', data, None
 
     # ITimelineEventProvider methods
+
     def get_timeline_filters(self, req):
         if 'CODE_REVIEW_DEV' in req.perm:
             yield ('codereview', 'Code Reviews')
@@ -119,15 +122,19 @@ class MainReviewModule(Component):
             db = self.env.get_db_cnx()
             dbBack = dbBackend(db)
             codeReviewDetails = dbBack.getCodeReviewsInPeriod(to_timestamp(start), to_timestamp(stop))
+            add_stylesheet(req, 'hw/css/peerreview.css')
 
             for codeReview in codeReviewDetails:
                 codereview_page = codereview_realm(id=codeReview.IDReview)
                 reviewers = Reviewer.select_by_review_id(self.env, codeReview.IDReview)
 
                 reviewersList = ''
-                for reviewer in reviewers:
-                    reviewersList = reviewersList + reviewer.reviewer + ','
-           
+                last = len(reviewers) - 1
+                for idx, reviewer in enumerate(reviewers):
+                    reviewersList = reviewersList + reviewer.reviewer
+                    if idx != last:
+                        reviewersList += ', '
+
                 yield('codereview', codeReview.DateCreate, codeReview.Author,
                       (codereview_page, codeReview.Name, codeReview.Notes,
                        reviewersList))
@@ -140,18 +147,20 @@ class MainReviewModule(Component):
         if field == 'title':
             return tag('Code review ', tag.em(name), ' has been raised')
         if field == 'description':
-            return tag('Assigned to: ', reviewersList, tag.br(), ' Additional notes: ', notes)
+            return tag('Assigned to: ', reviewersList,
+                       tag.div(' Additional notes: '),
+                       tag.div(
+                           format_to_html(self.env, context, notes),
+                           class_='notes'
+                       ))
 
     # ITemplateProvider methods
+
     def get_templates_dirs(self):
-        """
-        Return the absolute path of the directory containing the provided
-        ClearSilver templates.
-        """
+        """Return the path of the directory containing the provided templates."""
         from pkg_resources import resource_filename
         return [resource_filename(__name__, 'templates')]
 
-    # Needed to be filled out based on interface
     def get_htdocs_dirs(self):
         from pkg_resources import resource_filename
         return [('hw', resource_filename(__name__, 'htdocs'))]
