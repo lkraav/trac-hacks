ddb34bfba3a903efe1127148b504e9380b4bda23
Cinc-th <Cinc-th@7322e99d-02ea-0310-aa39-e9a107903beb>
2016-02-09 09:30:30 +0000

    Javascript code refactoring.
    git-svn-id: https://trac-hacks.org/svn@15245 7322e99d-02ea-0310-aa39-e9a107903beb

--- peerreviewplugin/trunk/codereview/htdocs/js/peer_review_perform.js
+++ peerreviewplugin/trunk/codereview/htdocs/js/peer_review_perform.js
@@ -1,39 +1,31 @@
 
 jQuery(document).ready(function($) {
 
-  function append_img(id){
-     var tline = '<img src="chrome/hw/images/thumbtac11x11.gif" />';
-     $(id).append(tline);
-  };
-
-  for(var i=0; i < peer_comments.length; i++) {
-     var id = '#L'+peer_comments[i];
-     var tline = '<a href="javascript:getComments('+peer_comments[i]+','+peer_file_id+')">'+peer_comments[i]+'</a>'
-     $(id).empty();
-     append_img(id);
-     $(id).append(tline);
-  }
-    for(var i=0; i < peer_parent_comments.length; i++) {
-       var id = '#P'+peer_parent_comments[i];
-       var tline = '<a href="javascript:getComments('+peer_parent_comments[i]+
-       ','+peer_parent_file_id+')">'+peer_parent_comments[i]+'</a>'
-       $(id).empty();
-       append_img(id);
-       $(id).append(tline);
-    }
-
     function create_comment_link(LineNum, fileID){
-       return $('<a>', {href: 'javascript:getComments(' + LineNum + ',' + fileID+ ')',
-                        text: LineNum}).prepend($('<img>', {src: tacUrl}));
+        return $('<a>', {href: 'javascript:getComments(' + LineNum + ',' + fileID+ ')',
+                         text: LineNum}).prepend($('<img>', {src: tacUrl}));
     };
 
-    $( "#add-comment-dlg" ).dialog({
+    function add_get_comments_link(prefix, line, fileid){
+        $(prefix+line).empty();
+        $(prefix+line).append(create_comment_link(line, fileid));
+    }
+
+   for(var i=0; i < peer_comments.length; i++) {
+       add_get_comments_link('#L', peer_comments[i], peer_file_id)
+   }
+
+   for(var i=0; i < peer_parent_comments.length; i++) {
+       add_get_comments_link('#P', peer_parent_comments[i], peer_parent_file_id)
+   }
+
+   $( "#add-comment-dlg" ).dialog({
       title: "Add Comment",
       width: 500,
       autoOpen: false,
       resize: false,
       icons: { primary: "ui-icon-locked"},
-    });
+   });
 
     $('#addcomment').on('click', function(event){
        event.preventDefault ? event.preventDefault() : event.returnValue = false;
@@ -46,11 +38,7 @@ jQuery(document).ready(function($) {
        });
 
        $( "#add-comment-dlg" ).dialog('close');
-       /* Open comment link */
-       $('#L'+LineNum).empty();
-       $('#L'+LineNum).append(create_comment_link(LineNum, $("#comment-fileid" ).val()));
-
+       /* Add open comment link */
+       add_get_comments_link('#L', LineNum, $("#comment-fileid" ).val())
     });
-
-
 });
