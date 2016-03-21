2602ad8b4347a81211618caa253eb114dcaaad48
jun66j5 <jun66j5@7322e99d-02ea-0310-aa39-e9a107903beb>
2016-03-21 16:26:26 +0000

    TracMigratePlugin: bump up the version and call `setuptools.setup()` only if run `setup.py` as a script
    git-svn-id: https://trac-hacks.org/svn@15444 7322e99d-02ea-0310-aa39-e9a107903beb

--- tracmigrateplugin/0.12/setup.py
+++ tracmigrateplugin/0.12/setup.py
@@ -1,24 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from setuptools import setup, find_packages
+def main():
+    from setuptools import setup, find_packages
 
-extra = {}
+    extra = {}
 
-setup(
-    name = 'TracMigratePlugin',
-    version = '0.12.0.4',
-    description = '',
-    license = 'BSD', # the same as Trac
-    url = 'http://trac-hacks.org/wiki/TracMigratePlugin',
-    author = 'Jun Omae',
-    author_email = 'jun66j5@gmail.com',
-    install_requires = ['Trac'],
-    packages = find_packages(exclude=['*.tests*']),
-    test_suite = 'tracmigrate.tests.suite',
-    entry_points = {
-        'trac.plugins': [
-            'tracmigrate.admin = tracmigrate.admin',
-        ],
-    },
-    **extra)
+    setup(
+        name = 'TracMigratePlugin',
+        version = '0.12.0.5',
+        description = '',
+        license = 'BSD', # the same as Trac
+        url = 'http://trac-hacks.org/wiki/TracMigratePlugin',
+        author = 'Jun Omae',
+        author_email = 'jun66j5@gmail.com',
+        install_requires = ['Trac'],
+        packages = find_packages(exclude=['*.tests*']),
+        test_suite = 'tracmigrate.tests.suite',
+        entry_points = {
+            'trac.plugins': [
+                'tracmigrate.admin = tracmigrate.admin',
+            ],
+        },
+        **extra)
+
+
+if __name__ == '__main__':
+    main()
