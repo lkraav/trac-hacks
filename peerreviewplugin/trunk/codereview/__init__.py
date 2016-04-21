1e694c3fb8b993ff41eb7c74f5bc9d474dc08b94
Cinc-th <Cinc-th@7322e99d-02ea-0310-aa39-e9a107903beb>
2016-04-21 11:01:22 +0000

    PeerReviewPlugin: remove file list page obsoleted by [15496]. Use a report for listing files instead.
    git-svn-id: https://trac-hacks.org/svn@15497 7322e99d-02ea-0310-aa39-e9a107903beb

--- peerreviewplugin/trunk/codereview/__init__.py
+++ peerreviewplugin/trunk/codereview/__init__.py
@@ -1,11 +1,10 @@
 #
 # Copyright (C) 2005-2006 Team5
+# Copyright (C) 2016 Cinc
 # All rights reserved.
 #
 # This software is licensed as described in the file COPYING.txt, which
 # you should have received as part of this distribution.
-#
-# Author: Team5
 #
 
 import peerReviewMain
@@ -17,6 +16,5 @@ import peerReviewView
 import peerReviewCommentCallback
 from admin import PeerReviewFileAdmin
 from peerreview_docx import PeerReviewDocx
-from peerreviewfile import PeerReviewFile
 from report import PeerReviewReport
 from timeline import PeerReviewTimeline
