9b40f6cd130a0fe00fdfd96b7759806d1582c2a4
tkob-trac <tkob-trac@7322e99d-02ea-0310-aa39-e9a107903beb>
2016-06-25 14:51:11 +0000

    Add license indication, close #12801
    git-svn-id: https://trac-hacks.org/svn@15608 7322e99d-02ea-0310-aa39-e9a107903beb

--- citecodemacro/1.0/setup.py
+++ citecodemacro/1.0/setup.py
@@ -1,9 +1,18 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2016 tkob <ether4@gmail.com>
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.
+
 from setuptools import find_packages, setup
 
 setup(
     name = 'TracCiteCode',
-    version = '0.1.0',
+    version = '0.2.0',
     packages = find_packages(exclude=['*.tests*']),
+    license = "BSD 3-Clause",
     entry_points = {
         'trac.plugins': [
             'traccitecode = traccitecode',
--- citecodemacro/1.0/traccitecode/citecode.py
+++ citecodemacro/1.0/traccitecode/citecode.py
@@ -1,4 +1,10 @@
 # -*- coding: utf-8 -*-
+#
+# Copyright (C) 2016 tkob <ether4@gmail.com>
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.
 
 import re
 import urlparse
