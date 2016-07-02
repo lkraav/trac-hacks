3ab330b9c556057316ce88a016a2f30b32d52563
tkob-trac <tkob-trac@7322e99d-02ea-0310-aa39-e9a107903beb>
2016-07-02 04:38:12 +0000

    Annotate code with line numbers
    git-svn-id: https://trac-hacks.org/svn@15613 7322e99d-02ea-0310-aa39-e9a107903beb

--- citecodemacro/1.0/traccitecode/citecode.py
+++ citecodemacro/1.0/traccitecode/citecode.py
@@ -15,7 +15,7 @@ from genshi.core import escape
 from trac.core import Component, implements
 from trac.mimeview import Mimeview, Context
 from trac.mimeview.api import is_binary
-from trac.web.chrome import add_script, add_script_data, ITemplateProvider, Chrome
+from trac.web.chrome import add_script, add_script_data, add_stylesheet, ITemplateProvider, Chrome
 from trac.web.main import IRequestFilter, IRequestHandler
 from trac.wiki.macros import WikiMacroBase
 from trac.versioncontrol import RepositoryManager
@@ -27,6 +27,9 @@ class CiteCodeMacro(WikiMacroBase):
 
     def expand_macro(self, formatter, name, content):
         self.log.debug("content=%s" % content)
+
+        add_stylesheet(formatter.req, 'citecode/citecode.css')
+
         # parse the argument
         scheme, netloc, path, params, query, fragment = urlparse.urlparse(content)
         self.log.debug("scheme=%s, netloc=%s, path=%s, params=%s, query=%s, fragment=%s"
@@ -58,11 +61,14 @@ class CiteCodeMacro(WikiMacroBase):
                         end = end or start
                         lines = content.splitlines()[int(start)-1:int(end)]
                         content = "\n".join(lines)
+                        context.set_hints(lineno = int(start))
                 xhtml = self.mimeview.render(
                         context = context,
                         mimetype = content_type,
                         content = content,
-                        filename = "")
+                        filename = "",
+                        annotations=['citecode_lineno'],
+                        )
                 return xhtml
         finally:
             repo.close()
@@ -129,3 +135,56 @@ class CiteCodeAndCreateTicket(Chrome):
 """ % (path, at_rev, line, path, query, line),
                 preview = True,
         ))
+
+from trac.mimeview.api import IHTMLPreviewAnnotator
+from trac.util.translation import _
+from trac.util import Ranges
+from genshi.builder import tag
+
+# Just copied from trunk of Trac core and renamed annotation_type
+# since 'lineno' hint is not available in Trac 1.0
+
+# Copyright (C) 2004-2014 Edgewall Software
+# Copyright (C) 2004 Daniel Lundin <daniel@edgewall.com>
+# Copyright (C) 2005-2006 Christopher Lenz <cmlenz@gmx.de>
+# Copyright (C) 2006-2007 Christian Boos <cboos@edgewall.org>
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution. The terms
+# are also available at http://trac.edgewall.org/wiki/TracLicense.
+#
+# This software consists of voluntary contributions made by many
+# individuals. For the exact contribution history, see the revision
+# history and logs, available at http://trac.edgewall.org/log/.
+#
+# Author: Daniel Lundin <daniel@edgewall.com>
+#         Christopher Lenz <cmlenz@gmx.de>
+#         Christian Boos <cboos@edgewall.org>
+
+class LineNumberAnnotator(Component):
+    """Text annotator that adds a column with line numbers."""
+    implements(IHTMLPreviewAnnotator)
+
+    # IHTMLPreviewAnnotator methods
+
+    def get_annotation_type(self):
+        return 'citecode_lineno', _('Line'), _('Line numbers')
+
+    def get_annotation_data(self, context):
+        try:
+            marks = Ranges(context.get_hint('marks'))
+        except ValueError:
+            marks = None
+        return {
+            'id': context.get_hint('id', '') + 'L%s',
+            'marks': marks,
+            'offset': context.get_hint('lineno', 1) - 1
+        }
+
+    def annotate_row(self, context, row, lineno, line, data):
+        lineno += data['offset']
+        id = data['id'] % lineno
+        if data['marks'] and lineno in data['marks']:
+            row(class_='hilite')
+        row.append(tag.th(id=id)(tag.a(lineno, href='#' + id)))
