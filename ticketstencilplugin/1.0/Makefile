e65bfe072f6e7ee2d98be6fb8401fbec936c8007
tkob-trac <tkob-trac@7322e99d-02ea-0310-aa39-e9a107903beb>
2016-07-18 05:34:26 +0000

    Initial commit
    git-svn-id: https://trac-hacks.org/svn@15666 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ ticketstencilplugin/1.0/Makefile
@@ -0,0 +1,6 @@
+all:
+	python setup.py bdist_egg
+
+clean:
+	rm -rf build dist *.egg-info
+.PHONY: clean
---
+++ ticketstencilplugin/1.0/tracticketstencil/ticketstencil.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2016 tkob <ether4@gmail.com>
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.
+
+from trac.core import Component, implements
+from trac.web.chrome import add_script, add_script_data, ITemplateProvider
+from trac.web.main import IRequestFilter
+from trac.wiki.api import WikiSystem
+from trac.wiki.model import WikiPage
+
+class TicketStencil(Component):
+    implements(IRequestFilter, ITemplateProvider)
+        
+    def __init__(self):
+        self.wiki_system = WikiSystem(self.env)
+
+    # ITemplateProvider methods
+
+    def get_htdocs_dirs(self):
+        from pkg_resources import resource_filename 
+        return [('ticketstencil', resource_filename(__name__, 'htdocs'))]
+
+    def get_templates_dirs(self):
+        return []
+
+    # IRequestFilter methods
+
+    def pre_process_request(self, req, handler):
+        return handler 
+
+    def post_process_request(self, req, template, data, content_type):
+        if not req.path_info.startswith('/newticket'):
+            return template, data, content_type
+        add_script(req, 'ticketstencil/ticketstencil.js')
+        stencils = {}
+        prefix_len = len('TicketStencil')
+        for name in self.wiki_system.get_pages('TicketStencil'):
+            page = WikiPage(env = self.env, name = name)
+            ticket_type = name[prefix_len:].lower()
+            stencils[ticket_type] = page.text
+        add_script_data(req, {'_tracticketstencil': stencils})
+        return template, data, content_type
