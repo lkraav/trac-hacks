0091656ca10551bc26083e36b6efdd04547be3f3
tkob-trac <tkob-trac@7322e99d-02ea-0310-aa39-e9a107903beb>
2016-08-19 13:56:11 +0000

    Add Mermaid live editor
    git-svn-id: https://trac-hacks.org/svn@15714 7322e99d-02ea-0310-aa39-e9a107903beb

--- mermaidmacro/1.0/tracmermaid/htdocs/tracmermaid.js
+++ mermaidmacro/1.0/tracmermaid/htdocs/tracmermaid.js
@@ -1,1 +1,69 @@
 mermaid.init();
+
+var tracMermaidDialog = $(
+    "<div title='" + "preview" + "'>" +
+    "<input id='mermaidsave' name='mermaidsave' value='Save' type='submit'></input>" +
+    "<div style='display:table; height:100%; width:100%;'>" +
+    "<textarea class='ui-corner-all' id='mermaidsource' cols='40' rows='20' style='display:table-cell; vertical-align:top;'>" +
+    "</textarea>" +
+    "<div id='mermaidpreview' style='display:table-cell; vertical-align:top;'>" +
+    "</div>" +
+    "</div>" +
+    "</div>");
+
+function updateMermaid(target, source) {
+    target.text(source);
+    target.removeAttr('data-processed');
+    mermaid.init(undefined, target);
+}
+
+$(function() {
+    $('.mermaid').dblclick(function() {
+        var source = $(this);
+        var id = source.attr("id");
+        var resourceRealm = source.data("mermaidresourcerealm");
+        var resourceId = source.data("mermaidresourceid");
+        var resourceVersion = source.data("mermaidresourceversion");
+        if (resourceRealm != "wiki" || resourceVersion != "") {
+            return;
+        }
+        var curr = decodeURIComponent(source.data("mermaidsource"));
+        var prev = curr;
+        var intervalId;
+        tracMermaidDialog.dialog({
+            modal: true,
+            width: 640,
+            open: function(evt, ui) {
+                intervalId = setInterval(function() {
+                    var curr = $("#mermaidsource").val();
+                    if (curr != prev) {
+                        updateMermaid($('#mermaidpreview'), curr);
+                        prev = curr;
+                    }
+                }, 1000);
+            },
+            close: function(evt, ui) {
+                clearInterval(intervalId);
+            }
+        });
+        $("#mermaidsource").val(curr);
+        updateMermaid($('#mermaidpreview'), curr);
+        $('#mermaidsave').click(function() {
+            var curr = $("#mermaidsource").val()
+            var data = {
+                "__FORM_TOKEN": form_token,
+                id: id,
+                wikipage: resourceId,
+                source: curr
+            }
+            $.post(_tracmermaid.submit, data);
+
+            // update the original chart
+            source.data("mermaidsource", curr);
+            updateMermaid(source, curr);
+
+            tracMermaidDialog.dialog("close");
+        });
+    });
+
+});
--- mermaidmacro/1.0/tracmermaid/mermaid.py
+++ mermaidmacro/1.0/tracmermaid/mermaid.py
@@ -6,19 +6,48 @@
 # This software is licensed as described in the file COPYING, which
 # you should have received as part of this distribution.
 
+import urllib2
+import uuid
 from genshi.core import escape
 from trac.core import implements
-from trac.web.chrome import add_script, ITemplateProvider
+from trac.web.chrome import add_script, add_script_data, ITemplateProvider
+from trac.web.main import IRequestHandler
+from trac.wiki.api import IWikiPageManipulator
 from trac.wiki.macros import WikiMacroBase
+from trac.wiki.model import WikiPage
 
 class MermaidMacro(WikiMacroBase):
-    implements(ITemplateProvider)
+    implements(ITemplateProvider, IRequestHandler, IWikiPageManipulator)
 
-    def expand_macro(self, formatter, name, content):
+    def expand_macro(self, formatter, name, content, args=None):
         self.log.debug("content=%s" % content)
         add_script(formatter.req, 'mermaid/mermaid.min.js')
         add_script(formatter.req, 'mermaid/tracmermaid.js')
-        return '<div class="mermaid">%s</div>' % escape(content)
+        add_script_data(formatter.req,
+                {
+                    '_tracmermaid': {
+                        'submit': formatter.req.href + '/mermaid/submit',
+                    }
+                }
+        )
+        if args == None or 'id' not in args:
+            id_attr = ''
+        else:
+            id_attr = 'id=%s' % args['id']
+        url_escaped_content = urllib2.quote(content)
+        div = """<div class="mermaid"
+                      %s
+                      data-mermaidresourcerealm="%s"
+                      data-mermaidresourceid="%s"
+                      data-mermaidresourceversion="%s"
+                      data-mermaidsource="%s">%s</div>"""
+        return div % (
+                id_attr,
+                formatter.context.resource.realm,
+                formatter.context.resource.id,
+                formatter.context.resource.version or '',
+                escape(url_escaped_content),
+                escape(content))
 
     # ITemplateProvider methods
 
@@ -27,4 +56,56 @@ class MermaidMacro(WikiMacroBase):
         return [('mermaid', resource_filename(__name__, 'htdocs'))]
 
     def get_templates_dirs(self):
+        return []
+
+    # IRequestHandler methods
+
+    def match_request(self, req):
+        self.log.debug('match_request: path_info=' + req.path_info)
+        return req.path_info.startswith('/mermaid/submit')
+
+    def process_request(self, req):
+        self.log.debug('process_request: req=' + str(req))
+        self.log.debug('process_request: args=' + str(req.args))
+
+        id = req.args['id']
+        page_name = req.args['wikipage']
+        source = req.args['source']
+        page = WikiPage(env = self.env, name = page_name)
+        lines = page.text.splitlines()
+        lines.reverse()
+        buf = []
+        while len(lines) > 0:
+            line = lines.pop()
+            if line == ('{{{#!Mermaid id="%s"' % id):
+                buf.append(line)
+                buf.append(source)
+                while len(lines) > 0:
+                    line = lines.pop()
+                    if line.rstrip() == "}}}":
+                        buf.append(line)
+                        break
+            else:
+                buf.append(line)
+        page.text = "\n".join(buf)
+
+        req.perm(page).require('WIKI_MODIFY')
+        comment = "Update from Mermaid live editor"
+        page.save(req.authname, comment, req.remote_addr)
+
+        req.send("OK", 'text/plain')
+
+    # IWikiPageManipulator methods
+
+    def prepare_wiki_page(self, req, page, fields):
+        pass
+
+    def validate_wiki_page(self, req, page):
+        buf = []
+        for line in page.text.splitlines():
+            if line == "{{{#!Mermaid":
+                buf.append('{{{#!Mermaid id="%s"' % str(uuid.uuid1()))
+            else:
+                buf.append(line)
+        page.text = "\n".join(buf)
         return []
