cdb4034ebd0f91a1927199a1c0b1153a2dbb3e05
tkob-trac <tkob-trac@7322e99d-02ea-0310-aa39-e9a107903beb>
2016-08-20 15:10:25 +0000

    Initial version
    git-svn-id: https://trac-hacks.org/svn@15721 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ pdfpreviewplugin/1.0/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2016 tkob <ether4@gmail.com>
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.
+
+from setuptools import find_packages, setup
+
+setup(
+    name = 'TracPdfPreview',
+    version = '0.1.0',
+    packages = find_packages(exclude = ['*.tests*']),
+    license = "BSD 3-Clause",
+    entry_points = {
+        'trac.plugins': [
+            'tracpdfpreview = tracpdfpreview.pdfpreview',
+        ],
+    },
+    package_data = {
+        'tracpdfpreview': [
+            'htdocs/*',
+            'htdocs/*/*',
+            'htdocs/*/*/*',
+        ],
+    },
+)
---
+++ pdfpreviewplugin/1.0/tracpdfpreview/pdfpreview.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2016 tkob <ether4@gmail.com>
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.
+
+from genshi.core import escape
+from trac.core import implements, Component
+from trac.mimeview.api import IHTMLPreviewRenderer
+from trac.web.chrome import ITemplateProvider
+
+class PdfRenderer(Component):
+    """
+    HTML renderer for PDF files.  Add application/pdf to mime_map in trac.ini
+    """
+    implements(IHTMLPreviewRenderer, ITemplateProvider)
+
+    def __init__(self):
+        pass
+
+    # IHTMLPreviewRenderer methods
+
+    def get_quality_ratio(self, mimetype):
+        if mimetype in ['application/pdf']:
+            return 8
+        return 0
+
+    def render(self, context, mimetype, content, filename=None, rev=None):
+        href = context.href
+        resource = context.resource
+        realm = resource.realm
+        if realm == 'source':
+            version = resource.version
+            pdf_url = href('export', version, resource.id)
+        elif realm == 'attachment':
+            parent = resource.parent
+            pdf_url = href(
+                    'raw-attachment', parent.realm, parent.id, resource.id)
+        viewer_url = href('chrome', 'pdfpreview', 'web', 'viewer.html')
+        iframe = """
+        <iframe style="width:100%%; height:480px" src="%s?%s"></iframe>
+        """
+        return iframe % (viewer_url, pdf_url)
+
+    # ITemplateProvider methods
+
+    def get_htdocs_dirs(self):
+        from pkg_resources import resource_filename 
+        self.log.info(resource_filename(__name__, 'htdocs'))
+        return [('pdfpreview', resource_filename(__name__, 'htdocs'))]
+
+    def get_templates_dirs(self):
+        return []
