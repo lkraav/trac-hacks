125e76e33a99254a1c33eac1cdd97e5c75f5c30c
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2016-09-02 22:47:58 +0000

    0.1.1: Improve codin style
    git-svn-id: https://trac-hacks.org/svn@15864 7322e99d-02ea-0310-aa39-e9a107903beb

--- tracdupplugin/0.11/tracdup/ticket.py
+++ tracdupplugin/0.11/tracdup/ticket.py
@@ -8,8 +8,8 @@ from trac.ticket import api, model
 
 
 class TracDupPlugin(core.Component):
-    core.implements(api.ITicketChangeListener)
-    core.implements(api.ITicketManipulator)
+
+    core.implements(api.ITicketChangeListener, api.ITicketManipulator)
 
     # helper methods
     def _add_master(self, author, dup_id, master_id):
@@ -77,21 +77,21 @@ class TracDupPlugin(core.Component):
         """
         ids = self._get_dups_recursively(ticket.id)
 
-        dups = ", ".join([str(i) for i in ids])
+        dups = ', '.join(str(i) for i in ids)
         dup_count = len(ids)
 
-        if ticket.values.get('dups', None) == dups \
-         and int(ticket.values.get('dup_count', '')) == dup_count:
+        if ticket.values.get('dups', None) == dups and \
+                int(ticket.values.get('dup_count', '')) == dup_count:
             return False
 
-        self.env.log.debug('Recalculated ticket %s with dups %s (%d)' % (
-            ticket.id, dups, dup_count))
+        self.env.log.debug('Recalculated ticket %s with dups %s (%d)',
+                           ticket.id, dups, dup_count)
 
         ticket['dups'] = dups
         ticket['dup_count'] = str(dup_count)
 
         # delete fields if there are no dups
-        if dup_count == 0:
+        if not dup_count:
             ticket['dups'] = None
             ticket['dup_count'] = None
 
@@ -143,13 +143,12 @@ WHERE c.name='dup_of' AND t.id=c.ticket AND c.value=%s
         @type  dup:       L{trac.ticket.model.Ticket}
         @type  master_id: object with __str__
         """
-        self.env.log.debug("dup: dupping %s to %s" % (dup.id, master_id))
+        self.env.log.debug("dup: dupping %s to %s", dup.id, master_id)
         self._add_master(author, dup.id, master_id)
 
         # tag this ticket and close it
         cmt = u"Duplicate of ticket #%s." % master_id
-        self.env.log.debug(
-            "adding comment to dup ticket %s" % dup.id)
+        self.env.log.debug("adding comment to dup ticket %s", dup.id)
         # FIXME: ugly hack to avoid
         # IntegrityError: columns ticket, time, field are not unique
         time.sleep(1)
@@ -162,13 +161,12 @@ WHERE c.name='dup_of' AND t.id=c.ticket AND c.value=%s
         """
         Update the dup ticket to reflect the unduplication.
         """
-        self.env.log.debug("dup: undupping %s to %s" % (dup.id, master_id))
+        self.env.log.debug("dup: undupping %s to %s", dup.id, master_id)
         self._remove_master(author, dup.id, master_id)
 
         # tag this ticket and reopen it
         cmt = u"Not a duplicate of ticket #%s." % master_id
-        self.env.log.debug(
-            "adding comment to dup ticket %s" % dup.id)
+        self.env.log.debug("adding comment to dup ticket %s", dup.id)
         # FIXME: ugly hack to avoid
         # IntegrityError: columns ticket, time, field are not unique
         import time; time.sleep(1)
@@ -176,23 +174,18 @@ WHERE c.name='dup_of' AND t.id=c.ticket AND c.value=%s
 
         dup.save_changes(author, cmt)
 
-
     # ITicketChangeListener implementation
+
     def ticket_created(self, ticket):
-        """Called when a ticket is created."""
+        pass
 
     def ticket_changed(self, ticket, comment, author, old_values):
-        """Called when a ticket is modified.
+        self.env.log.debug("ticket_changed: %s", ticket.id)
 
-        `old_values` is a dictionary containing the previous values of the
-        fields that have changed.
-        """
-        self.env.log.debug("ticket_changed: %s" % ticket.id)
-
-        if old_values.get('status', None) == 'closed' \
-            and ticket.values.get('dup_of', None):
-            self.env.log.debug("ticket_changed: "
-                'duplicate ticket %s reopened, removing dup_of' % ticket.id)
+        if old_values.get('status', None) == 'closed' and \
+                ticket.values.get('dup_of', None):
+            self.env.log.debug("ticket_changed: duplicate ticket %s "
+                               "reopened, removing dup_of", ticket.id)
             # unsetting it will trigger this method again anyway
             time.sleep(1)
             ticket['dup_of'] = None
@@ -201,8 +194,7 @@ WHERE c.name='dup_of' AND t.id=c.ticket AND c.value=%s
 
         # ignore if dup_of was not changed, or set to None (the first time)
         if not old_values.has_key('dup_of'):
-            self.env.log.debug(
-                "No dup_of change on %s" % ticket.id)
+            self.env.log.debug("No dup_of change on %s", ticket.id)
             return
 
         # ignore if dup_of did not really get a new value
@@ -210,14 +202,13 @@ WHERE c.name='dup_of' AND t.id=c.ticket AND c.value=%s
 
         # also possible to go from None to '', when updating a ticket
         # created before dup_of was a custom key
-        if old_values.get('dup_of', None) == None and master_id == '':
-            self.env.log.debug(
-                'dup_of was added to %s without a value, return' % ticket.id)
+        if old_values.get('dup_of', None) is None and master_id == '':
+            self.env.log.debug("dup_of was added to %s without a value, "
+                               "return", ticket.id)
             return
 
         if master_id == old_values.get('dup_of', None):
-            self.env.log.debug(
-                "No dup_of change on %s" % ticket.id)
+            self.env.log.debug("No dup_of change on %s", ticket.id)
             return
 
         if not master_id:
@@ -229,20 +220,14 @@ WHERE c.name='dup_of' AND t.id=c.ticket AND c.value=%s
             self._add_dup(author, ticket, master_id)
 
     def ticket_deleted(self, ticket):
-        """Called when a ticket is deleted."""
+        pass
 
     # ITicketManipulator implementation
+
     def prepare_ticket(self, req, ticket, fields, actions):
-        """Not currently called, but should be provided for future
-        compatibility."""
+        pass
 
     def validate_ticket(self, req, ticket):
-        """Validate a ticket after it's been populated from user input.
-
-        Must return a list of `(field, message)` tuples, one for each problem
-        detected. `field` can be `None` to indicate an overall problem with the
-        ticket. Therefore, a return value of `[]` means everything is OK."""
-
         res = []
 
         # the ticket we receive is a temporary not-yet-commited ticket
@@ -250,7 +235,7 @@ WHERE c.name='dup_of' AND t.id=c.ticket AND c.value=%s
         # retrieve the original one so we can compare
         ot = model.Ticket(self.env, ticket.id)
 
-        self.env.log.debug('validate_ticket: %s' % ticket.id)
+        self.env.log.debug("validate_ticket: %s", ticket.id)
 
         # refuse changes to dup_count and dups fields
         new = ticket.values.get('dups', None)
@@ -261,7 +246,7 @@ WHERE c.name='dup_of' AND t.id=c.ticket AND c.value=%s
         new = ticket.values.get('dup_count', None)
         if new is not None and new != ot.values.get('dup_count', None):
             res.append(('dup_count',
-                'Cannot manually change the dup_count field.'))
+                "Cannot manually change the dup_count field."))
             return res
 
         new_id = ticket.values.get('dup_of', None)
@@ -285,45 +270,36 @@ WHERE c.name='dup_of' AND t.id=c.ticket AND c.value=%s
                 # but still subject to other rules
             else:
                 self.env.log.debug(
-                    "validate_ticket: refusing to dup closed ticket #%s" %
-                        ticket.id)
+                    "validate_ticket: refusing to dup closed ticket #%s",
+                    ticket.id)
                 res.append(('dup_of',
-                    'Ticket is already closed, and not as a duplicate.'))
+                    "Ticket is already closed, and not as a duplicate."))
                 return res
 
         # refuse to dup_of and reopen ticket in one go
-        if ot.values['status'] == u'closed' \
-            and ticket.values['status'] == u'reopened':
+        if ot.values['status'] == u'closed' and \
+                ticket.values['status'] == u'reopened':
             self.env.log.debug("validate_ticket: "
-                "refusing to dup_of and reopen ticket #%s" %
-                    ticket.id)
+                "refusing to dup_of and reopen ticket #%s", ticket.id)
             res.append(('status',
-                'If you want to duplicate an already closed ticket, '
-                'only change dup_of without reopening the ticket.'))
+                "If you want to duplicate an already closed ticket, "
+                "only change dup_of without reopening the ticket."))
             return res
 
         # warn when it starts with #
         if len(new_id) > 0 and new_id[0] == '#':
             res.append(('dup_of',
-                'Please enter the ticket number without a leading #.'))
+                "Please enter the ticket number without a leading #."))
             return res
 
-        # refuse to dup to non-existing tickets; this raises a TracError
-        # if it doesn't exist
-        # coderanger says a Ticket can have anything with a __str__ method
-        # as id
-        # except in the 0.10.5dev branch, a non-existing ticket id raises
-        # a TracError with %d in the format string (fixed on TRUNK),
-        # so make it an int here
         master = model.Ticket(self.env, int(new_id))
 
         # refuse to dup to self
         if str(new_id) == str(ticket.id):
             self.env.log.debug("validate_ticket: "
-                "cowardly refusing to dup to self #%s" % ticket.id)
-            res.append(('dup_of',
-                'Cannot duplicate a ticket to itself.'))
+                "cowardly refusing to dup to self #%s", ticket.id)
+            res.append(('dup_of', "Cannot duplicate a ticket to itself."))
             return res
 
-        self.env.log.debug('validate_ticket: Validated ticket %s' % ticket.id)
+        self.env.log.debug("validate_ticket: Validated ticket %s", ticket.id)
         return res
