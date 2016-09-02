a519db8f46057f344859a8b2eb0a9d49fe7091fb
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2016-09-02 21:32:12 +0000

    Hack FakeUsernamePlugin deleted by rjollos
    git-svn-id: https://trac-hacks.org/svn@15804 7322e99d-02ea-0310-aa39-e9a107903beb

--- fakeusernameplugin/0.10/fakeusername/htdocs/fakeusername.js
+++
@@ -1,4 +0,0 @@
-$(function() {
-    var username = $('#fakeusername_evil').text();
-    $('#newticket').prepend('<div class="field"><label for="reporter">Your email or username:</label><br /><input type="text" id="reporter" name="reporter" size="40" value="'+username+'" /><br /></div>');
-});
