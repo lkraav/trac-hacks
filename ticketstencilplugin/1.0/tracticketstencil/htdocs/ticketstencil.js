9213e422bd90a0238491e297e9d406374333fb63
tkob-trac <tkob-trac@7322e99d-02ea-0310-aa39-e9a107903beb>
2016-11-19 07:28:19 +0000

    Apply a patch from Cinc-th to avoid TypeError in JS, closes #12957
    git-svn-id: https://trac-hacks.org/svn@15982 7322e99d-02ea-0310-aa39-e9a107903beb

--- ticketstencilplugin/1.0/tracticketstencil/htdocs/ticketstencil.js
+++ ticketstencilplugin/1.0/tracticketstencil/htdocs/ticketstencil.js
@@ -1,14 +1,14 @@
 $(function() {
     $('#field-type').change(function () {
-        var prev_type = $(this).data('prev');
+        var prev_type = $(this).data('prev') || '_ticketstencil_default_type';
         var prev_desc = $('#field-description').val().trim().replace(/\r\n/g, "\n");
         var new_type = $('#field-type option:selected').val().toLowerCase();
         var can_replace =
             prev_desc == "" ||
             prev_desc == _tracticketstencil[prev_type].trim().replace(/\r\n/g, "\n");
+        $(this).data('prev', new_type);
         if (can_replace) {
             $('#field-description').val(_tracticketstencil[new_type]);
-            $(this).data('prev', new_type);
         }
     });
     $('#field-type').change();
--- ticketstencilplugin/1.0/tracticketstencil/ticketstencil.py
+++ ticketstencilplugin/1.0/tracticketstencil/ticketstencil.py
@@ -7,6 +7,7 @@
 # you should have received as part of this distribution.
 
 from trac.core import Component, implements
+from trac.ticket.model import Type
 from trac.web.chrome import add_script, add_script_data, ITemplateProvider
 from trac.web.main import IRequestFilter
 from trac.wiki.api import WikiSystem
@@ -38,9 +39,21 @@ class TicketStencil(Component):
         add_script(req, 'ticketstencil/ticketstencil.js')
         stencils = {}
         prefix_len = len('TicketStencil')
+        all_types = [enum.name for enum in Type.select(self.env)]
         for name in self.wiki_system.get_pages('TicketStencil'):
             page = WikiPage(env = self.env, name = name)
             ticket_type = name[prefix_len:].lower()
             stencils[ticket_type] = page.text
+            try:
+                all_types.remove(ticket_type)
+            except ValueError:
+                pass
+        # Set defaults for remaining ticket types
+        for ticket_type in all_types:
+            stencils[ticket_type.lower()] = ''
+
+        # An internal dummy value. See JavaScript code.
+        stencils['_ticketstencil_default_type'] = ''
+
         add_script_data(req, {'_tracticketstencil': stencils})
         return template, data, content_type
