2352f0a406eef03d1d2f98c1ce215ec7c428c0bb
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2016-11-22 23:07:24 +0000

    1.0dev: Implement admin command `ticket close-pending`
    git-svn-id: https://trac-hacks.org/svn@15996 7322e99d-02ea-0310-aa39-e9a107903beb

--- pendingticketplugin/1.0/removepending/remove_pending.py
+++ pendingticketplugin/1.0/removepending/remove_pending.py
@@ -7,19 +7,28 @@
 # This software is licensed as described in the file COPYING, which
 # you should have received as part of this distribution.
 
+import datetime
+
+from trac.admin.api import AdminCommandError, IAdminCommandProvider
 from trac.attachment import IAttachmentChangeListener
 from trac.config import Option
 from trac.core import *
 from trac.ticket.api import ITicketManipulator
 from trac.ticket.model import Ticket
 from trac.ticket.notification import TicketNotifyEmail
-from trac.util import get_reporter_id
-from trac.util.datefmt import utc
+from trac.util import as_int, get_reporter_id
+from trac.util.datefmt import to_utimestamp, utc
+from trac.util.translation import _
+
+MESSAGE = "This ticket was closed automatically by the system. " \
+          "It was previously set to a Pending status and hasn't " \
+          "been updated within %s days."
 
 
 class RemovePendingPlugin(Component):
 
-    implements(IAttachmentChangeListener, ITicketManipulator)
+    implements(IAdminCommandProvider, IAttachmentChangeListener,
+               ITicketManipulator)
 
     pending_removal_status = Option('ticket', 'pending_removal_status', 'new',
         """Status to apply when removing 'Pending' status automatically.""")
@@ -54,10 +63,38 @@ class RemovePendingPlugin(Component):
                     ticket['status'] == 'pending':
                 ticket['status'] = self.pending_removal_status
                 ticket.save_changes(attachment.author, when=attachment.date)
-
-                # Trigger notification since we've changed the ticket.
-                tn = TicketNotifyEmail(self.env)
-                tn.notify(ticket, newticket=False, modtime=attachment.date)
+                self._send_notification(ticket, attachment.date)
 
     def attachment_deleted(self, attachment):
         pass
+
+    # AdminCommandProvider methods
+
+    def get_admin_commands(self):
+        yield ('ticket close-pending', '<maxage>',
+               "Close pending tickets older than <maxage>",
+               None, self._close_pending)
+
+    # Private methods
+
+    def _close_pending(self, maxage):
+        maxage = as_int(maxage, None)
+        if maxage is None:
+            raise AdminCommandError(
+                _("The argument 'maxage' must be an int."))
+        msg = MESSAGE % maxage
+        now = datetime.datetime.now(utc)
+        max_time = to_utimestamp(now - datetime.timedelta(days=maxage))
+
+        for id, in self.env.db_query("""
+                SELECT id FROM ticket
+                WHERE status = %s AND changetime < %s
+                """, ('pending', max_time)):
+            ticket = Ticket(self.env, id)
+            ticket['status'] = 'closed'
+            ticket.save_changes('trac', msg, now)
+            self._send_notification(ticket, now)
+
+    def _send_notification(self, ticket, modtime):
+        tn = TicketNotifyEmail(self.env)
+        tn.notify(ticket, newticket=False, modtime=modtime)
