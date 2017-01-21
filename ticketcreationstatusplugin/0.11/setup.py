099317920b5779b892fd7d23e8a5c7e8f9e0a4a3
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2017-01-21 22:47:35 +0000

    0.1: Set Trac version requirements
    git-svn-id: https://trac-hacks.org/svn@16194 7322e99d-02ea-0310-aa39-e9a107903beb

--- ticketcreationstatusplugin/0.11/setup.py
+++ ticketcreationstatusplugin/0.11/setup.py
@@ -1,11 +1,11 @@
 # Copyright (c) 2008, Stephen Hansen
-# 
+#
 # All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
-# 
-#     * Redistributions of source code must retain the above copyright 
+#
+#     * Redistributions of source code must retain the above copyright
 #       notice, this list of conditions and the following disclaimer.
 #     * Redistributions in binary form must reproduce the above copyright
 #       notice, this list of conditions and the following disclaimer in the
@@ -13,7 +13,7 @@
 #     * Neither the name of the <ORGANIZATION> nor the names of its
 #       contributors may be used to endorse or promote products derived from
 #       this software without specific prior written permission.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 # "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
 # LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
@@ -30,18 +30,16 @@
 from setuptools import find_packages, setup
 
 setup(
-    name = 'TicketCreationStatus', 
+    name = 'TicketCreationStatus',
     version = '0.1',
     author = 'Stephen Hansen',
     author_email = 'shansen@advpubtech.com',
     description = 'Sets the initial status of a ticket',
     license = \
     """Copyright (c) 2008, Stephen Hansen. All rights reserved. Released under the 3-clause BSD license. """,
-    url = "http://www.trac-hacks.org/wiki/TicketCreationStatusPlugin",
+    url = "https://www.trac-hacks.org/wiki/TicketCreationStatusPlugin",
     packages = ['ticketcreationstatus'],
-    install_requires = [
-        #'trac>=0.11',
-    ],
+    install_requires = ['Trac'],
     entry_points = {
         'trac.plugins': [
             'ticketcreationstatus = ticketcreationstatus',
@@ -53,6 +51,6 @@ setup(
             # 'announcerplugin.subscribers.ticket = announcerplugin.subscribers.ticket',
             # 'announcerplugin.subscribers.ticket_compat = announcerplugin.subscribers.ticket_compat',
             # 'announcerplugin.pref = announcerplugin.pref'
-        ]    
+        ]
     }
 )
--- ticketcreationstatusplugin/0.11/ticketcreationstatus/__init__.py
+++ ticketcreationstatusplugin/0.11/ticketcreationstatus/__init__.py
@@ -1,1 +1,3 @@
 import plugin
+import pkg_resources
+pkg_resources.require('Trac>=0.11,<1.2')
