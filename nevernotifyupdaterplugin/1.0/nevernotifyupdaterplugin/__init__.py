7809de38e4bb3d8d487f94af4436471bc23f3619
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2017-01-21 22:45:39 +0000

    1.0: Set Trac version requirements
    git-svn-id: https://trac-hacks.org/svn@16193 7322e99d-02ea-0310-aa39-e9a107903beb

--- nevernotifyupdaterplugin/1.0/nevernotifyupdaterplugin/__init__.py
+++ nevernotifyupdaterplugin/1.0/nevernotifyupdaterplugin/__init__.py
@@ -1,1 +1,4 @@
+import pkg_resources
 from api import *
+
+pkg_resources.require('Trac>=1.0,<1.2')
--- nevernotifyupdaterplugin/1.0/setup.py
+++ nevernotifyupdaterplugin/1.0/setup.py
@@ -5,14 +5,15 @@ PACKAGE = 'nevernotifyupdaterplugin'
 setup(name=PACKAGE,
       version='1.0',
       packages=[PACKAGE],
-      url='http://www.trac-hacks.org/wiki/NeverNotifyUpdaterPlugin',
+      url='https://www.trac-hacks.org/wiki/NeverNotifyUpdaterPlugin',
       license='http://www.opensource.org/licenses/mit-license.php',
       author='Russ Tyndall at Acceleration.net',
       author_email='russ@acceleration.net',
       long_description="""
-      Never send emails to the person who made the change. 
+      Never send emails to the person who made the change.
       Presumably they already know they made that change.
       """,
       entry_points={'trac.plugins': '%s = %s' % (PACKAGE, PACKAGE)},
+      install_requires=['Trac'],
 )
 
