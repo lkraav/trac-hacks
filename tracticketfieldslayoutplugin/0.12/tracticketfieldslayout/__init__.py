2c35d493bc8c54f617deee8536fc722d05a99677
jun66j5 <jun66j5@7322e99d-02ea-0310-aa39-e9a107903beb>
2017-01-30 03:12:54 +0000

    TracTicketFieldsLayoutPlugin: prevent Trac 1.3.2dev with Jinja2
    git-svn-id: https://trac-hacks.org/svn@16211 7322e99d-02ea-0310-aa39-e9a107903beb

--- tracticketfieldslayoutplugin/0.12/tracticketfieldslayout/__init__.py
+++ tracticketfieldslayoutplugin/0.12/tracticketfieldslayout/__init__.py
@@ -1,3 +1,7 @@
 # -*- coding: utf-8 -*-
 import pkg_resources
 pkg_resources.require('Trac>=0.12,<1.4')
+from trac.web.chrome import Chrome
+if hasattr(Chrome, 'jenv'):
+    raise pkg_resources.VersionConflict("Trac with Jinja2 isn't supported")
+del pkg_resources, Chrome
