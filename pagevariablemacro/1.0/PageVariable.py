f2d3c566f261a748372c6cbea3edb9cd681ed693
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2017-03-07 04:50:38 +0000

    Make compatible with Trac 1.0 and later
    Fixes #12719.
    
    
    git-svn-id: https://trac-hacks.org/svn@16297 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ pagevariablemacro/1.0/PageVariable.py
@@ -0,0 +1,60 @@
+# Copyright (c) 2008, Philippe Monnaie <philippe.monnaie@gmail.com>
+#
+# Permission to use, copy, modify, and/or distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+from trac.util import TracError
+from trac.wiki.api import parse_args
+from trac.wiki.macros import WikiMacroBase
+
+
+class PageVariable(WikiMacroBase):
+    """
+    PageVariable macro.
+    This macro sets or gets a variable that is accessible throughout the
+    entire page.
+
+    Use: [[PageVariable(name,value)]]
+    Name signifies the name of the parameter.
+    Value signifies the value the parameter should be set to.
+    If a value is supplied, this macro returns an empty string. When
+    trying to set a variable that is already set (or present in the url
+    for some other reason), an error is raised.
+
+    If the parameter value is ommitted, the currently known value is
+    returned. If no value is known, an error is raised.
+    """
+
+    revision = "$Rev$"
+    url = "$URL$"
+
+    def expand_macro(self, formatter, name, args):
+        args = parse_args(args)[0]
+        req = formatter.req
+        if len(args) == 1:
+            val = req.args.get(name)
+            if val is None:
+                return 'ERROR: Variable %s not declared' % name
+            return val
+        else:
+            if len(args) == 2:
+                key = args[0]
+                val = req.args.get(key)
+                if val is not None:
+                    return 'ERROR: Variable %s already set or present in ' \
+                           'the url' % name
+                else:
+                    req.args[name] = args[1]
+                return ''
+            else:
+                return 'ERROR: Invalid number of arguments supplied to ' \
+                       'PageVariable macro: %s' % args
