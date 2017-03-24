9dc7e8db1ea66f4708ac953130e94ba0c34e65df
psuter <psuter@7322e99d-02ea-0310-aa39-e9a107903beb>
2017-03-24 18:35:08 +0000

    TableSorterPlugin: Bump version to 1.1. (see #12850)
    git-svn-id: https://trac-hacks.org/svn@16381 7322e99d-02ea-0310-aa39-e9a107903beb

--- tablesorterplugin/trunk/setup.py
+++ tablesorterplugin/trunk/setup.py
@@ -5,10 +5,10 @@ from setuptools import setup
 
 setup(
     name = 'Table Sorter Plugin',
-    version = '1.0',
+    version = '1.1',
     author = 'Peter Suter',
     author_email = 'petsuter@gmail.com',
-    description = 'Allows sorting any HTML table',
+    description = 'Allows sorting any wiki table',
     packages = ['tablesorter'],
     package_data = {'tablesorter': ['htdocs/*.*']},
     
