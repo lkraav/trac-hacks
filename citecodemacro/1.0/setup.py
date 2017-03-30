1e1d97334f5ebaa2750846aeb180801280dce72c
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2017-03-30 07:24:23 +0000

    TracCiteCode 0.3.4: Conform to PEP8
    git-svn-id: https://trac-hacks.org/svn@16422 7322e99d-02ea-0310-aa39-e9a107903beb

--- citecodemacro/1.0/setup.py
+++ citecodemacro/1.0/setup.py
@@ -9,19 +9,20 @@
 from setuptools import find_packages, setup
 
 setup(
-    name = 'TracCiteCode',
-    version = '0.3.4',
-    packages = find_packages(exclude=['*.tests*']),
-    license = "BSD 3-Clause",
-    url = 'https://trac-hacks.org/wiki/CiteCodeMacro',
-    entry_points = {
+    name='TracCiteCode',
+    version='0.3.4',
+    packages=find_packages(exclude=['*.tests*']),
+    license="BSD 3-Clause",
+    url='https://trac-hacks.org/wiki/CiteCodeMacro',
+    entry_points={
         'trac.plugins': [
             'traccitecode = traccitecode',
         ],
     },
-    package_data = {
+    package_data={
         'traccitecode': [
             'htdocs/*',
         ],
     },
+    install_requires=['Trac'],
 )
--- citecodemacro/1.0/traccitecode/citecode.py
+++ citecodemacro/1.0/traccitecode/citecode.py
@@ -6,35 +6,39 @@
 # This software is licensed as described in the file COPYING, which
 # you should have received as part of this distribution.
 
-import re
-import urlparse
 import mimetypes
 import os
+import re
+import urlparse
 
 from trac.core import Component, implements
-from trac.mimeview import Mimeview, Context
+from trac.mimeview import Context, Mimeview
 from trac.mimeview.api import is_binary
 from trac.util.html import escape
-from trac.web.chrome import add_script, add_script_data, add_stylesheet, ITemplateProvider
+from trac.web.chrome import ITemplateProvider, add_script, add_script_data, \
+                            add_stylesheet
 from trac.web.main import IRequestFilter, IRequestHandler
 from trac.wiki.macros import WikiMacroBase
 from trac.versioncontrol import RepositoryManager
 
 
 class CiteCodeMacro(WikiMacroBase):
+
     def __init__(self):
         self.repoman = RepositoryManager(self.env)
         self.mimeview = Mimeview(self.env)
 
-    def expand_macro(self, formatter, name, content):
+    def expand_macro(self, formatter, name, content, args=None):
         self.log.debug("content=%s", content)
 
         add_stylesheet(formatter.req, 'citecode/citecode.css')
 
         # parse the argument
-        scheme, netloc, path, params, query, fragment = urlparse.urlparse(content)
-        self.log.debug("scheme=%s, netloc=%s, path=%s, params=%s, query=%s, fragment=%s",
-                       scheme, netloc, path, params, query, fragment)
+        scheme, netloc, path, params, query, fragment = urlparse.urlparse(
+            content)
+        self.log.debug(
+            "scheme=%s, netloc=%s, path=%s, params=%s, query=%s, fragment=%s",
+            scheme, netloc, path, params, query, fragment)
         qs = urlparse.parse_qs(query)
         self.log.debug("qs=%s", qs)
 
@@ -44,36 +48,36 @@ class CiteCodeMacro(WikiMacroBase):
         else:
             rev = None
         self.log.debug("rev=%s", rev)
-        node = repo.get_node(path, rev = rev)
+        node = repo.get_node(path, rev=rev)
         content = node.get_content()
-        if content == None:
+        if content is None:
             self.log.debug("node is directory")
             return "<p>%s</p>" % escape(content)
         else:
             context = Context.from_request(formatter.req)
-            content_type = node.get_content_type() or mimetypes.guess_type(path)[0]
+            content_type = node.get_content_type() or \
+                           mimetypes.guess_type(path)[0]
             self.log.debug("content_type=%s", content_type)
             content = content.read()
             if fragment != "" and not is_binary(content):
                 m = re.match("L(\d+)(-L?(\d+))?", fragment)
-                if m != None:
+                if m is not None:
                     start, _, end = m.groups()
                     end = end or start
-                    lines = content.splitlines()[int(start)-1:int(end)]
+                    lines = content.splitlines()[int(start) - 1:int(end)]
                     content = "\n".join(lines)
-                    context.set_hints(lineno = int(start))
+                    context.set_hints(lineno=int(start))
             xhtml = self.mimeview.render(
-                    context = context,
-                    mimetype = content_type,
-                    content = content,
-                    filename = "",
-                    annotations=['citecode_lineno'],
-                    )
+                context=context,
+                mimetype=content_type,
+                content=content,
+                filename="",
+                annotations=['citecode_lineno'],
+            )
             return xhtml
 
 
 class CiteCodeAndCreateTicket(Component):
-
     implements(IRequestFilter, IRequestHandler, ITemplateProvider)
 
     # ITemplateProvider methods
@@ -91,15 +95,13 @@ class CiteCodeAndCreateTicket(Component):
         return handler
 
     def post_process_request(self, req, template, data, content_type):
-        #Chrome(self.env).add_jquery_ui(req)
+        # Chrome(self.env).add_jquery_ui(req)
         add_script(req, 'citecode/citecode.js')
-        add_script_data(req,
-                {
-                    '_traccitecode': {
-                        'newticket': req.href + '/citecode/newticket',
-                    }
-                }
-        )
+        add_script_data(req, {
+            '_traccitecode': {
+                'newticket': req.href + '/citecode/newticket',
+            }
+        })
         return template, data, content_type
 
     # IRequestHandler methods
@@ -121,19 +123,15 @@ class CiteCodeAndCreateTicket(Component):
             line = '#L' + qs['L'][0].encode()
 
         req.redirect(req.href.newticket(
-                summary = 'Comment on %s' % os.path.basename(path),
-                description = """source:%s%s%s:
+            summary='Comment on %s' % os.path.basename(path),
+            description="""source:%s%s%s:
 
 [[CiteCode(%s%s%s)]]
 
 """ % (path, at_rev, line, path, query, line),
-                preview = True,
+            preview=True,
         ))
 
-from trac.mimeview.api import IHTMLPreviewAnnotator
-from trac.util import Ranges
-from trac.util.html import html as tag
-from trac.util.translation import _
 
 # Just copied from trunk of Trac core and renamed annotation_type
 # since 'lineno' hint is not available in Trac 1.0
@@ -155,6 +153,12 @@ from trac.util.translation import _
 # Author: Daniel Lundin <daniel@edgewall.com>
 #         Christopher Lenz <cmlenz@gmx.de>
 #         Christian Boos <cboos@edgewall.org>
+
+from trac.mimeview.api import IHTMLPreviewAnnotator
+from trac.util import Ranges
+from trac.util.html import html as tag
+from trac.util.translation import _
+
 
 class LineNumberAnnotator(Component):
     """Text annotator that adds a column with line numbers."""
