ea9be1bffb045735f24338f662fc33fbb47d70b3
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2017-04-16 02:08:36 +0000

    Fix indentation
    git-svn-id: https://trac-hacks.org/svn@16528 7322e99d-02ea-0310-aa39-e9a107903beb

--- metasearchplugin/0.10/tracmetasearch/search.py
+++ metasearchplugin/0.10/tracmetasearch/search.py
@@ -1,16 +1,16 @@
 ## Meta-Search - Search multiple trac projects at once
 #
-# The tracmetasearch plugin is a first shot at searching multiple trac 
-# repositories at a time. 
+# The tracmetasearch plugin is a first shot at searching multiple trac
+# repositories at a time.
 #
-# It performs searches using equal filters and queries on all trac repositories 
+# It performs searches using equal filters and queries on all trac repositories
 # configured for the ProjectMenu Plugin.
 #
 # tracmetasearch is currently implemented using the XMLRPC Plugin (and python's
-# xmlrpc api). 
+# xmlrpc api).
 #
 # Both ProjectMenu and XMLRPC plugin must be installed and configured.
-# Access to all trac projects must be unrestricted from localhost for the 
+# Access to all trac projects must be unrestricted from localhost for the
 # XMLRPC searches to work.
 #
 
@@ -52,36 +52,36 @@ class TracMetaSearchPlugin(Component):
         # get search path and base_url
         search_path, this_project = os.path.split(self.env.path)
         base_url, _ = posixpath.split(req.abs_href())
-        
+
         for project in os.listdir(search_path):
             # skip our own project
             if project == this_project:
                 continue
-            
+
             # make up URL for project
-            project_url = '/'.join( (base_url, project, 'xmlrpc') )        
-            
+            project_url = '/'.join( (base_url, project, 'xmlrpc') )
+
             # remove 'meta' from filters
             rpc_filters = [];
             for filter in filters:
-               if not filter == 'meta':
-                   rpc_filters.append( filter )
-            
+                if not filter == 'meta':
+                    rpc_filters.append( filter )
+
             self.env.log.debug("Searching project %s" % project )
             self.env.log.debug("Searching URL %s" % project_url )
-            self.env.log.debug("Searching for %s" % query[0] )           
+            self.env.log.debug("Searching for %s" % query[0] )
             self.env.log.debug("Searching with filters %s" % rpc_filters )
 
             # don't do anything if we have no filters
             if not rpc_filters:
                 continue
-            
+
             # try searching remote repositories
             try:
                 # the URL is the first component of the project list
-                # 
+                #
                 server = xmlrpclib.ServerProxy( project_url )
-            
+
                 # get query results
                 results = server.search.performSearch( query[0], rpc_filters );
 
@@ -93,7 +93,7 @@ class TracMetaSearchPlugin(Component):
             # return on empty query results
             if not results:
                 continue
-            
+
             for result in results:
                 # result is a list containing
                 # (href, title, date, author, excerpt)
