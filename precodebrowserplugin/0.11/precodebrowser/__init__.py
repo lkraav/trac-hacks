b70e574dc7dc00378666ca8fff4eaf2b11ba1e0a
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2017-04-16 01:38:34 +0000

    Fix indentation
    git-svn-id: https://trac-hacks.org/svn@16513 7322e99d-02ea-0310-aa39-e9a107903beb

--- precodebrowserplugin/0.11/precodebrowser/__init__.py
+++ precodebrowserplugin/0.11/precodebrowser/__init__.py
@@ -1,4 +1,3 @@
 # $Id$
 
 from precodebrowser import *
-
--- precodebrowserplugin/0.11/precodebrowser/precodebrowser.py
+++ precodebrowserplugin/0.11/precodebrowser/precodebrowser.py
@@ -5,13 +5,12 @@ from trac.web.chrome import ITemplateStreamFilter
 from genshi.filters.transform import Transformer, StreamBuffer
 
 class PreCodeBrowserPlugin(Component):
-	implements(ITemplateStreamFilter)
+    implements(ITemplateStreamFilter)
 
-	# ITemplateStreamFilter methods
-	def filter_stream(self, req, method, filename, stream, data):
-		filter = Transformer('//table[@class = "code"]')
-		return stream | filter.rename('pre') \
-			.select('//pre[@class = "code"]/thead').remove().end() \
-			.select('//pre[@class = "code"]/tbody/tr/th').remove().end() \
-			.select('//pre[@class = "code"]/tbody/tr/td').append('\n').unwrap()
-
+    # ITemplateStreamFilter methods
+    def filter_stream(self, req, method, filename, stream, data):
+        filter = Transformer('//table[@class = "code"]')
+        return stream | filter.rename('pre') \
+                .select('//pre[@class = "code"]/thead').remove().end() \
+                .select('//pre[@class = "code"]/tbody/tr/th').remove().end() \
+                .select('//pre[@class = "code"]/tbody/tr/td').append('\n').unwrap()
