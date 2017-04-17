2e04d02e4e3997557ecdc7c05f87dde861b8b79f
jun66j5 <jun66j5@7322e99d-02ea-0310-aa39-e9a107903beb>
2017-04-17 03:12:31 +0000

    TracWysiwygPlugin/0.12.0.5: invoke `setup()` only if run as a script
    git-svn-id: https://trac-hacks.org/svn@16547 7322e99d-02ea-0310-aa39-e9a107903beb

--- tracwysiwygplugin/0.12/setup.py
+++ tracwysiwygplugin/0.12/setup.py
@@ -1,23 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from setuptools import setup, find_packages
-
-setup(
-    name = 'TracWysiwyg',
-    version = '0.12.0.5',
-    description = 'TracWiki WYSIWYG Editor',
-    license = 'BSD',
-    url = 'http://trac-hacks.org/wiki/TracWysiwygPlugin',
-    author = 'Jun Omae',
-    author_email = 'omae@opengroove.com',
-    packages = find_packages(exclude=['*.tests*']),
-    package_data = {
-        'tracwysiwyg' : [ 'htdocs/*.js', 'htdocs/*.css', 'htdocs/*.png' ],
-    },
-    entry_points = {
-        'trac.plugins': [
-            'tracwysiwyg = tracwysiwyg',
-        ],
+def main():
+    kwargs = {
+        'name': 'TracWysiwyg',
+        'version': '0.12.0.5',
+        'description': 'TracWiki WYSIWYG Editor',
+        'license': 'BSD',
+        'url': 'https://trac-hacks.org/wiki/TracWysiwygPlugin',
+        'author': 'Jun Omae',
+        'author_email': 'omae@opengroove.com',
+        'packages': find_packages(exclude=['*.tests*']),
+        'package_data': {
+            'tracwysiwyg' : ['htdocs/*.js', 'htdocs/*.css', 'htdocs/*.png'],
+        },
+        'entry_points': {
+            'trac.plugins': [
+                'tracwysiwyg = tracwysiwyg',
+            ],
+        }
     }
-)
+    setup(**kwargs)
+
+if __name__ == '__main__':
+    from setuptools import setup, find_packages
+    main()
