ad5e6141b981624b0e2ef34f95dbb26674a1cf65
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2017-06-08 05:21:01 +0000

    Tracchildtickettreemacro 1.1.0dev: Adapt to Trac 1.0 API
    Fixes #13204.
    
    
    git-svn-id: https://trac-hacks.org/svn@16651 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ childtickettreemacro/1.0/childtickettree/macro.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+
+import re
+
+from trac.core import TracError
+from trac.wiki.api import parse_args
+from trac.ticket.model import Ticket
+from trac.util import as_bool
+from trac.util.html import html as tag
+from trac.wiki.macros import WikiMacroBase
+from trac.wiki.model import WikiPage
+
+
+class ChildTicketTreeMacro(WikiMacroBase):
+    """This macro provides a complete hierarchy of all childtickets under
+    the given ticket.
+
+    Examples:
+    {{{
+    [[ChildTicketTree]] - Used alone in a ticket field, will take the current ticket as the top level and print the tree.
+    [[ChildTicketTree(#1)]] - Can be used in any wiki page anywhere and provides a tree for ticket with id '#1' ('1' ie. without '#' also accepted).
+    [[ChildTicketTree(ticket=1)]] - The ticket number can also be explicitly given as an argument.
+    [[ChildTicketTree(#1,border=true,root=true)]] - Show the complete tree (including original parent) within a border.
+    }}}
+
+    Args:
+        ticket  - The ticket number to 'query'. The number can also be provided as a single stand alone argument. If no ticket is supplied and the macro is used within a ticket, the ticket number will be taken as the intended.
+        root    - Boolean value indicating whether the parent ticket should be listed in tree (default: false).
+        border  - If true, a border will be drawn around the child ticket list (default: false).
+
+    """
+
+    def expand_macro(self, formatter, name, text):
+
+        req = formatter.req
+        context = formatter.context
+        resource = context.resource
+
+        # Process the arguments.
+        args, kwargs = parse_args(text)
+
+        if 'ticket' not in kwargs and len(args) > 0:
+            kwargs['ticket'] = args[0]
+        elif 'ticket' not in kwargs and not len(args):
+            kwargs['ticket'] = WikiPage(self.env, resource).name
+
+        try:
+            kwargs['ticket'] = int(unicode(kwargs.get('ticket')).lstrip('#'))
+        except ValueError:
+            raise TracError('No ticket id supplied or it is not valid '
+                            'integer value.')
+
+        ticket = Ticket(self.env, kwargs['ticket'])
+
+        self.childtickets = {}    # { parent -> children } - 1:n
+
+        for child, parent in self.env.db_query("""
+                SELECT ticket,value FROM ticket_custom WHERE name='parent'
+                """):
+            if parent and re.match('#\d+', parent):
+                self.childtickets.setdefault(int(parent.lstrip('#')), []) \
+                                 .append(child)
+
+        # First ticket has no indentation.
+        ticket['indent'] = 0
+
+        # List of tickets that will be displayed.
+        if as_bool(kwargs.get('root')):
+            self.tickets = [ticket]
+        else:
+            self.tickets = []
+
+        # Do this neater!
+        self.indent_children(ticket)
+
+        def ticket_anchor(t):
+            title = "%s : %s : %s" % (t['type'], t['owner'], t['status'])
+            return tag.a('#%s' % t.id, class_=t['status'],
+                         href=req.href.ticket(int(t.id)), title=title)
+
+        def_list = tag.dl(
+            [(tag.dt(ticket_anchor(t),
+                     style='padding-left: %spx;' % (int(t['indent']) * 20)),
+              tag.dd("%s" % t['summary'])) for t in self.tickets],
+            class_='wiki compact')
+
+        if as_bool(kwargs.get('border')):
+            return tag.fieldset(
+                def_list,
+                tag.legend('Ticket Child Tree (#%s)' % ticket.id),
+                class_='description')
+        else:
+            return tag.div(def_list)
+
+    def indent_children(self, ticket, indent=0):
+        if ticket.id in self.childtickets:
+            indent += 1
+            for child in self.childtickets[ticket.id]:
+                child = Ticket(self.env, child)
+                child['indent'] = unicode(indent)
+                self.tickets.append(child)
+                self.indent_children(child, indent)
---
+++ childtickettreemacro/1.0/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import find_packages, setup
+
+setup(
+    name='Tracchildtickettreemacro',
+    version='1.1.0',
+    packages=find_packages(),
+    author='Mark Ryan',
+    author_email='walnut.trac.hacks@gmail.com',
+    description='Macro to see a complete tree/hierarchy of tickets under '
+                'the given ticket number.',
+    keywords='trac plugins ticket dependency childtickets',
+    url='https://trac-hacks.org/wiki/ChildTicketTreeMacro',
+    install_requires=['Trac'],
+    entry_points={'trac.plugins': 'childtickettree = childtickettree'},
+)
