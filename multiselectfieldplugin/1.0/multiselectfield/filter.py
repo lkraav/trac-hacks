3ccd30d0cedfb8b8fdf0a132f36963105d822a8b
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2017-07-18 22:07:42 +0000

    TracMultiSelectField 1.0.2: Tidy up codebase
    Refs #13244.
    
    
    git-svn-id: https://trac-hacks.org/svn@16716 7322e99d-02ea-0310-aa39-e9a107903beb

--- multiselectfieldplugin/1.0/multiselectfield/filter.py
+++ multiselectfieldplugin/1.0/multiselectfield/filter.py
@@ -1,51 +1,58 @@
-from trac.core import *
-from trac.web.api import IRequestFilter, ITemplateStreamFilter
-from trac.web.chrome import ITemplateProvider, add_script, add_script_data, add_stylesheet
-from trac.ticket.api import ITicketManipulator
-from trac.config import Option, IntOption, BoolOption, ListOption
+# -*- coding: utf-8 -*-
 
-from genshi.builder import tag
+from trac.core import Component, implements
+from trac.config import BoolOption, Option
+from trac.util.html import html as tag
+from trac.web.api import IRequestFilter, ITemplateStreamFilter
+from trac.web.chrome import ITemplateProvider, add_script, add_script_data, \
+                            add_stylesheet
+
 from genshi.filters.transform import Transformer
 
-import time
-from traceback import format_exc
-import re
 
 class MultiSelectFieldModule(Component):
-    """A trac plugin implementing a custom ticket field that can hold multiple predefined values."""
+    """A trac plugin implementing a custom ticket field that can hold
+    multiple predefined values.
+    """
 
     implements(IRequestFilter, ITemplateStreamFilter, ITemplateProvider)
 
-    option_simple_selection = BoolOption('multiselectfield', 'simple_selection', False,
+    option_simple_selection = BoolOption(
+        'multiselectfield', 'simple_selection', False,
         doc="Force using a simple standard html multiselect box.")
 
-    option_delimiter = Option('multiselectfield', 'data_delimiter', ' ',
-        doc="The delimiter that is used when storing the data (as the selected options are appended to a "
-            "single custom text field). Space is used by default as values separated by space will be "
-            "recognized by the custom text field as separate values. "
-            "NOTE: changing this option when there is already data saved with other options value is probably not good idea")
+    option_delimiter = Option(
+        'multiselectfield', 'data_delimiter', ' ',
+        doc="""The delimiter that is used when storing the data (as the
+        selected options are appended to a single custom text field).
+        Space is used by default as values separated by space will be
+        recognized by the custom text field as separate values.
+        NOTE: changing this option when there is already data saved
+        with other options value is probably not good idea
+        """)
 
-    option_strip_whitespace = BoolOption('multiselectfield', 'strip_whitespace', True,
-        doc="Defined whether whitespace in the names of the predefined selectable values is removed before saving the data. "
-            "This should be enabled when using white space as data delimiter. "
-            "NOTE: changing this option when there is already data saved with other options value is probably not good idea")
+    option_strip_whitespace = BoolOption(
+        'multiselectfield', 'strip_whitespace', True,
+        doc="""Defined whether whitespace in the names of the predefined
+        selectable values is removed before saving the data. This should
+        be enabled when using white space as data delimiter. NOTE: changing
+        this option when there is already data saved with other options
+        value is probably not good idea
+        """)
+
+    paths = '/newticket', '/ticket', '/simpleticket'
 
     # IRequestFilter methods
+
     def pre_process_request(self, req, handler):
         return handler
 
     def post_process_request(self, req, template, data, content_type):
-        mine = ['/newticket', '/ticket', '/simpleticket']
-
-        match = False
-        for target in mine:
-            if req.path_info.startswith(target):
-                match = True
-                break
-
-        if match:
-            add_script_data(req, {'multiselectfieldDelimiter': self.option_delimiter or ' '})
-            add_script_data(req, {'multiselectfieldSimple': self.option_simple_selection})
+        if req.path_info.startswith(self.paths):
+            add_script_data(req, {
+                'multiselectfieldDelimiter': self.option_delimiter or ' ',
+                'multiselectfieldSimple': self.option_simple_selection
+            })
 
             if not self.option_simple_selection:
                 add_script(req, 'multiselectfield/chosen.jquery.min.js')
@@ -56,33 +63,39 @@ class MultiSelectFieldModule(Component):
         return template, data, content_type
 
     # ITemplateStreamFilter methods
+
     def filter_stream(self, req, method, filename, stream, data):
 
         if filename == 'ticket.html':
             for field in list(self._multi_select_fields()):
-                #
                 # For all multiselect fields:
-                #   The actual data is a standard custom text field containing something like "value1 value2 value3".
-                #   This custom field uses the "list" type so each value separated by whitespace is treated as
-                #   a separate value. We hide that field from the user and and another "select" element after it with
+                #   The actual data is a standard custom text field containing
+                #   something like "value1 value2 value3". This custom field
+                #   uses the "list" type so each value separated by whitespace
+                #   is treated as a separate value. We hide that field from
+                #   the user and and another "select" element after it with
                 #   multiple selection enabled.
 
-                options = self.config.get('ticket-custom', field + '.options').split('|')
+                options = self.config.getlist('ticket-custom',
+                                              field + '.options', sep='|')
                 options_html = tag()
                 for option in options:
                     if self.option_strip_whitespace:
-                        option_without_white_space = "_".join(option.split())
+                        option_without_white_space = '_'.join(option.split())
                         options_html(tag.option(option, value=option_without_white_space))
                     else:
                         options_html(tag.option(option, value=option))
 
-                stream = stream | Transformer('//input[@name="field_' + field + '"]') \
-                                  .attr('style', 'display:none;') \
-                                  .after(tag.select(multiple="multiple", class_="multiselect", style="width:100%;")(options_html))
+                stream |= Transformer('//input[@name="field_%s"]' % field) \
+                          .attr('style', 'display:none;') \
+                          .after(tag.select(multiple="multiple",
+                                            class_="multiselect",
+                                            style="width:100%;")(options_html))
 
         return stream
 
     # ITemplateProvider methods
+
     def get_htdocs_dirs(self):
         from pkg_resources import resource_filename
         return [('multiselectfield', resource_filename(__name__, 'htdocs'))]
@@ -93,6 +106,5 @@ class MultiSelectFieldModule(Component):
     # Internal methods
     def _multi_select_fields(self):
         for key, value in self.config['ticket-custom'].options():
-            if key.endswith('.multiselect') and value == "true":
+            if key.endswith('.multiselect') and value == 'true':
                 yield key.split('.', 1)[0]
-
--- multiselectfieldplugin/1.0/setup.py
+++ multiselectfieldplugin/1.0/setup.py
@@ -1,23 +1,29 @@
 #!/usr/bin/env python
-# -*- coding: iso-8859-1 -*-
+# -*- coding: utf-8 -*-
 
 from setuptools import setup
 
 setup(
-    name = 'TracMultiSelectField',
-    version = '1.0.2',
-    packages = ['multiselectfield'],
-    package_data = { 'multiselectfield': ['htdocs/*.css', 'htdocs/*.js', 'htdocs/*.png' ] },
-    author = 'Olli Kallioinen',
-    author_email = 'olli.kallioinen@iki.fi',
-    description = 'A plugin providing a multiselection custom ticket field.',
-    license = 'BSD',
-    keywords = 'trac plugin multiselection',
-    classifiers = [
+    name='TracMultiSelectField',
+    version='1.0.2',
+    packages=['multiselectfield'],
+    author='Olli Kallioinen',
+    author_email='olli.kallioinen@iki.fi',
+    description='A plugin providing a multiselection custom ticket field.',
+    license='BSD',
+    keywords='trac plugin multiselection',
+    classifiers=[
         'Framework :: Trac',
     ],
-    install_requires = ['Trac >= 1.0'],
-    entry_points = {
+    install_requires=['Trac'],
+    package_data={
+        'multiselectfield': [
+            'htdocs/*.css',
+            'htdocs/*.js',
+            'htdocs/*.png'
+        ]
+    },
+    entry_points={
         'trac.plugins': [
             'multiselectfield.filter = multiselectfield.filter',
         ]
