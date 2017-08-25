eb017ced8d8ed0b92c48c27ad5f7729704b19bb2
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2017-08-25 18:58:16 +0000

    TracCrystalXTheme 1.1: Run autopep8 on codebase
    git-svn-id: https://trac-hacks.org/svn@16763 7322e99d-02ea-0310-aa39-e9a107903beb

--- crystalxtheme/trunk/crystalxtheme/theme.py
+++ crystalxtheme/trunk/crystalxtheme/theme.py
@@ -5,8 +5,8 @@ from trac.core import *
 
 from themeengine.api import ThemeBase
 
+
 class CrystalXTheme(ThemeBase):
     """A theme for Trac based on http://www.oswd.org/design/information/id/3465."""
 
     template = htdocs = css = screenshot = True
-    
