e81ac82eb6a8e9f24e8d0c86d062576b76128471
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2017-08-25 18:50:13 +0000

    TracRobotsTxt 2.1: Run autopep8 on codebase
    git-svn-id: https://trac-hacks.org/svn@16757 7322e99d-02ea-0310-aa39-e9a107903beb

--- robotstxtplugin/0.11/robotstxt/web_ui.py
+++ robotstxtplugin/0.11/robotstxt/web_ui.py
@@ -11,15 +11,16 @@ from trac.core import *
 from trac.web.api import IRequestHandler
 from trac.wiki.model import WikiPage
 
+
 class RobotsTxtModule(Component):
     """Serve a robots.txt file from Trac."""
-    
+
     implements(IRequestHandler)
-    
+
     # IRequestHandler methods
     def match_request(self, req):
         return req.path_info == '/robots.txt'
-        
+
     def process_request(self, req):
         page = WikiPage(self.env, 'RobotsTxt')
         data = ''
--- robotstxtplugin/0.11/setup.py
+++ robotstxtplugin/0.11/setup.py
@@ -13,19 +13,20 @@ import os
 from setuptools import setup
 
 setup(
-    name = 'TracRobotsTxt',
-    version = '2.0',
-    packages = ['robotstxt'],
+    name='TracRobotsTxt',
+    version='2.1',
+    packages=['robotstxt'],
 
-    author = 'Noah Kantrowitz',
-    author_email = 'noah@coderanger.net',
-    description = 'Serve a robots.txt file from Trac.',
-    long_description = open(os.path.join(os.path.dirname(__file__), 'README')).read(),
-    license = '3-Clause BSD',
-    keywords = 'trac plugin robots',
-    url = 'http://trac-hacks.org/wiki/RobotsTxtPlugin',
-    download_url = 'http://trac-hacks.org/svn/robotstxtplugin/0.11#egg=TracRobotsTxt-dev',
-    classifiers = [
+    author='Noah Kantrowitz',
+    author_email='noah@coderanger.net',
+    description='Serve a robots.txt file from Trac.',
+    long_description=open(os.path.join(
+        os.path.dirname(__file__), 'README')).read(),
+    license='3-Clause BSD',
+    keywords='trac plugin robots',
+    url='http://trac-hacks.org/wiki/RobotsTxtPlugin',
+    download_url='http://trac-hacks.org/svn/robotstxtplugin/0.11#egg=TracRobotsTxt-dev',
+    classifiers=[
         'Framework :: Trac',
         #'Development Status :: 1 - Planning',
         # 'Development Status :: 2 - Pre-Alpha',
@@ -39,10 +40,10 @@ setup(
         'Natural Language :: English',
         'Operating System :: OS Independent'
     ],
-    
-    install_requires = ['Trac'],
 
-    entry_points = {
+    install_requires=['Trac'],
+
+    entry_points={
         'trac.plugins': [
             'robotstxt.web_ui = robotstxt.web_ui'
         ]
