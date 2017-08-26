995487f90b8b34ec3e6923b2fd6ebe5fe4573221
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2017-08-26 03:53:31 +0000

    TracIniAdmin 0.4: Change plugin name
    git-svn-id: https://trac-hacks.org/svn@16777 7322e99d-02ea-0310-aa39-e9a107903beb

--- iniadminplugin/0.11/setup.py
+++ iniadminplugin/0.11/setup.py
@@ -12,14 +12,14 @@
 
 from setuptools import setup
 
-setup(name='IniAdmin',
-      version='0.3',
+setup(name='TracIniAdmin',
+      version='0.4',
       packages=['iniadmin'],
       author='Alec Thomas',
       maintainer='Jun Omae',
       maintainer_email='jun66j5@gmail.com',
       description='Expose all TracIni options using the Trac config option API',
-      url='http://trac-hacks.org/wiki/IniAdminPlugin',
+      url='https://trac-hacks.org/wiki/IniAdminPlugin',
       license='BSD',
       entry_points={'trac.plugins': ['iniadmin = iniadmin']},
       package_data={'iniadmin' : ['htdocs/css/*.css', 'templates/*.html', ]},
