e23542868dc2d09bb0245bca424a75c6034c1ca6
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2017-08-26 03:24:53 +0000

    TracPoll 0.4.0dev: Add license info
    git-svn-id: https://trac-hacks.org/svn@16771 7322e99d-02ea-0310-aa39-e9a107903beb

--- pollmacro/trunk/setup.py
+++ pollmacro/trunk/setup.py
@@ -1,14 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2005-2008 Alex Thomas
+# Copyright (C) 2010-2017 Ryan J Ollos <ryan.j.ollos@gmail.com>
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.
+#
+
 from setuptools import setup
 
 setup(name='TracPoll',
       version='0.4.0',
       packages=['tracpoll'],
-      entry_points = {'trac.plugins': ['tracpoll = tracpoll']},
+      entry_points={'trac.plugins': ['tracpoll = tracpoll']},
       author='Alec Thomas',
-      maintainer = 'Ryan J Ollos',
-      maintainer_email = 'ryan.j.ollos@gmail.com',
+      maintainer='Ryan J Ollos',
+      maintainer_email='ryan.j.ollos@gmail.com',
       url='https://trac-hacks.org/wiki/PollMacro',
-      license='BSD',
+      license='3-Clause BSD',
       package_data={'tracpoll': ['htdocs/css/*.css']},
       install_requires=['Trac'],
       )
