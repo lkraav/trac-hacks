e83b9d3135b2536b6a994fe95afafa027881a9da
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2017-08-26 03:42:36 +0000

    Add license info
    git-svn-id: https://trac-hacks.org/svn@16774 7322e99d-02ea-0310-aa39-e9a107903beb

--- forcepreviewplugin/0.11/forcepreview.py
+++ forcepreviewplugin/0.11/forcepreview.py
@@ -1,3 +1,12 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2007 Alec Thomas
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.
+#
+
 from trac.core import *
 from trac.config import *
 from trac.web.api import ITemplateStreamFilter
