c70060a74d5a38e106fab9a35998c43dc29a72d1
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2017-09-08 19:01:17 +0000

    TracExcelXLSTViewer 0.9.3: Fix mixed tabs and spaces
    Refs #13269.
    
    
    git-svn-id: https://trac-hacks.org/svn@16798 7322e99d-02ea-0310-aa39-e9a107903beb

--- excelxlstviewerplugin/0.11/setup.py
+++ excelxlstviewerplugin/0.11/setup.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2012 Daniel Rolls, Maxeler Technologies Inc <drolls@maxeler.com>  
-# Derived from Christopher Lenz's ExcelViewerPlugin 
+# Copyright (C) 2012 Daniel Rolls <drolls@maxeler.com>
+# Copyright (C) 2012 Maxeler Technologies Inc
+# Derived from Christopher Lenz's ExcelViewerPlugin
 #   Copyright (C) 2006 Christopher Lenz <cmlenz@gmx.de>
 #   All rights reserved.
 #
@@ -12,15 +13,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'TracExcelXLSTViewer', version = '0.9.2',
-    author = 'Daniel Rolls', author_email = 'drolls@maxeler.com',
-    description = 'Support for preview of Microsoft Excel XLST files in Trac',
-    license = 'BSD',
-    packages = find_packages(exclude=['*.test*']),
-    entry_points = {
+    name='TracExcelXLSTViewer',
+    version='0.9.3',
+    author='Daniel Rolls',
+    author_email='drolls@maxeler.com',
+    description='Support for preview of Microsoft Excel XLST files in Trac',
+    license='BSD',
+    packages=find_packages(exclude=['*.test*']),
+    entry_points={
         'trac.plugins': ['excelXLSTviewer = tracexcelxlstviewer']
     },
-    install_requires = ['openpyxl'],
-    zip_safe = True
+    install_requires=['openpyxl'],
+    zip_safe=True,
 )
-
--- excelxlstviewerplugin/0.11/tracexcelxlstviewer/__init__.py
+++ excelxlstviewerplugin/0.11/tracexcelxlstviewer/__init__.py
@@ -1,17 +1,20 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2012 Daniel Rolls, Maxeler Technologies Inc <drolls@maxeler.com>  
-# Derived from Christopher Lenz's ExcelViewerPlugin 
+# Copyright (C) 2012 Daniel Rolls <drolls@maxeler.com>
+# Copyright (C) 2012 Maxeler Technologies Inc
+# Derived from Christopher Lenz's ExcelViewerPlugin
 #   Copyright (C) 2006 Christopher Lenz <cmlenz@gmx.de>
 #   All rights reserved.
 #
 # This software is licensed as described in the file COPYING, which
 # you should have received as part of this distribution.
 
-from trac.core import *
+import os
+import tempfile
+
+from trac.core import Component, implements
 from trac.mimeview.api import IHTMLPreviewRenderer
 from trac.util import escape
-import tempfile, os
 
 __all__ = ['ExcelXSLTRenderer']
 
@@ -25,8 +28,8 @@ except ImportError:
 class ExcelXSLTRenderer(Component):
     implements(IHTMLPreviewRenderer)
 
-    MIME_TYPES = ('application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 
-		  'application/vnd.ms-excel.sheet.macroEnabled.12')
+    MIME_TYPES = ('application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+                  'application/vnd.ms-excel.sheet.macroEnabled.12')
 
     # IHTMLPreviewRenderer methods
 
@@ -38,31 +41,32 @@ class ExcelXSLTRenderer(Component):
     def render(self, req, mimetype, content, filename=None, url=None):
         if hasattr(content, 'read'):
             content = content.read()
-	fd, path = tempfile.mkstemp(suffix='.xlsx', text=True)
-	os.write(fd, content)
-	os.close(fd)
-	book = openpyxl.load_workbook(path)
+        fd, path = tempfile.mkstemp(suffix='.xlsx', text=True)
+        os.write(fd, content)
+        os.close(fd)
+        book = openpyxl.load_workbook(path)
         buf = []
-	for sheetName in book.get_sheet_names():
-	    sheet = book.get_sheet_by_name(sheetName)
-	    if len(sheet.get_cell_collection()) == 0:
-	        continue # Skip empty sheet
-	    buf.append(u'<table class="listing"><caption>%s</caption>\n' % escape(sheetName))
+        for sheetName in book.get_sheet_names():
+            sheet = book.get_sheet_by_name(sheetName)
+            if len(sheet.get_cell_collection()) == 0:
+                continue  # Skip empty sheet
+            buf.append(u'<table class="listing"><caption>%s</caption>\n'
+                       % escape(sheetName))
             buf.append(u'<tbody>')
-	    sheet.get_highest_row()
-	    rowIdx = 0
-	    for row in sheet.range(sheet.calculate_dimension()):
-                buf.append(u'<tr class="%s">' % (rowIdx % 2 and 'odd' or 'even'))
-		for cell in row:
-		    if cell.value == None:
-		        val = u''
-	            else:
-		        val = cell.value
+            sheet.get_highest_row()
+            rowIdx = 0
+            for row in sheet.range(sheet.calculate_dimension()):
+                buf.append(u'<tr class="%s">'
+                           % (rowIdx % 2 and 'odd' or 'even'))
+                for cell in row:
+                    if cell.value is None:
+                        val = u''
+                    else:
+                        val = cell.value
                     buf.append(u'<td>%s</td>' % val)
                 buf.append(u'</tr>\n')
-		rowIdx = rowIdx + 1
+                rowIdx = rowIdx + 1
             buf.append(u'</tbody>')
             buf.append(u'</table>\n')
-	os.unlink(path)
+        os.unlink(path)
         return u''.join(buf)
-
