f522c49d20a7cd64ab424d48cabadc8660afcf82
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2017-10-01 01:59:05 +0000

    TracHideVals 1.2: Filter fields with `optgroups`
    Refs #13276.
    
    
    git-svn-id: https://trac-hacks.org/svn@16838 7322e99d-02ea-0310-aa39-e9a107903beb

--- hidevalsplugin/0.11/hidevals/filter.py
+++ hidevalsplugin/0.11/hidevals/filter.py
@@ -4,7 +4,7 @@
 # Copyright (c) 2007 Noah Kantrowitz. All rights reserved.
 # Copyright (c) 2008 Iker Jimenez. All rights reserved.
 
-from trac.core import *
+from trac.core import Component, implements
 from trac.web.api import IRequestFilter
 
 try:
@@ -34,62 +34,75 @@ class HideValsFilter(Component):
             return template, data, content_type
         else:
             visible_fields = HideValsSystem(self.env).visible_fields(req)
-            self.env.log.debug("visible_fields: %s" % str(visible_fields))
+            self.log.debug("visible_fields: %s", visible_fields)
             dont_filter = set(HideValsSystem(self.env).dont_filter)
-            self.env.log.debug("dont_filter: %s" % str(dont_filter))
+            self.log.debug("dont_filter: %s", dont_filter)
             to_delete = []
             fields = data['fields']
 
-            if req.path_info.startswith('/newticket') or req.path_info.startswith('/ticket'):
-                for field in fields:
-                    if field['options'] and field['name'] not in dont_filter:
-                        if visible_fields.has_key(field['name']):
-                            # If we have any values, filter what is there
-                            # ???: Can a field have no options at this point? <NPK>
-                            opts = field['options']
-                            valid_opts = visible_fields[field['name']]
-                            opts_to_delete = []
-                            for opt in opts:
-                                if opt not in valid_opts:
-                                    opts_to_delete.append(opt)
+            def filter_field(field):
+                if field['options']:
+                    opts = field['options']
+                    valid_opts = visible_fields[field['name']]
+                    opts_to_delete = []
+                    for opt in opts:
+                        if opt not in valid_opts:
+                            opts_to_delete.append(opt)
 
-                            for opt in opts_to_delete:
-                                self.env.log.debug(
-                                    "HideValsFilter: '%s' option removed from '%s' field" % (opt, field['name']))
-                                opts.remove(opt)
+                    for opt in opts_to_delete:
+                        self.log.debug(
+                            "HideValsFilter: '%s' option removed "
+                            "from '%s' field", opt, field['name'])
+                        opts.remove(opt)
+                elif field['optgroups']:
+                    groups = field['optgroups']
+                    valid_opts = visible_fields[field['name']]
+                    opts_to_delete = []
+                    for grp in groups:
+                        for opt in grp['options']:
+                            if opt not in valid_opts:
+                                opts_to_delete.append(opt)
+
+                    for grp in groups:
+                        for opt in opts_to_delete:
+                            if opt in grp['options']:
+                                self.log.debug(
+                                    "HideValsFilter: '%s' option removed "
+                                    "from '%s' field",
+                                    opt, field['name'])
+                                grp['options'].remove(opt)
+
+            if req.path_info.startswith('/newticket') or \
+                    req.path_info.startswith('/ticket'):
+                for field in fields:
+                    if (field.get('options') or field.get('optgroups')) and \
+                            field['name'] not in dont_filter:
+                        if field['name'] in visible_fields:
+                            filter_field(field)
                         else:
-                            # If there are no values for this user, remove the field entirely
-                            # NOTE: Deleting in place screws up the iteration,
-                            # so do it all afterwards. <NPK>
+                            # If there are no values for this user, remove
+                            # the field entirely
+                            # NOTE: Deleting in place screws up the
+                            # iteration, so do it all afterwards.
                             to_delete.append(field)
                 for field in to_delete:
                     self.env.log.debug(
-                        "HideValsFilter: '%s' field removed" % field['name'])
+                        "HideValsFilter: '%s' field removed", field['name'])
                     fields.remove(field)
             elif req.path_info.startswith('/query'):
-                for field_name, field_value in fields.iteritems():
-                    if field_value.has_key('options') and field_value['options'] and field_name not in dont_filter:
-                        if visible_fields.has_key(field_name):
-                            # If we have any values, filter what is there
-                            # ???: Can a field have no options at this point? <NPK>
-                            opts = field_value['options']
-                            valid_opts = visible_fields[field_name]
-                            opts_to_delete = []
-                            for opt in opts:
-                                if opt not in valid_opts:
-                                    opts_to_delete.append(opt)
-
-                            for opt in opts_to_delete:
-                                self.env.log.debug(
-                                    "HideValsFilter: '%s' option removed from '%s' field" % (opt, field_name))
-                                opts.remove(opt)
+                for field in fields.itervalues():
+                    if (field.get('options') or field.get('optgroups')) and \
+                            field['name'] not in dont_filter:
+                        if field['name'] in visible_fields:
+                            filter_field(field)
                         else:
-                            # If there are no values for this user, remove the field entirely
-                            # NOTE: Deleting in place screws up the iteration,
-                            # so do it all afterwards. <NPK>
-                            to_delete.append(field_name)
+                            # If there are no values for this user, remove
+                            # the field entirely
+                            # NOTE: Deleting in place screws up the
+                            # iteration, so do it all afterwards.
+                            to_delete.append(field['name'])
                 for field_name in to_delete:
-                    self.env.log.debug(
-                        "HideValsFilter: '%s' field removed" % field_name)
+                    self.log.debug(
+                        "HideValsFilter: '%s' field removed", field_name)
                     del fields[field_name]
-            return (template, data, content_type)
+            return template, data, content_type
