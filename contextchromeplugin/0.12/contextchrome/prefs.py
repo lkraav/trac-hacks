fe7becd2c6e61c80e006a501e29e19375099c8b8
matobaa <matobaa@7322e99d-02ea-0310-aa39-e9a107903beb>
2017-10-28 21:30:29 +0000

    add new UserStyle feature
    git-svn-id: https://trac-hacks.org/svn@16928 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ contextchromeplugin/0.12/contextchrome/prefs.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2017 MATOBA Akihiro <matobaa+trac-hacks@gmail.com>
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.
+
+from trac.core import Component, implements
+from trac.prefs.api import IPreferencePanelProvider
+from trac.util.translation import _
+from trac.web.chrome import ITemplateProvider, add_notice, add_stylesheet,\
+    add_script
+from trac.web.api import IRequestHandler, IRequestFilter, HTTPException
+from pkg_resources import ResourceManager
+
+_path_css = '/contextchrome/userstyle.css'
+_path_js = '/contextchrome/userscript.js'
+
+
+class UserStyle(Component):
+    """ inject user's own script or style to all page. """
+    implements(IPreferencePanelProvider, IRequestFilter,
+               IRequestHandler, ITemplateProvider)
+
+    # IRequestFilter methods
+
+    def pre_process_request(self, req, handler):
+        pass  # do nothing
+        return handler
+
+    def post_process_request(self, req, template, data, content_type):
+        if template and template.startswith('prefs') or \
+                template.startswith('admin'):
+            pass  # do nothing, for troubleshoot yourself
+            return template, data, content_type
+        if _path_css in req.session:
+            add_stylesheet(req, _path_css)
+        if _path_js in req.session:
+            add_script(req, _path_js)
+        return template, data, content_type
+
+    # IRequestHandler methods
+
+    def match_request(self, req):
+        return _path_css == req.path_info \
+            or _path_js == req.path_info
+
+    def process_request(self, req):
+        if _path_css == req.path_info:
+            content = req.session[_path_css]
+            content_type = 'text/css'
+        elif _path_js == req.path_info:
+            content = req.session[_path_js]
+            content_type = 'application/javascript'
+        else:
+            raise HTTPException('File %s not found', req.path_info)
+        req.send_response(200)
+        req.send_header('Content-Type', content_type)
+        # req.send_header('Last-Modified', last_modified)
+        req.send_header('Content-Length', len(content))
+        req.write(str(content))
+
+    # IPreferencePanelProvider methods
+
+    def get_preference_panels(self, req):
+        yield 'userstyle', _("User Style")
+
+    def render_preference_panel(self, req, panel):
+        if req.method == 'POST':
+            if 'userstyle' in req.args:
+                if _path_css in req.session:
+                    req.session.pop(_path_css)
+                userstyle = req.args.get('userstyle')
+                if userstyle:
+                    req.session[_path_css] = userstyle
+                add_notice(req, _("Your preferences have been saved."))
+            if 'userscript' in req.args:
+                if _path_js in req.session:
+                    req.session.pop(_path_js)
+                userscript = req.args.get('userscript')
+                if userscript:
+                    req.session[_path_js] = userscript
+                add_notice(req, _("Your preferences have been saved."))
+            req.redirect(req.href.prefs(panel or None))
+
+        userstyle = req.session[_path_css] if _path_css in req.session else ''
+        userscript = req.session[_path_js] if _path_js in req.session else ''
+        data = {'userstyle': userstyle,
+                'userscript': userscript}
+        return 'prefs_userstyle.html', data
+
+    # ITemplateProvider methods
+
+    def get_htdocs_dirs(self):
+        return []
+
+    def get_templates_dirs(self):
+        return [ResourceManager().resource_filename('contextchrome',
+                                                    'templates')]
--- contextchromeplugin/0.12/setup.py
+++ contextchromeplugin/0.12/setup.py
@@ -5,16 +5,17 @@ from setuptools import setup, find_packages
 
 setup(
     name='ContextChrome',
-    version='0.5',
+    version='0.6',
     license='Modified BSD;  except xdr.js under MIT-LICENSE',
-    author= {'MATOBA Akihiro', 'Ryan J Ollos'},
+    author='MATOBA Akihiro',
     author_email='matobaa+trac-hacks@gmail.com',
     url='http://trac-hacks.org/wiki/matobaa',
     description='Add context-aware css attribute or style',
     zip_safe=True,
     packages=find_packages(exclude=['*.tests']),
     package_data={
-        'contextchrome': ['htdocs/*.png', 'htdocs/js/*.js', 'htdocs/css/*.css']
+        'contextchrome': ['htdocs/*.png', 'htdocs/js/*.js', 'htdocs/css/*.css',
+                          'templates/*.html']
     },
     entry_points={
         'trac.plugins': [
@@ -23,6 +24,7 @@ setup(
             'ContextChrome.TicketValidator = contextchrome.ticketvalidator',
             'ContextChrome.LinkDecorator = contextchrome.linkdeco',
             'ContextChrome.CrossOriginResourceSharingEnabler = contextchrome.cors',
+            'ContextChrome.UserStylesheet = contextchrome.prefs',
         ]
     },
 )
