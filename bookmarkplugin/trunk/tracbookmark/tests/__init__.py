22cd8ba610247903b8c315d35fc59140670fed3d
jun66j5 <jun66j5@7322e99d-02ea-0310-aa39-e9a107903beb>
2017-11-07 02:39:37 +0000

    BookmarkPlugin 1.0.0dev: refactor unit tests
    git-svn-id: https://trac-hacks.org/svn@16962 7322e99d-02ea-0310-aa39-e9a107903beb

--- bookmarkplugin/trunk/tracbookmark/tests/__init__.py
+++ bookmarkplugin/trunk/tracbookmark/tests/__init__.py
@@ -39,30 +39,32 @@ class BookmarkSystemTestCase(unittest.TestCase):
         self.env.reset_db()
         shutil.rmtree(self.env.path)
 
+    def _format_name(self, *args, **kwargs):
+        return self.bmsys._format_name(*args, **kwargs)
+
     def test_format_name_default_page(self):
-        data = self.bmsys._format_name(self.req, '/')
+        data = self._format_name(self.req, '/')
         self.assertEquals('wiki', data['class_'])
         self.assertEquals('/trac.cgi', data['href'])
         self.assertEquals('WikiStart', data['linkname'])
         self.assertEquals('', data['name'])
 
     def test_format_name_wiki(self):
-        data = self.bmsys._format_name(self.req, '/wiki/WikiStart')
+        data = self._format_name(self.req, '/wiki/WikiStart')
         self.assertEquals('wiki', data['class_'])
         self.assertEquals('/trac.cgi/wiki/WikiStart', data['href'])
         self.assertEquals('WikiStart', data['linkname'])
         self.assertEquals('', data['name'])
 
     def test_format_name_wiki_default(self):
-        data = self.bmsys._format_name(self.req, '/wiki')
+        data = self._format_name(self.req, '/wiki')
         self.assertEquals('wiki', data['class_'])
         self.assertEquals('/trac.cgi/wiki', data['href'])
         self.assertEquals('WikiStart', data['linkname'])
         self.assertEquals('', data['name'])
 
     def test_format_name_wiki_versioned(self):
-        data = self.bmsys._format_name(self.req,
-                                       '/wiki/Page/SubPage?version=42')
+        data = self._format_name(self.req, '/wiki/Page/SubPage?version=42')
         self.assertEquals('wiki', data['class_'])
         self.assertEquals('/trac.cgi/wiki/Page/SubPage?version=42',
                           data['href'])
@@ -77,7 +79,7 @@ class BookmarkSystemTestCase(unittest.TestCase):
         ticket.insert()
         tkt_id = ticket.id
 
-        data = self.bmsys._format_name(self.req, '/ticket/%d' % tkt_id)
+        data = self._format_name(self.req, '/ticket/%d' % tkt_id)
         self.assertEquals('new ticket', data['class_'])
         self.assertEquals('/trac.cgi/ticket/%d' % tkt_id, data['href'])
         self.assertEquals('#%d' % tkt_id, data['linkname'])
@@ -92,7 +94,7 @@ class BookmarkSystemTestCase(unittest.TestCase):
         ticket.insert()
         tkt_id = ticket.id
 
-        data = self.bmsys._format_name(self.req, '/ticket/%d' % tkt_id)
+        data = self._format_name(self.req, '/ticket/%d' % tkt_id)
         self.assertEquals('closed ticket', data['class_'])
         self.assertEquals('/trac.cgi/ticket/%d' % tkt_id, data['href'])
         self.assertEquals('#%d' % tkt_id, data['linkname'])
@@ -101,7 +103,7 @@ class BookmarkSystemTestCase(unittest.TestCase):
     def test_format_name_missing_ticket(self):
         tkt_id = 1
 
-        data = self.bmsys._format_name(self.req, '/ticket/%d' % tkt_id)
+        data = self._format_name(self.req, '/ticket/%d' % tkt_id)
         self.assertEquals('missing ticket', data['class_'])
         self.assertEquals(None, data['href'])
         self.assertEquals('#%d' % tkt_id, data['linkname'])
@@ -116,27 +118,27 @@ class BookmarkSystemTestCase(unittest.TestCase):
                 """)
             report_id = db.get_last_id(cursor, 'report')
 
-        data = self.bmsys._format_name(self.req, '/report/%d' % report_id)
+        data = self._format_name(self.req, '/report/%d' % report_id)
         self.assertEquals('report', data['class_'])
         self.assertEquals('/trac.cgi/report/%d' % report_id, data['href'])
         self.assertEquals('{%d}' % report_id, data['linkname'])
         self.assertEquals('Active Tickets', data['name'])
 
     def test_format_name_milestone(self):
-        data = self.bmsys._format_name(self.req, '/milestone/milestone1')
+        data = self._format_name(self.req, '/milestone/milestone1')
         self.assertEquals('milestone', data['class_'])
         self.assertEquals('/trac.cgi/milestone/milestone1', data['href'])
         self.assertEquals('milestone:milestone1', data['linkname'])
         self.assertEquals('', data['name'])
 
     def test_format_name_changeset(self):
-        data = self.bmsys._format_name(self.req, '/changeset/42/trunk')
+        data = self._format_name(self.req, '/changeset/42/trunk')
         self.assertEquals('changeset', data['class_'])
         self.assertEquals('/trac.cgi/changeset/42/trunk', data['href'])
         self.assertEquals('[42]', data['linkname'])
         self.assertEquals('Changeset 42 in trunk', data['name'])
 
-        data = self.bmsys._format_name(self.req, '/changeset/42')
+        data = self._format_name(self.req, '/changeset/42')
         self.assertEquals('changeset', data['class_'])
         self.assertEquals('/trac.cgi/changeset/42', data['href'])
         self.assertEquals('[42]', data['linkname'])
@@ -148,8 +150,8 @@ class BookmarkSystemTestCase(unittest.TestCase):
         page.save('user1', 'Create page', '::1')
         attachment = Attachment(self.env, 'wiki', 'WikiStart')
         attachment.insert('foo.txt', StringIO(''), 1)
-        data = self.bmsys._format_name(self.req,
-                                       '/attachment/wiki/WikiStart/foo.txt')
+        data = self._format_name(self.req,
+                                 '/attachment/wiki/WikiStart/foo.txt')
         self.assertEquals('attachment', data['class_'])
         self.assertEquals('/trac.cgi/attachment/wiki/WikiStart/foo.txt',
                           data['href'])
@@ -158,8 +160,8 @@ class BookmarkSystemTestCase(unittest.TestCase):
         self.assertEquals('', data['name'])
 
     def test_format_name_missing_attachment(self):
-        data = self.bmsys._format_name(self.req,
-                                       '/attachment/wiki/WikiStart/foo.txt')
+        data = self._format_name(self.req,
+                                 '/attachment/wiki/WikiStart/foo.txt')
         self.assertEquals('missing attachment', data['class_'])
         self.assertEquals(None, data['href'])
         self.assertEquals("Attachment 'foo.txt' in WikiStart",
@@ -167,8 +169,7 @@ class BookmarkSystemTestCase(unittest.TestCase):
         self.assertEquals('', data['name'])
 
     def test_format_name_attachment_list(self):
-        data = self.bmsys._format_name(self.req,
-                                       '/attachment/wiki/WikiStart')
+        data = self._format_name(self.req, '/attachment/wiki/WikiStart')
         self.assertEquals('attachment', data['class_'])
         self.assertEquals('/trac.cgi/attachment/wiki/WikiStart/',
                           data['href'])
