9fb86d3832afcbad3bbf95ab36839c8f3062f4e1
jun66j5 <jun66j5@7322e99d-02ea-0310-aa39-e9a107903beb>
2017-11-07 02:37:24 +0000

    BookmarkPlugin 1.0.0dev: make compatible with Python 2.5
    git-svn-id: https://trac-hacks.org/svn@16961 7322e99d-02ea-0310-aa39-e9a107903beb

--- bookmarkplugin/trunk/tracbookmark/__init__.py
+++ bookmarkplugin/trunk/tracbookmark/__init__.py
@@ -8,6 +8,8 @@
 # This software is licensed as described in the file COPYING, which
 # you should have received as part of this distribution.
 
+from __future__ import with_statement
+
 import fnmatch
 import pkg_resources
 import re
