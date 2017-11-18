bb671a7f1856d1b57f6463b206019955a18f98c9
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2017-11-18 10:03:48 +0000

    Add metadata
    git-svn-id: https://trac-hacks.org/svn@16974 7322e99d-02ea-0310-aa39-e9a107903beb

--- javadocwikimacro/0.11/javadoc.py
+++ javadocwikimacro/0.11/javadoc.py
@@ -24,7 +24,7 @@
 # If the second argument is ommitted the class name is used,
 # or in the case of a package the full package name is used.
 
-#-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------
 # This block can be edited to change the urls used for the Javadocs
 # Note: all Javadoc urls should end with a '/' or index.html
 
@@ -52,67 +52,70 @@ urls = {
     'javax.servlet': j2ee_url,
     'javax.transaction': j2ee_url,
     'javax.xml': j2ee_url,
-    
+
     'org.apache.commons.logging': commons_logging_url,
     'org.apache.commmons.beanutils': commons_beanutils_url,
-    
+
     'org.apache.commons.collections':  commons_collections_url,
     'org.apache.commons.lang':   commons_lang_url,
 
     'org.w3c.dom': xerces_url,
     'org.xml.sax': xerces_url
-    }
-#-------------------------------------------------------------------------------
+}
+
 from trac.core import *
+from trac.util.html import html as tag
 from urlparse import urljoin
 from string import ascii_uppercase
-from genshi.builder import tag
 from trac.wiki.macros import WikiMacroBase
 
-revison = "$Rev$"
-url = "$URL$"
+author = "Matthew Good"
+version = "1.0 ($Rev$)"
+license = "Beerware"
+url = "https://trac-hacks.org/wiki/JavadocWikiMacro"
+
 
 class javadocMacro(WikiMacroBase):
 
-	def best_match(self, value, items):
-		match = ''
-		for i in items:
-			if len(i) > len(match) and value.startswith(i):
-				match = i
+    def best_match(self, value, items):
+        match = ''
+        for i in items:
+            if len(i) > len(match) and value.startswith(i):
+                match = i
 
-		return match
+        return match
 
-	def base_url(self, package):
-		return urls[self.best_match(package, urls.keys())]
-    
-	def package_path(self, package):
-		return package.replace('.', '/') + '/'
-    
-	def type_path(self, package, clss):
-		return self.package_path(package) + (clss and (clss + '.html') or 'package-summary.html')
-    
-	def javadoc_url(self, package, clss):
-		return urljoin(self.base_url(package), self.type_path(package, clss))
-    
-	def split_at(self, a, index):
-		return (a[:index], a[index + 1:])
-    
-	def split_type(self, value):
-		package, clss = self.split_at(value, value.rfind('.'))
-		if clss[0] in ascii_uppercase:
-			return (package, clss)
-		else:
-			return (value, None)
-    
-	def link(self, href, text):
-		return '<a href="%s" class="javadoc">%s</a>' % (href, text)
-    
-	def javadoc_link(self, value, linktext):
-		package, clss = self.split_type(value)
-		return self.link(self.javadoc_url(package, clss), linktext or clss or package)
+    def base_url(self, package):
+        return urls[self.best_match(package, urls.keys())]
 
-	def expand_macro(self, formatter, name, args):
-		txt = args.split(',', 1)
-		javatype = txt[0]
-		linktext = len(txt) > 1 and txt[1].strip() or None
-		return self.javadoc_link(javatype, linktext)
+    def package_path(self, package):
+        return package.replace('.', '/') + '/'
+
+    def type_path(self, package, clss):
+        return self.package_path(package) + (clss and (clss + '.html') or 'package-summary.html')
+
+    def javadoc_url(self, package, clss):
+        return urljoin(self.base_url(package), self.type_path(package, clss))
+
+    def split_at(self, a, index):
+        return (a[:index], a[index + 1:])
+
+    def split_type(self, value):
+        package, clss = self.split_at(value, value.rfind('.'))
+        if clss[0] in ascii_uppercase:
+            return (package, clss)
+        else:
+            return (value, None)
+
+    def link(self, href, text):
+        return '<a href="%s" class="javadoc">%s</a>' % (href, text)
+
+    def javadoc_link(self, value, linktext):
+        package, clss = self.split_type(value)
+        return self.link(self.javadoc_url(package, clss), linktext or clss or package)
+
+    def expand_macro(self, formatter, name, args):
+        txt = args.split(',', 1)
+        javatype = txt[0]
+        linktext = len(txt) > 1 and txt[1].strip() or None
+        return self.javadoc_link(javatype, linktext)
