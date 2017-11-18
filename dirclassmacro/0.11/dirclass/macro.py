e2add177d1a957e28ca14a4ae66d710dc3ce7943
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2017-11-18 09:49:02 +0000

    Conform to PEP8
    git-svn-id: https://trac-hacks.org/svn@16969 7322e99d-02ea-0310-aa39-e9a107903beb

--- dirclassmacro/0.11/dirclass/macro.py
+++ dirclassmacro/0.11/dirclass/macro.py
@@ -1,19 +1,23 @@
+# -*- coding: utf-8 -*-
+#
+
+from genshi import Markup
 from trac.core import implements
 from trac.web.chrome import ITemplateProvider, add_stylesheet
 from trac.wiki.macros import WikiMacroBase
-from genshi import Markup
-from pkg_resources import resource_filename
+
 
 class RTLMacro(WikiMacroBase):
     """Emits dir-aligned div blocks."""
-    
+
     implements(ITemplateProvider)
-    
-    # ITemplateProvider
+
+    # ITemplateProvider methods
     def get_templates_dirs(self):
         return []
-        
+
     def get_htdocs_dirs(self):
+        from pkg_resources import resource_filename
         yield 'dirclass', resource_filename(__name__, 'htdocs')
 
     # IWikiMacroProvier methods
@@ -21,10 +25,9 @@ class RTLMacro(WikiMacroBase):
         add_stylesheet(formatter.req, 'dirclass/css/dirclass.css')
         return Markup('<div class="rtl">')
 
+
 class LTRMacro(WikiMacroBase):
     """End a dir-aligned div block."""
-    
+
     def expand_macro(self, formatter, name, content):
         return Markup('</div>')
-
-
--- dirclassmacro/0.12/dirclass/macro.py
+++ dirclassmacro/0.12/dirclass/macro.py
@@ -1,30 +1,35 @@
+# -*- coding: utf-8 -*-
+#
+
 from trac.core import implements
+from trac.util.html import Markup
 from trac.web.chrome import ITemplateProvider, add_stylesheet
 from trac.wiki.macros import WikiMacroBase
-from genshi import Markup
-from pkg_resources import resource_filename
+
 
 class RTLMacro(WikiMacroBase):
     """Emits dir-aligned div blocks."""
-    
+
     implements(ITemplateProvider)
-    
-    # ITemplateProvider
+
+    # ITemplateProvider methods
+
     def get_templates_dirs(self):
         return []
-        
+
     def get_htdocs_dirs(self):
+        from pkg_resources import resource_filename
         yield 'dirclass', resource_filename(__name__, 'htdocs')
 
     # IWikiMacroProvier methods
+
     def expand_macro(self, formatter, name, content):
         add_stylesheet(formatter.req, 'dirclass/css/dirclass.css')
         return Markup('<div class="rtl">')
 
+
 class LTRMacro(WikiMacroBase):
     """End a dir-aligned div block."""
-    
+
     def expand_macro(self, formatter, name, content):
         return Markup('</div>')
-
-
