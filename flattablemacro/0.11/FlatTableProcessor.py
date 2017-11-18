5ab626710d4eb4b4299451deee5afa7da03dae03
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2017-11-18 09:59:43 +0000

    Add metadata
    git-svn-id: https://trac-hacks.org/svn@16972 7322e99d-02ea-0310-aa39-e9a107903beb

--- flattablemacro/0.11/FlatTableProcessor.py
+++ flattablemacro/0.11/FlatTableProcessor.py
@@ -1,38 +1,42 @@
-## -*- coding: utf-8 -*-
-##
-##  Copyright (c) 2008 Ashish Kulkarni <kulkarni.ashish@gmail.com>
-##
-##  This software is provided 'as-is', without any express or implied
-##  warranty.  In no event will the authors be held liable for any damages
-##  arising from the use of this software.
-##
-##  Permission is granted to anyone to use this software for any purpose,
-##  including commercial applications, and to alter it and redistribute it
-##  freely, subject to the following restrictions:
-##
-##  1. The origin of this software must not be misrepresented; you must not
-##     claim that you wrote the original software. If you use this software
-##     in a product, an acknowledgment in the product documentation would be
-##     appreciated but is not required.
-##  2. Altered source versions must be plainly marked as such, and must not be
-##     misrepresented as being the original software.
-##  3. This notice may not be removed or altered from any source distribution.
+# -*- coding: utf-8 -*-
+#
+#  Copyright (c) 2008 Ashish Kulkarni <kulkarni.ashish@gmail.com>
+#
+#  This software is provided 'as-is', without any express or implied
+#  warranty.  In no event will the authors be held liable for any damages
+#  arising from the use of this software.
+#
+#  Permission is granted to anyone to use this software for any purpose,
+#  freely, subject to the following restrictions:
+#
+#  1. The origin of this software must not be misrepresented; you must not
+#     claim that you wrote the original software. If you use this software
+#     in a product, an acknowledgment in the product documentation would be
+#     appreciated but is not required.
+#  2. Altered source versions must be plainly marked as such, and must not be
+#     misrepresented as being the original software.
+#  3. This notice may not be removed or altered from any source distribution.
 
-from trac.util.html import Markup
+import inspect
+import re
+
+from trac.util.html import Markup, html as tag
 from trac.wiki import format_to_oneliner
 from trac.wiki.macros import WikiMacroBase
-from genshi.builder import tag
 
-import re, StringIO, inspect
-
-revison = "$Rev$"
-url     = "$URL$"
+author = "Ashish Kulkarni"
+version = "1.0 ($Rev$)"
+license = "BSD"
+url = "https://trac-hacks.org/wiki/FlatTableMacro"
 
 KEY_REGEX    = re.compile('^([^\s]+.*)\:\s*$', re.I)
 ATTRIB_REGEX = re.compile('^(\s+)@([^@\s]+?)\:\s*(.+)?$', re.I)
 
+
 class FlatTableProcessor(WikiMacroBase):
-    """WikiProcessor for displaying a table with the markup being entered in a flat form."""
+    """WikiProcessor for displaying a table with the markup being entered
+    in a flat form.
+    """
 
     CONFIG_KEY = 'flat-table'
     DESC_KEY   = '.description'
