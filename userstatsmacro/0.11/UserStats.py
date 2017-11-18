2a3a605e38abd9add68fe0bb0649059fa29e0e6c
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2017-11-18 10:32:15 +0000

    Add metadata
    git-svn-id: https://trac-hacks.org/svn@16991 7322e99d-02ea-0310-aa39-e9a107903beb

--- userstatsmacro/0.11/UserStats.py
+++ userstatsmacro/0.11/UserStats.py
@@ -1,18 +1,17 @@
 # -*- coding: utf-8 -*-
 
 from datetime import datetime
-import inspect
-import re
-import sys
 import time
 
 from trac.core import *
-from trac.wiki.api import IWikiMacroProvider
 from trac.wiki.formatter import format_to_html
 from trac.wiki.macros import WikiMacroBase
 
-revision="$Rev$"
-url="http://trac-hacks.org/wiki/UserStatsMacro"
+author = "Pedro"
+version = "1.0 ($Rev$)"
+license = "GPL"
+url = "https://trac-hacks.org/wiki/UserStatsMacro"
+
 
 class UserStatsMacro(WikiMacroBase):
     """Produces a table with username, last login, elapsed time since last
@@ -32,7 +31,7 @@ class UserStatsMacro(WikiMacroBase):
         content = '<div class="component-list">%s</div>' % content
         return content
 
-    def get_user_last_login(self,user):
+    def get_user_last_login(self, user):
 
         cursor = self.env.get_db_cnx().cursor()
         cursor.execute("SELECT last_visit FROM session "
@@ -47,10 +46,10 @@ class UserStatsMacro(WikiMacroBase):
         now = datetime.fromtimestamp(time.time())
         how_long = now - last
         last_date = time.strftime("%Y/%m/%d %H:%M:%S", when)
-        if how_long.days>0:
+        if how_long.days > 0:
             return "|| [query:?status=assigned&status=new&status=reopened&group=milestone&order=priority&col=id&col=summary&col=status&col=owner&col=type&col=priority&col=component&col=due_close&type=task&owner=%s %s] || %s || %s day(s)||\n" % (user, user, last_date, how_long.days)
         else:
-            return "|| [query:?status=assigned&status=new&status=reopened&group=milestone&order=priority&col=id&col=summary&col=status&col=owner&col=type&col=priority&col=component&col=due_close&type=task&owner=%s %s]|| %s || <24h ||\n" % (user, user,last_date)
+            return "|| [query:?status=assigned&status=new&status=reopened&group=milestone&order=priority&col=id&col=summary&col=status&col=owner&col=type&col=priority&col=component&col=due_close&type=task&owner=%s %s]|| %s || <24h ||\n" % (user, user, last_date)
 
     def get_users_last_login(self):
 
