9ddc1699428754fc0c76d7674042e2507078e951
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2017-11-30 02:03:51 +0000

    Remove dependency that is handled by Trac install
    git-svn-id: https://trac-hacks.org/svn@17006 7322e99d-02ea-0310-aa39-e9a107903beb

--- precodebrowserplugin/0.11/setup.py
+++ precodebrowserplugin/0.11/setup.py
@@ -8,17 +8,12 @@ VERSION = '1'
 setup(
     name=PACKAGE,
     version=VERSION,
-    url='http://trac-hacks.org/wiki/%s' % PACKAGE,
     author='Katherine Flavel',
     author_email='kate@elide.org',
     description='Replace tables output in the source browser with simple <pre> file listings',
+    url='http://trac-hacks.org/wiki/%s' % PACKAGE,
 
     license='Public Domain',
-    install_requires=["Genshi >= 0.5dev"],
-    dependency_links=[
-        'http://svn.edgewall.org/repos/genshi/trunk#egg = Genshi-0.5dev'],
-
     packages=['precodebrowser'],
-
     entry_points={'trac.plugins': '%s = precodebrowser' % PACKAGE}
 )
