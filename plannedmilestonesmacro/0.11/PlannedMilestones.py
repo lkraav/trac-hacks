ad1f930efbcdb40c64454ed9a6971e9c8cc51311
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2017-11-30 04:07:55 +0000

    Conform to PEP8
    git-svn-id: https://trac-hacks.org/svn@17008 7322e99d-02ea-0310-aa39-e9a107903beb

--- plannedmilestonesmacro/0.11/PlannedMilestones.py
+++ plannedmilestonesmacro/0.11/PlannedMilestones.py
@@ -1,18 +1,20 @@
 # -*- coding: utf-8 -*-
 
-#TODO: add header
+# TODO: add header
 
-import re    
-from datetime import datetime
+import re
 from StringIO import StringIO
+from datetime import datetime
+
 from trac.ticket import Milestone
 from trac.util.datefmt import format_date, to_timestamp
 from trac.util.html import Markup
 from trac.wiki.api import parse_args
 from trac.wiki.macros import WikiMacroBase
 
-revision="$Rev$"
-url="http://trac-hacks.org/wiki/PlannedMilestonesMacro"
+revision = "$Rev$"
+url = "http://trac-hacks.org/wiki/PlannedMilestonesMacro"
+
 
 class PlannedMilestonesMacro(WikiMacroBase):
     """
@@ -20,37 +22,39 @@ class PlannedMilestonesMacro(WikiMacroBase):
 
     {{{
         [[PlannedMilestones()]]
-	[[PlannedMilestone(N)]]
-    }}}    
-    """       
+        [[PlannedMilestone(N)]]
+    }}}
+    """
+
     def expand_macro(self, formatter, name, content):
-        
-        arg,kwarg = parse_args(content)
-        
+
+        arg, kwarg = parse_args(content)
+
         includepattern = kwarg.get('include', '')
         #excludepattern = kwarg.get('exclude', '')
         length = int(kwarg.get('max', -1))
         ignorenoduedate = kwarg.get('ignore') == 'noduedate' or None
-        
-        if length==-1:
+
+        if length == -1:
             length = None
-        
+
         out = StringIO()
-        
+
         include = re.compile(includepattern)
         #exclude = re.compile(excludepattern)
 
         milestones = []
-        
+
         for milestone in Milestone.select(self.env, include_completed=False):
-            if include.match(milestone.name): # and not exclude.match(milestone.name):
+            # and not exclude.match(milestone.name):
+            if include.match(milestone.name):
                 milestones.append(milestone)
-                
+
         out.write('<ul>\n')
         for milestone in milestones[0:length]:
 
             if milestone.due:
-                #TODO: add one day to tdelta
+                # TODO: add one day to tdelta
                 tdelta = (to_timestamp(milestone.due) -
                           to_timestamp(datetime.now(formatter.req.tz)))
                 if tdelta > 0:
@@ -62,11 +66,11 @@ class PlannedMilestonesMacro(WikiMacroBase):
                 date = Markup('<i>(Unspecified)</i>')
             else:
                 date = None
-                
-            if date:        
-                out.write('<li>%s - <a href="%s">%s</a></li>\n' % 
+
+            if date:
+                out.write('<li>%s - <a href="%s">%s</a></li>\n' %
                           (date, self.env.href.milestone(milestone.name),
                            milestone.name))
-            
+
         out.write('</ul>\n')
         return Markup(out.getvalue())
