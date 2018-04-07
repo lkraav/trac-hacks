9c7380ba9e43225aae04d0394372a191026cebf0
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2018-04-07 19:12:22 +0000

    TracTicketValidator 0.2: Conform to PEP8
    git-svn-id: https://trac-hacks.org/svn@17103 7322e99d-02ea-0310-aa39-e9a107903beb

--- tracticketvalidatorplugin/0.11/setup.py
+++ tracticketvalidatorplugin/0.11/setup.py
@@ -1,23 +1,27 @@
 from setuptools import find_packages, setup
 
 setup(
-    name = 'TracTicketValidator',
-    version = '0.1',
-    packages = ['ticketvalidator'],
-    package_data = { 'ticketvalidator': [ '*.txt' ] },
-
-    author = "Richard Liao",
-    author_email = 'richard.liao.i@gmail.com',
-    maintainer = 'Richard Liao',
-    maintainer_email = "richard.liao.i@gmail.com",
-    description = "Ticket validator plugin for Trac.",
-    license = "BSD",
-    keywords = "trac ticket validator",
-    url = "http://trac-hacks.org/wiki/TracTicketValidatorPlugin",
-    classifiers = [
+    name='TracTicketValidator',
+    version='0.2',
+    author='Richard Liao',
+    author_email='richard.liao.i@gmail.com',
+    maintainer='Richard Liao',
+    maintainer_email='richard.liao.i@gmail.com',
+    description="Ticket validator plugin for Trac.",
+    license='BSD',
+    keywords='trac ticket validator',
+    url='https://trac-hacks.org/wiki/TracTicketValidatorPlugin',
+    packages=['ticketvalidator'],
+    package_data={
+        'ticketvalidator': ['*.txt']
+    },
+    classifiers=[
         'Framework :: Trac',
     ],
-    
-    install_requires = [],
-    entry_points = {'trac.plugins': ['tracticketvalidator = ticketvalidator.ticketvalidator']},
+    install_requires=['Trac'],
+    entry_points={
+        'trac.plugins': [
+            'tracticketvalidator = ticketvalidator.ticketvalidator'
+        ]
+    },
 )
--- tracticketvalidatorplugin/0.11/ticketvalidator/ticketvalidator.py
+++ tracticketvalidatorplugin/0.11/ticketvalidator/ticketvalidator.py
@@ -1,37 +1,34 @@
 # -*- coding: utf-8 -*-
-#----------------------------------------------------------------------------
+#
 # Name:         ticketvalidator.py
 # Purpose:      The ticket validator Trac plugin handler module
 #
 # Author:       Richard Liao <richard.liao.i@gmail.com>
 #
-#----------------------------------------------------------------------------
-from trac.config import BoolOption, IntOption, ListOption
-
-from trac.core import *
-from trac.ticket.api import ITicketManipulator
-from trac.ticket.model import Ticket
-from trac.util.html import html, Markup
 
 import re
 
+from trac.config import BoolOption, ListOption
+from trac.core import Component, implements
+from trac.ticket.api import ITicketManipulator
+
+
 class TicketsValidator(Component):
     """ Validate ticket fields.
-        Only authenticated user or anonymous with valid email address can submit.
+
+    Only authenticated user or anonymous with valid email address can submit.
     """
-    
+
     implements(ITicketManipulator)
 
-    validate_author = BoolOption('ticketvalidator', 'validate_author', 'false', '')
+    validate_author = BoolOption('ticketvalidator', 'validate_author', False)
     validates = ListOption('ticketvalidator', 'validates', [])
 
     # ITicketManipulator methods
     def prepare_ticket(self, req, ticket, fields, actions):
         pass
-        
-    def validate_ticket(self, req, ticket):
-        result = None
 
+    def validate_ticket(self, req, ticket):
         if self.validate_author:
             errorMessage = self._validateAuthor(req, ticket)
             if errorMessage:
@@ -42,54 +39,58 @@ class TicketsValidator(Component):
             if errorMessage:
                 yield None, errorMessage
 
+    # Private methods
 
-    # private methods
     def _validateAuthor(self, req, ticket):
         # get error message
-        errorMessage = self.config.get('ticketvalidator', 'validate_author.tip', "Ticket field is not filled correctly.")
+        errorMessage = self.config.get('ticketvalidator',
+                                       'validate_author.tip',
+                                       "Ticket field is not filled correctly.")
 
         # is logged in?
         if req.authname == 'anonymous':
             isLoggedIn = False
         else:
-           isLoggedIn = True
-           
+            isLoggedIn = True
+
         # get author
         author = req.args.get("author")
         reporter = ticket.values.get("reporter")
-        
+
         # validate author
         if not isLoggedIn:
             if ticket.exists:
                 if not self._isValidateEmail(author):
                     return errorMessage
             else:
-                if not self._isValidateEmail(author) and not self._isValidateEmail(reporter):
+                if not self._isValidateEmail(author) and \
+                        not self._isValidateEmail(reporter):
                     return errorMessage
 
     def _validateField(self, req, ticket, field):
         # get error message
-        errorMessage = self.config.get('ticketvalidator', '%s.tip' % field, "Ticket field is not filled correctly.")
-        
+        errorMessage = self.config.get('ticketvalidator', '%s.tip' % field,
+                                       "Ticket field is not filled correctly.")
+
         # validate field
         rule = self.config.get('ticketvalidator', '%s.rule' % field, None)
 
         fieldValue = None
-        if ticket.values.has_key(field):
+        if field in ticket.values:
             fieldValue = ticket.values.get(field)
-        elif req.args.has_key(field):
+        elif field in req.args:
             fieldValue = req.args.get(field)
-        
+
         if fieldValue is not None:
-            if (not fieldValue) or (rule and re.match(rule, fieldValue) == None):
+            if not fieldValue or (rule and re.match(rule, fieldValue) is None):
                 return errorMessage
-                
+
     def _isValidateEmail(self, email):
         if not email:
             return False
 
         if len(email) > 7:
-            if re.match("^[a-zA-Z0-9._%-]+@[a-zA-Z0-9._%-]+\.[a-zA-Z]{2,6}$", email) != None:
+            if re.match("^[a-zA-Z0-9._%-]+@[a-zA-Z0-9._%-]+\.[a-zA-Z]{2,6}$",
+                        email) is not None:
                 return True
         return False
-
