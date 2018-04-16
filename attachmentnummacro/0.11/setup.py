102bfd17845895156dc0a4a2ea1a9e782ff6574c
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2018-04-16 19:39:49 +0000

    TracAttachmentNumMacro 0.8: Conform to PEP8
    git-svn-id: https://trac-hacks.org/svn@17134 7322e99d-02ea-0310-aa39-e9a107903beb

--- attachmentnummacro/0.11/setup.py
+++ attachmentnummacro/0.11/setup.py
@@ -3,24 +3,26 @@
 
 from setuptools import setup
 
-__url__      = ur"$URL$"[6:-2]
-__author__   = ur"$Author$"[9:-2]
+__url__ = ur"$URL$"[
+    6:-2]
+__author__ = ur"$Author$"[9:-2]
 __revision__ = int("0" + ur"$Rev$"[6:-2])
-__date__     = ur"$Date$"[7:-2]
-
+__date__ = ur"$Date$"[7:-2]
 
 
 setup(
-    name         = 'TracAttachmentNumMacro',
-    version      = '0.7',
-    packages     = ['tracattachmentnum'],
-    author       = 'Martin Scharrer',
-    author_email = 'martin@scharrer-online.de',
-    description  = "Trac Macro to link to local attachments by number.",
-    url          = 'http://www.trac-hacks.org/wiki/AttachmentNumMacro',
-    license      = 'GPLv3',
-    zip_safe     = False,
-    keywords     = 'trac attachment number macro',
-    classifiers  = ['Framework :: Trac'],
-    entry_points = {'trac.plugins': ['tracattachmentnum.attachmentnum = tracattachmentnum.attachmentnum']}
+    name='TracAttachmentNumMacro',
+    version='0.8',
+    packages=['tracattachmentnum'],
+    author='Martin Scharrer',
+    author_email='martin@scharrer-online.de',
+    description="Trac Macro to link to local attachments by number.",
+    url='https://www.trac-hacks.org/wiki/AttachmentNumMacro',
+    license='GPLv3',
+    zip_safe=False,
+    keywords='trac attachment number macro',
+    install_requires=['Trac'],
+    classifiers=['Framework :: Trac'],
+    entry_points={'trac.plugins': [
+        'tracattachmentnum.attachmentnum = tracattachmentnum.attachmentnum']}
 )
--- attachmentnummacro/0.11/tracattachmentnum/attachmentnum.py
+++ attachmentnummacro/0.11/tracattachmentnum/attachmentnum.py
@@ -5,16 +5,12 @@ a plugin for Trac
 http://trac.edgewall.org
 """
 
-__url__      = ur"$URL$"[6:-2]
-__author__   = ur"$Author$"[9:-2]
-__revision__ = int("0" + ur"$Rev$"[6:-2])
-__date__     = ur"$Date$"[7:-2]
+from trac.core import Component, TracError, implements
+from trac.wiki.api import IWikiMacroProvider
+from trac.wiki.formatter import format_to_oneliner
+from trac.wiki.macros import parse_args
+from trac.config import Option, BoolOption
 
-from  trac.core            import  *
-from  trac.wiki.api        import  IWikiMacroProvider
-from  trac.wiki.formatter  import  format_to_oneliner
-from  trac.wiki.macros     import  parse_args
-from  trac.config          import  Option, BoolOption
 
 class AttachmentNumMacro(Component):
     """
@@ -31,55 +27,56 @@ Examples:
 [[AttachmentNum(1,format=short)]]   # First attachment, hyper-linked filename is printed only, result: "first.doc".
 }}}
     """
-    implements ( IWikiMacroProvider )
+    implements(IWikiMacroProvider)
 
-    raw    = BoolOption('attachmentnum', 'raw', False, 'Default value for argument `raw`')
-    format =     Option('attachmentnum', 'format', None, 'Default value for argument `format`')
+    raw = BoolOption('attachmentnum', 'raw', False,
+                     'Default value for argument `raw`')
 
-    ### methods for IWikiMacroProvider
+    format = Option('attachmentnum', 'format', None,
+                    'Default value for argument `format`')
+
+    # methods for IWikiMacroProvider
     def get_macros(self):
-      yield 'AttachmentNum'
+        yield 'AttachmentNum'
 
     def get_macro_description(self, name):
-      return self.__doc__
+        return self.__doc__
 
     def expand_macro(self, formatter, name, content):
         largs, kwargs = parse_args(content)
         try:
             num = int(largs[0]) - 1
             if num < 0:
-              raise Exception
+                raise Exception
         except:
             raise TracError("Argument must be a positive integer!")
 
-        option = self.env.config.get
         if 'raw' in kwargs:
             vraw = kwargs['raw'].lower()
-            if vraw in ('yes','true','1','on'):
+            if vraw in ('yes', 'true', '1', 'on'):
                 raw = True
-            elif vraw in ('no','false','0','off'):
+            elif vraw in ('no', 'false', '0', 'off'):
                 raw = False
         else:
             raw = self.raw
 
-        res  = formatter.resource
-        id   = res.id
+        res = formatter.resource
+        id = res.id
         type = res.realm
 
         db = self.env.get_db_cnx()
         cursor = db.cursor()
-        cursor.execute( "SELECT filename FROM attachment WHERE type=%s " \
-                        "AND id=%s ORDER BY time", (type,id) )
+        cursor.execute("SELECT filename FROM attachment WHERE type=%s "
+                       "AND id=%s ORDER BY time", (type, id))
 
         attmnts = cursor.fetchall()
         if len(attmnts) < num + 1 or not attmnts[num] or not attmnts[num][0]:
-            raise TracError("Attachment #%i doesn't exists!" % (num+1))
+            raise TracError("Attachment #%i doesn't exists!" % (num + 1))
         filename = attmnts[num][0]
 
         wikilink = "attachment:'" + filename + "'"
         if raw:
             wikilink = "raw-" + wikilink
         if kwargs.get('format', self.format) == 'short':
-            wikilink = "[%s %s]" % (wikilink,filename)
+            wikilink = "[%s %s]" % (wikilink, filename)
         return format_to_oneliner(self.env, formatter.context, wikilink)
-
