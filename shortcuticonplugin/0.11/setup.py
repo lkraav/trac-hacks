526c6b01139e05ff37c21bff850e6ae13d294b99
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2018-04-16 19:44:04 +0000

    TracShortcutIconPlugin 0.3: Conform to PEP8
    git-svn-id: https://trac-hacks.org/svn@17135 7322e99d-02ea-0310-aa39-e9a107903beb

--- shortcuticonplugin/0.11/setup.py
+++ shortcuticonplugin/0.11/setup.py
@@ -3,23 +3,19 @@
 
 from setuptools import setup
 
-__url__      = ur"$URL$"[6:-2]
-__author__   = ur"$Author$"[9:-2]
-__revision__ = int("0" + ur"$Rev$"[6:-2])
-__date__     = ur"$Date$"[7:-2]
-
-
 setup(
-    name = 'TracShortcutIconPlugin',
-    version = '0.2',
-    packages = ['tracshortcuticon'],
-    author = 'Martin Scharrer',
-    author_email = 'martin@scharrer-online.de',
-    description = "Configurables shortcut icons for Trac.",
-    url = 'http://www.trac-hacks.org/wiki/ShortcutIconPlugin',
-    license      = 'GPLv3',
-    zip_safe     = False,
-    keywords = 'trac plugin favicon shortcuticon',
-    classifiers = ['Framework :: Trac'],
-    entry_points = {'trac.plugins': ['tracshortcuticon.plugin = tracshortcuticon.plugin']}
+    name='TracShortcutIconPlugin',
+    version='0.3',
+    packages=['tracshortcuticon'],
+    author='Martin Scharrer',
+    author_email='martin@scharrer-online.de',
+    description="Configurables shortcut icons for Trac.",
+    url='https://www.trac-hacks.org/wiki/ShortcutIconPlugin',
+    license='GPLv3',
+    zip_safe=False,
+    keywords='trac plugin favicon shortcuticon',
+    install_requires=['Trac'],
+    classifiers=['Framework :: Trac'],
+    entry_points={'trac.plugins': [
+        'tracshortcuticon.plugin = tracshortcuticon.plugin']}
 )
--- shortcuticonplugin/0.11/tracshortcuticon/plugin.py
+++ shortcuticonplugin/0.11/tracshortcuticon/plugin.py
@@ -4,91 +4,92 @@
     $HeadURL$
 
     This is Free Software under the GPL v3!
-""" 
+"""
 
-__url__      = ur"$URL$"[6:-2]
-__author__   = ur"$Author$"[9:-2]
-__revision__ = int("0" + ur"$Rev$"[6:-2])
-__date__     = ur"$Date$"[7:-2]
-
-import os.path
-from  os               import  access,R_OK
-from  trac.core        import  *
-from  trac.web.api     import  IRequestHandler,IRequestFilter,RequestDone
-from  trac.web.chrome  import  add_link
-from  trac.config      import  Option, BoolOption
-try: # PathOption exists only since Trac 0.11.5
-  from  trac.config    import  PathOption
+import os
+from trac.core import Component, implements
+from trac.web.api import IRequestHandler, IRequestFilter, RequestDone
+from trac.web.chrome import add_link
+from trac.config import Option, BoolOption
+try:  # PathOption exists only since Trac 0.11.5
+    from trac.config import PathOption
 except:
-  from  trac.config    import  Option  as  PathOption
+    from trac.config import Option as PathOption
+
 
 class ShortcutIconRequestPlugin(Component):
     """Implements the /favicon.ico handler."""
-    implements ( IRequestHandler, IRequestFilter )
+    implements(IRequestFilter, IRequestHandler)
 
-    iconpath  = PathOption('shortcuticon', 'iconpath', None, "Filesystem path of shortcut icon")
-    _mimetype =     Option('shortcuticon', 'mimetype', None, "Mimetype of shortcut icon")
-    ishandler = BoolOption('shortcuticon', 'handler', True, "Handler for '/favicon.ico'")
-    isfilter  = BoolOption('shortcuticon', 'linkheader', ishandler, "Add 'link' tags for icon into HTML pages")
+    iconpath = PathOption('shortcuticon', 'iconpath', None,
+                          "Filesystem path of shortcut icon")
 
-    path    = r'/favicon.ico'
+    _mimetype = Option('shortcuticon', 'mimetype', None,
+                       "Mimetype of shortcut icon")
+
+    ishandler = BoolOption('shortcuticon', 'handler',
+                           True, "Handler for '/favicon.ico'")
+
+    isfilter = BoolOption('shortcuticon', 'linkheader', ishandler,
+                          "Add 'link' tags for icon into HTML pages")
+
+    path = r'/favicon.ico'
 
     exttypes = {
-              '.ico' : 'image/x-icon',
-              '.png' : 'image/png',
-              '.jpg' : 'image/jpg',
-              '.gif' : 'image/gif',
-            }
+        '.ico': 'image/x-icon',
+        '.png': 'image/png',
+        '.jpg': 'image/jpg',
+        '.gif': 'image/gif',
+    }
 
     def __init__(self):
         if self._mimetype:
-          self.mimetype = self._mimetype
+            self.mimetype = self._mimetype
         else:
-          try:
-            iconpath = self.iconpath
-            idx = iconpath.rindex('.', -4)
-            self.mimetype = self.exttypes[ iconpath[idx:] ]
-            self.mimetype = 'image/x-icon'
-          except:
-            self.mimetype = 'image/x-icon'
-
+            try:
+                iconpath = self.iconpath
+                idx = iconpath.rindex('.', -4)
+                self.mimetype = self.exttypes[iconpath[idx:]]
+                self.mimetype = 'image/x-icon'
+            except:
+                self.mimetype = 'image/x-icon'
 
     # IRequestHandler methods
+
     def match_request(self, req):
         if not self.ishandler:
             return False
         return req.path_info == self.path \
             or req.path_info == req.base_path + self.path
 
-
     def process_request(self, req):
         iconpath = self.iconpath
-        iconok   = False
+        iconok = False
         if iconpath:
-            if os.path.isfile(iconpath) and os.access(iconpath,R_OK):
+            if os.path.isfile(iconpath) and os.access(iconpath, os.R_OK):
                 iconok = True
             else:
-                self.env.log.warning("Icon '%s' isn't a readable file!" % iconpath)
+                self.log.warning(
+                    "Icon '%s' isn't a readable file!", iconpath)
         else:
-            self.env.log.warning("No icon file configured!")
+            self.log.warning("No icon file configured!")
 
         if iconok:
-            req.send_file( self.iconpath, self.mimetype )
+            req.send_file(self.iconpath, self.mimetype)
         else:
             req.send_response(404)
             req.end_headers()
         raise RequestDone
 
+    # IRequestFilter methods
 
     def pre_process_request(self, req, handler):
         return handler
-
 
     def post_process_request(self, req, template, data, content_type):
         if self.isfilter:
             path = req.base_path + self.path
             add_link(req, 'shortcut icon', path, None, self.mimetype)
-            add_link(req, 'icon',          path, None, self.mimetype)
+            add_link(req, 'icon', path, None, self.mimetype)
 
-        return (template, data, content_type)
-
+        return template, data, content_type
