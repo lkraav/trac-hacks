9091e888e89ac3e3a4393b8a450711152fafef1d
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2018-04-16 20:45:09 +0000

    TracGoogleSitemapPlugin 1.1: Conform to PEP8
    git-svn-id: https://trac-hacks.org/svn@17141 7322e99d-02ea-0310-aa39-e9a107903beb

--- googlesitemapplugin/0.11/setup.py
+++ googlesitemapplugin/0.11/setup.py
@@ -3,23 +3,21 @@
 
 from setuptools import setup
 
-__url__      = ur"$URL$"[6:-2]
-__author__   = ur"$Author$"[9:-2]
-__revision__ = int("0" + ur"$Rev$"[6:-2])
-__date__     = ur"$Date$"[7:-2]
-
-
 setup(
-    name = 'TracGoogleSitemapPlugin',
-    version = '1.0',
-    packages = ['tracgooglesitemap'],
-    author = 'Martin Scharrer',
-    author_email = 'martin@scharrer-online.de',
-    description = "Plugin to generate Google Sitemaps (version for Trac 0.11)",
-    url = 'http://www.trac-hacks.org/wiki/GoogleSitemapPlugin',
-    license      = 'GPLv3',
-    zip_safe     = False,
-    keywords = 'trac google sitemap plugin',
-    classifiers = ['Framework :: Trac'],
-    entry_points = {'trac.plugins': ['tracgooglesitemap.plugin = tracgooglesitemap.plugin', 'tracgooglesitemap.notify = tracgooglesitemap.notify']}
+    name='TracGoogleSitemapPlugin',
+    version='1.1',
+    packages=['tracgooglesitemap'],
+    author='Martin Scharrer',
+    author_email='martin@scharrer-online.de',
+    description="Plugin to generate Google Sitemaps (version for Trac 0.11)",
+    url='https://www.trac-hacks.org/wiki/GoogleSitemapPlugin',
+    license='GPLv3',
+    zip_safe=False,
+    keywords='trac google sitemap plugin',
+    install_requires=['Trac'],
+    classifiers=['Framework :: Trac'],
+    entry_points={'trac.plugins': [
+        'tracgooglesitemap.plugin = tracgooglesitemap.plugin',
+        'tracgooglesitemap.notify = tracgooglesitemap.notify'
+    ]}
 )
