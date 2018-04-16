d5c828cb94b5ce31cd0a4a4434622d0a15ccd87d
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2018-04-16 18:26:09 +0000

    TracExtLinksNewWindow 1.1: Conform to PEP8
    git-svn-id: https://trac-hacks.org/svn@17130 7322e99d-02ea-0310-aa39-e9a107903beb

--- externallinksnewwindowplugin/0.11/setup.py
+++ externallinksnewwindowplugin/0.11/setup.py
@@ -3,26 +3,22 @@
 
 from setuptools import setup, find_packages
 
-__url__      = ur"$URL$"[6:-2]
-__author__   = ur"$Author$"[9:-2]
-__revision__ = int("0" + ur"$Rev$"[6:-2])
-__date__     = ur"$Date$"[7:-2]
-
 setup(
-    name         = 'TracExtLinksNewWindow',
-    version      = '1.0',
-    description  = 'Trac Load External Links in New Window',
-    license      = 'GPLv3',
-    zip_safe     = False,
-    url          = 'http://trac-hacks.org/wiki/ExternalLinksNewWindow',
-    download_url = 'http://trac-hacks.org/svn/externallinksnewwindowplugin/releases',
-    author       = 'Martin Scharrer',
-    author_email = 'martin@scharrer-online.de',
-    packages     = ['tracextlinksnewwindow'],
-    package_data = {
-        'tracextlinksnewwindow' : [ 'htdocs/*.js' ],
+    name='TracExtLinksNewWindow',
+    version='1.1',
+    description='Trac plugin to open external links in a new window.',
+    license='GPLv3',
+    zip_safe=False,
+    url='https://trac-hacks.org/wiki/ExternalLinksNewWindow',
+    download_url='https://pypi.python.org/pypi/ExternalLinksNewWindowPlugin',
+    author='Martin Scharrer',
+    author_email='martin@scharrer-online.de',
+    packages=['tracextlinksnewwindow'],
+    package_data={
+        'tracextlinksnewwindow': ['htdocs/*.js'],
     },
-    entry_points = {
+    install_requires=['Trac'],
+    entry_points={
         'trac.plugins': [
             'tracextlinksnewwindow.plugin = tracextlinksnewwindow.plugin',
         ],
--- externallinksnewwindowplugin/0.11/tracextlinksnewwindow/plugin.py
+++ externallinksnewwindowplugin/0.11/tracextlinksnewwindow/plugin.py
@@ -6,38 +6,30 @@
     This is Free Software under the GPL v3!
 """
 
-__url__      = ur"$URL$"[6:-2]
-__author__   = ur"$Author$"[9:-2]
-__revision__ = int("0" + ur"$Rev$"[6:-2])
-__date__     = ur"$Date$"[7:-2]
+from trac.core import Component, implements
+from trac.web.api import IRequestFilter
+from trac.web.chrome import ITemplateProvider, add_script
 
-from  trac.core        import  Component, implements
-from  trac.web.api     import  IRequestFilter
-from  trac.web.chrome  import  ITemplateProvider, add_stylesheet, add_script
 
 class ExtLinksNewWindowPlugin(Component):
     """Opens external links in new window
-
-       `$Id$`
     """
-    implements ( IRequestFilter, ITemplateProvider )
+    implements(IRequestFilter, ITemplateProvider)
 
-    # ITemplateProvider#get_htdocs_dirs
+    # ITemplateProvider methods
+
     def get_htdocs_dirs(self):
         from pkg_resources import resource_filename
         return [('extlinksnewwindow', resource_filename(__name__, 'htdocs'))]
 
-    # ITemplateProvider#get_templates_dirs
     def get_templates_dirs(self):
         return []
 
+    # IRequestFilter methods
 
-    # IRequestFilter#pre_process_request
     def pre_process_request(self, req, handler):
         return handler
 
-    # IRequestFilter#post_process_request
     def post_process_request(self, req, template, data, content_type):
         add_script(req, 'extlinksnewwindow/extlinks.js')
-        return (template, data, content_type)
-
+        return template, data, content_type
