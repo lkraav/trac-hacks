8573f8357af524eda6b6026155d5e5e831e1501c
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2018-04-16 08:41:36 +0000

    tracsuperuser 0.4: Conform to PEP8
    git-svn-id: https://trac-hacks.org/svn@17128 7322e99d-02ea-0310-aa39-e9a107903beb

--- superuserplugin/setup.py
+++ superuserplugin/setup.py
@@ -4,15 +4,13 @@ PACKAGE = 'tracsuperuser'
 VERSION = '0.4'
 
 setup(name=PACKAGE,
-    version=VERSION,
-    packages=['tracsuperuser'],
-    author='Alvaro J. Iradier',
-    author_email = "alvaro.iradier@polartech.es",
-    description = "Create a super user that always has TRAC_ADMIN permissions",
-    long_description = "",
-    license = "GPL",
-    keywords = "trac plugin auth superuser admin",
-    
-    url="http://www.trac-hacks.org/wiki/SuperUserPlugin",
-    
-    entry_points = {'trac.plugins': ['tracsuperuser = tracsuperuser']})
+      version=VERSION,
+      packages=['tracsuperuser'],
+      author='Alvaro J. Iradier',
+      author_email="alvaro.iradier@polartech.es",
+      description="Create a super user that always has TRAC_ADMIN permissions",
+      long_description="",
+      license="GPL",
+      keywords="trac plugin auth superuser admin",
+      url="http://www.trac-hacks.org/wiki/SuperUserPlugin",
+      entry_points={'trac.plugins': ['tracsuperuser = tracsuperuser']})
--- superuserplugin/tracsuperuser/superuser.py
+++ superuserplugin/tracsuperuser/superuser.py
@@ -1,10 +1,10 @@
 ## AMB SuperUser - Give TRAC_ADMIN permissions to a configurable user
 #
 # Automatically gives TRAC_ADMIN permission to a defined user
-# 
+#
 # [trac]
 # permission_store = SuperUserPlugin
-# 
+#
 # [superuser]
 # superuser = admin
 # wrapped_permission_store = DefaultPermissionStore
@@ -13,19 +13,16 @@ from trac.core import *
 from trac.perm import IPermissionStore
 from trac.config import ExtensionOption, ListOption
 
-import logging
-
 
 class SuperUserPlugin(Component):
     """ Adds a superuser with TRAC_ADMIN permissions """
     implements(IPermissionStore)
-    
+
     superusers = ListOption('superuser', 'superuser', 'admin', doc='Superuser username(s) (comma separated)')
-    
+
     store = ExtensionOption('superuser', 'wrapped_permission_store', IPermissionStore,
         'DefaultPermissionStore', """Name of the component implementing `IPermissionStore`, which is used
         for managing user and group permissions.""")
-    
 
     def get_user_permissions(self, username):
         if username in self.superusers:
@@ -35,15 +32,15 @@ class SuperUserPlugin(Component):
 
     def get_users_with_permissions(self, permissions):
         return list(set(self.superusers) | set(self.store.get_users_with_permissions(permissions)))
-            
+
     def get_all_permissions(self):
         return self.store.get_all_permissions() + [(user, 'TRAC_ADMIN') for user in self.superusers]
-        
+
     def grant_permission(self, username, action):
         if username in self.superusers:
             assert False, "Superuser %s can't be given any permissions" % username
         return self.store.grant_permission(username, action)
-        
+
     def revoke_permission(self, username, action):
         if username in self.superusers:
             assert False, "Superuser %s can't be revoked any permissions" % username
