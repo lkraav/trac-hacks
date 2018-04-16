585c95fb99bcab7cfbb0fa52bc30739e7eb3a3b3
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2018-04-16 19:54:35 +0000

    TracAddHeadersPlugin 0.4: Conform to PEP8
    git-svn-id: https://trac-hacks.org/svn@17136 7322e99d-02ea-0310-aa39-e9a107903beb

--- addheadersplugin/0.12/setup.py
+++ addheadersplugin/0.12/setup.py
@@ -3,23 +3,19 @@
 
 from setuptools import setup
 
-__url__      = ur"$URL$"[6:-2]
-__author__   = ur"$Author$"[9:-2]
-__revision__ = int("0" + ur"$Rev$"[6:-2])
-__date__     = ur"$Date$"[7:-2]
-
-
 setup(
-    name = 'TracAddHeadersPlugin',
-    version = '0.3',
-    packages = ['tracaddheaders'],
-    author = 'Martin Scharrer',
-    author_email = 'martin@scharrer-online.de',
-    description = 'AddHeaders Trac Plugin.',
-    url = 'http://www.trac-hacks.org/wiki/AddHeadersPlugin',
-    license = 'GPLv3',
-    zip_safe     = False,
-    keywords = 'trac plugin addheaders',
-    classifiers = ['Framework :: Trac'],
-    entry_points = {'trac.plugins': ['tracaddheaders.plugin = tracaddheaders.plugin']}
+    name='TracAddHeadersPlugin',
+    version='0.4',
+    packages=['tracaddheaders'],
+    author='Martin Scharrer',
+    author_email='martin@scharrer-online.de',
+    description='AddHeaders Trac Plugin.',
+    url='https://www.trac-hacks.org/wiki/AddHeadersPlugin',
+    license='GPLv3',
+    zip_safe=False,
+    keywords='trac plugin addheaders',
+    install_requires=['Trac'],
+    classifiers=['Framework :: Trac'],
+    entry_points={'trac.plugins': [
+        'tracaddheaders.plugin = tracaddheaders.plugin']}
 )
--- addheadersplugin/0.12/tracaddheaders/plugin.py
+++ addheadersplugin/0.12/tracaddheaders/plugin.py
@@ -4,22 +4,16 @@
     $HeadURL$
 
     This is Free Software under the GPL v3!
-""" 
+"""
 
-__url__      = ur"$URL$"[6:-2]
-__author__   = ur"$Author$"[9:-2]
-__revision__ = int("0" + r"$Rev$"[6:-2])
-__date__     = r"$Date$"[7:-2]
-
-from  trac.core        import  *
-from  trac.web.api     import  IRequestFilter
-from  trac.web.chrome  import  add_link, add_stylesheet, add_script
+from trac.config import Option, ListOption
+from trac.core import Component, implements
+from trac.web.api import IRequestFilter
+from trac.web.chrome import add_link, add_stylesheet, add_script
 try:
     from trac.web.chrome import add_meta
-except:
-    pass
-
-from  trac.config      import  Option, ListOption
+except ImportError:
+    add_meta = None
 
 
 class AddHeadersPlugin(Component):
@@ -33,15 +27,21 @@ class AddHeadersPlugin(Component):
 
     section = 'addheaders'
 
-    default_base        = Option(section, 'default_base', 'site/')
-    default_script_base = Option(section, 'default_script_base', default_base)
-    default_style_base  = Option(section, 'default_style_base',  default_base)
-    default_meta_lang   = Option(section, 'default_meta_lang',  None)
+    default_base = Option(section, 'default_base', 'site/')
 
-    links         = ListOption(section, 'add_links')
-    stylesheets   = ListOption(section, 'add_styles')
-    scripts       = ListOption(section, 'add_scripts')
-    metas         = ListOption(section, 'add_metas')
+    default_script_base = Option(section, 'default_script_base', default_base)
+
+    default_style_base = Option(section, 'default_style_base', default_base)
+
+    default_meta_lang = Option(section, 'default_meta_lang', None)
+
+    links = ListOption(section, 'add_links')
+
+    stylesheets = ListOption(section, 'add_styles')
+
+    scripts = ListOption(section, 'add_scripts')
+
+    metas = ListOption(section, 'add_metas')
 
     def pre_process_request(self, req, handler):
         return handler
@@ -50,43 +50,42 @@ class AddHeadersPlugin(Component):
         get = self.env.config.get
 
         for link in self.links:
-            rel       = get(self.section, link + '.rel'   )
-            href      = get(self.section, link + '.href'  )
-            title     = get(self.section, link + '.title' )
-            mimetype  = get(self.section, link + '.type'  )
-            classname = get(self.section, link + '.class' )
+            rel = get(self.section, link + '.rel')
+            href = get(self.section, link + '.href')
+            title = get(self.section, link + '.title')
+            mimetype = get(self.section, link + '.type')
+            classname = get(self.section, link + '.class')
 
             if rel and href:
-                add_link(req, rel, href, title or None, mimetype or None, classname or None)
-
+                add_link(req, rel, href, title or None,
+                         mimetype or None, classname or None)
 
         for stylesheet in self.stylesheets:
-            filename  = get(self.section, stylesheet + '.filename', unicode(self.default_style_base) + stylesheet + '.css' )
-            mimetype  = get(self.section, stylesheet + '.mimetype', 'text/css')
+            filename = get(self.section, stylesheet + '.filename',
+                           unicode(self.default_style_base) + stylesheet +
+                           '.css')
+            mimetype = get(self.section, stylesheet + '.mimetype', 'text/css')
 
             if filename:
                 add_stylesheet(req, filename, mimetype)
 
-
         for script in self.scripts:
-            filename  = get(self.section, script     + '.filename', unicode(self.default_script_base) + script + '.js' )
-            mimetype  = get(self.section, script     + '.mimetype', 'text/javascript')
+            filename = get(self.section, script + '.filename',
+                           unicode(self.default_script_base) + script + '.js')
+            mimetype = get(self.section, script +
+                           '.mimetype', 'text/javascript')
 
             if filename:
                 add_script(req, filename, mimetype)
 
         for meta in self.metas:
-            http_equiv = get(self.section, meta      + '.http_equiv', None )
-            name       = get(self.section, meta      + '.name', meta )
-            scheme     = get(self.section, meta      + '.scheme', None )
-            lang       = get(self.section, meta      + '.lang', self.default_meta_lang )
-            content    = get(self.section, meta      + '.content', u'' )
+            http_equiv = get(self.section, meta + '.http_equiv', None)
+            name = get(self.section, meta + '.name', meta)
+            scheme = get(self.section, meta + '.scheme', None)
+            lang = get(self.section, meta + '.lang', self.default_meta_lang)
+            content = get(self.section, meta + '.content', u'')
 
-            if http_equiv or name or scheme:
-                try:
-                    add_meta(req, content, http_equiv, name, scheme, lang)
-                except:
-                    pass
+            if http_equiv or name or scheme and add_meta:
+                add_meta(req, content, http_equiv, name, scheme, lang)
 
-        return (template, data, content_type)
-
+        return template, data, content_type
