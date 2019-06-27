7ffa4769a0944e71bea1788fa983bebb74318c2b
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2019-06-27 20:54:19 +0000

    TracKeywordsPlugin 1.0dev: Improve separator handling
    Patch by clemens.
    
    Fixes #13534.
    
    
    git-svn-id: https://trac-hacks.org/svn@17396 7322e99d-02ea-0310-aa39-e9a107903beb

--- trackeywordsplugin/trunk/trackeywords/htdocs/trac_keywords.js
+++ trackeywordsplugin/trunk/trackeywords/htdocs/trac_keywords.js
@@ -1,34 +1,52 @@
 jQuery(function($) {
 
   field_id = document.getElementById('tags') ? "tags" : "field-keywords";
-  function addRemoveTag(w) {
+
+  // Remove or add a keyword tag to the field.
+  function addRemoveKeyword(w) {
     var el = document.getElementById(field_id);
     var orig = el.value;
-    var newval = orig.replace(new RegExp('\\b' + w + '\\b'), '');
+    // Remove keyword including white spaces and separator, match case-insensitive
+    var filter = function(v) {
+      return v.length !== w.length || v.toLowerCase() !== w.toLowerCase();
+    };
+    var orig_split = orig.split(/[;,\s]+/).filter(function(value, index, arr) {
+      return value != '';
+    });
+    var newval;
     var link = document.getElementById('trac-keyword-' + w);
-    if (orig != newval) { // remove tag.
+    if (orig_split.includes(w)) { // remove keyword
+        newval = $.grep(orig_split, filter);
         if(link) link.className = '';
-    } else {
-        newval = orig + (orig ? ' ' : '') + w;
+    } else { // add keyword
+        newval = orig_split;
+        newval.push(w);
         if(link) link.className = 'trac-keyword';
     }
-    el.value = newval.replace(/^\s+|\s+$/, '');
+    // Strip leading and trailing white spaces and separator.
+    el.value = newval.join(' ').replace(/^[;,\s]+|[;,\s]+$/, '');
   }
 
+  // Avoid duplicate insertion of keyword section.
+  if ($('#trac-keywords').length > 0) return;
   var $fieldset = $('<fieldset id="trac-keywords" />');
   $fieldset.append('<legend>Add Keywords</legend>')
   $ul = $('<ul></ul>');
   $fieldset.append($ul)
-  var resource_keywords = document.getElementById(field_id).value.split(' ');
+  // Case-insensitive: compare to lower case.
+  var field = document.getElementById(field_id).value.toLowerCase();
+  // Regex splits the content of the field at separator characters.
+  var resource_keywords = field.split(/[;,\s]+/);
+  // Iterate over each keyword defined in the Trac ini-file.
   $.each(trac_keywords, function(i, item) {
     $ul.append($('<li><a href="#">' + item[0] + '</a></li> ')
       .attr({
         'title': item[1],
         'id': 'trac-keyword-' + item[0],
-        'class': $.inArray(item[0], resource_keywords) != -1 ? 'trac-keyword': '',
+        'class': $.inArray(item[0].toLowerCase(), resource_keywords) != -1 ? 'trac-keyword' : '',
       })
       .on('click', function() {
-        addRemoveTag($(this).text());
+        addRemoveKeyword($(this).text());
         return false;
       }));
   })
