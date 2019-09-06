597307236f72a4cec98dfd59bd8b939cbd1acf05
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2019-09-06 01:46:10 +0000

    Limit cloud to min_count and/or max_keywords
    Refs #13605.
    
    
    git-svn-id: https://trac-hacks.org/svn@17490 7322e99d-02ea-0310-aa39-e9a107903beb

--- loomingcloudsplugin/trunk/loomingclouds/loomingclouds.py
+++ loomingcloudsplugin/trunk/loomingclouds/loomingclouds.py
@@ -8,31 +8,45 @@ from genshi.filters.transform import Transformer
 
 from pkg_resources import resource_filename
 
+from trac.config import IntOption
 from trac.core import *
 from trac.util.html import html as tag
 from trac.web.api import ITemplateStreamFilter
 from trac.web.chrome import (
     ITemplateProvider, add_script, add_stylesheet, web_context)
-from trac.wiki.formatter import Formatter
 
-from tractags.macros import TagWikiMacros
+from tractags.api import Counter
+from tractags.macros import TagSystem, TagWikiMacros
 
 
 class LoomingClouds(Component):
 
     implements(ITemplateProvider, ITemplateStreamFilter)
 
+    max_keywords = IntOption('loomingclouds', 'max_keywords', doc="""
+        Maximum number of keywords to display in the cloud.""")
+
+    min_count = IntOption('loomingclouds', 'min_count', default=1, doc="""
+        Hide tags with count less than this value.""")
+
     # ITemplateStreamFilter methods
 
     def filter_stream(self, req, method, filename, stream, data):
         if filename in ('ticket.html', 'agilo_ticket_new.html',):
 
+            tag_system = TagSystem(self.env)
+            all_realms = tag_system.get_taggable_realms()
+            all_tags = tag_system.get_all_tags(req, realms=all_realms)
+            tags = Counter(dict(all_tags.most_common(self.max_keywords))) \
+                   if self.max_keywords else all_tags
+
+            cloud = TagWikiMacros(self.env).render_cloud(
+                    req, tags, caseless_sort=True, mincount=self.min_count,
+                    realms=all_realms)
+
             add_stylesheet(req, 'tags/css/tractags.css')
             add_stylesheet(req, 'loomingclouds/css/tagcloud.css')
             add_script(req, 'loomingclouds/js/tag_filler.js')
-            formatter = Formatter(self.env, web_context(req))
-            macro = TagWikiMacros(self.env)
-            cloud = macro.expand_macro(formatter, 'TagCloud', '')
 
             stream |= Transformer("//input[@id='field-keywords']").after(cloud).after(tag.a('More...',href='#',class_='tag-cloud-filler'))
 
