1d1597a411afa949d95b704e323b55b16e545f60
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2019-09-06 01:02:57 +0000

    Click on tag in keywords field removes the tag
    Refs #13605.
    
    
    git-svn-id: https://trac-hacks.org/svn@17488 7322e99d-02ea-0310-aa39-e9a107903beb

--- loomingcloudsplugin/trunk/loomingclouds/htdocs/js/tag_filler.js
+++ loomingcloudsplugin/trunk/loomingclouds/htdocs/js/tag_filler.js
@@ -2,7 +2,15 @@ jQuery(function($) {
   var tagnode = $("#field-keywords");
   $("ul.tagcloud a").click(function() {
     var value = tagnode.val();
-    value += ' ' + $(this).text();
+    var newval = $(this).text();
+    var value_as_array = value.trim().split(/[;,\s]+/);
+    var index = value_as_array.indexOf(newval);
+    if (index > -1) {
+      value_as_array.splice(index, 1);
+    } else {
+      value_as_array.push(newval);
+    }
+    value = value_as_array.join(' ');
     tagnode.val(value);
     return false;
   });
