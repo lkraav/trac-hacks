1c47d7a0f5bad2531aa63654388bba4a07d97253
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2019-09-18 17:00:20 +0000

    TracSumFields 2.0.0dev: Refactor jQuery
    * Use recommended document ready handler syntax
    * Fix indentation
    
    
    git-svn-id: https://trac-hacks.org/svn@17504 7322e99d-02ea-0310-aa39-e9a107903beb

--- sumfieldsplugin/1.0/sumfields/htdocs/sumfields.js
+++ sumfieldsplugin/1.0/sumfields/htdocs/sumfields.js
@@ -4,48 +4,48 @@
 
   This software is licensed as described in the file COPYING, which
   you should have received as part of this distribution.
-*/
-jQuery(document).ready(function($){
-    // loop through each "listing tickets" table
-    $('.listing.tickets tbody').has("tr:not(.trac-group,.trac-columns)").each(function() {
-        var $tbody = $(this)
-        var sums = {};
-        // sum each provided field's column
-        for (var i=0; i < fields.length; i++) {
-            var field = fields[i];
-            sums[field] = 0;
-            // sum up each row
-            $('.' + field, this).each(function () {
-                var num = parseFloat($(this).text().replace(',', '.'));
-                if (!isNaN(num)) {
-                    sums[field] += num;
-                }
-            });
+  */
+jQuery(function($) {
+  // loop through each "listing tickets" table
+  $('.listing.tickets tbody').has("tr:not(.trac-group,.trac-columns)").each(function() {
+    var $tbody = $(this)
+    var sums = {};
+    // sum each provided field's column
+    for (var i=0; i < fields.length; i++) {
+      var field = fields[i];
+      sums[field] = 0;
+      // sum up each row
+      $('.' + field, this).each(function () {
+        var num = parseFloat($(this).text().replace(',', '.'));
+        if (!isNaN(num)) {
+          sums[field] += num;
         }
+      });
+    }
 
-        // clone the last row in table and replace with the sum(s)
-        var lastrow = $(this).find('tr:last');
-        var sumrow = lastrow.clone().removeAttr('class');
-        var found = false;
-        sumrow.find('td').each(function(){
-            var field = $(this).attr('class');
-            if (field != 'id'){
-                // HACK: remove all but the id class to resolve plugin conflicts:
-                //  1. have BatchModify plugin find the row to add checkbox (to ensure column alignment)
-                //  2. have GridModify not find the rest of the fields
-                $(this).removeAttr('class');
-            }
-            if ($.inArray(field, fields) == -1){
-                $(this).text('');
-                $(this).children().remove();
-            } else {
-                $(this).text(sums[field]);
-                found = true;
-            }
-        });
-        if (found){
-            sumrow.find('td:first').text('sum:');
-            $tbody.after($('<tbody class="trac-query-summary">').append(sumrow));
-        }
+    // clone the last row in table and replace with the sum(s)
+    var lastrow = $(this).find('tr:last');
+    var sumrow = lastrow.clone().removeAttr('class');
+    var found = false;
+    sumrow.find('td').each(function(){
+      var field = $(this).attr('class');
+      if (field != 'id'){
+        // HACK: remove all but the id class to resolve plugin conflicts:
+        //  1. have BatchModify plugin find the row to add checkbox (to ensure column alignment)
+        //  2. have GridModify not find the rest of the fields
+        $(this).removeAttr('class');
+      }
+      if ($.inArray(field, fields) == -1){
+        $(this).text('');
+        $(this).children().remove();
+      } else {
+        $(this).text(sums[field]);
+        found = true;
+      }
     });
+    if (found){
+      sumrow.find('td:first').text('sum:');
+      $tbody.after($('<tbody class="trac-query-summary">').append(sumrow));
+    }
+  });
 });
