3a5fdd2926e3d7e4b8897a3dc952e065f91cd1a8
psuter <psuter@7322e99d-02ea-0310-aa39-e9a107903beb>
2019-11-22 20:27:52 +0000

    WikiAutoCompletePlugin: Publish to PyPI. Add classifier etc. (see #13502)
    git-svn-id: https://trac-hacks.org/svn@17586 7322e99d-02ea-0310-aa39-e9a107903beb

--- wikiautocompleteplugin/trunk/setup.py
+++ wikiautocompleteplugin/trunk/setup.py
@@ -4,11 +4,14 @@
 from setuptools import setup
 
 setup(
-    name = 'WikiAutoComplete',
+    name = 'TracWikiAutoComplete',
     version = '1.4',
     author = 'Peter Suter',
     author_email = 'peter@lucid.ch',
     description = 'Auto-completes wiki formatting',
+    classifiers=['Framework :: Trac'],
+    license='BSD',
+    url='https://trac-hacks.org/wiki/WikiAutoCompletePlugin',
     packages = ['wikiautocomplete'],
     package_data = {'wikiautocomplete': [
             'htdocs/js/*.js',
