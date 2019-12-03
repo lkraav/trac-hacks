6a61061008cdbbcf847fe6a172a1fc2822ebfa10
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2019-12-03 12:22:15 +0000

    Convert tabs to spaces
    * Add `classifiers` metadata
    * Version 0.2 has been published to pypi:TracUnixGroups
    
    Fixes #13689.
    
    
    git-svn-id: https://trac-hacks.org/svn@17607 7322e99d-02ea-0310-aa39-e9a107903beb

--- unixgroupsplugin/1.0/unixgroups/unixgroups.py
+++ unixgroupsplugin/1.0/unixgroups/unixgroups.py
@@ -1,5 +1,4 @@
-# UnixGroups plugin v1.0
-
+# -*- coding: utf-8 -*-
 
 from pwd import *
 from grp import *
@@ -7,52 +6,53 @@ from trac.core import *
 from trac.perm import IPermissionGroupProvider
 from trac.perm import PermissionSystem
 
+
 class UnixGroups(Component):
     implements(IPermissionGroupProvider)
 
     def __init__(self):
-	self.ENV = self.env
-	self.DEBUG = 0
+        self.ENV = self.env
+        self.DEBUG = 0
 
     # IPermissionGroupProvider methods
+
     def get_permission_groups(self, username):
         try:
             maingroup = getgrgid(getpwnam(username).pw_gid).gr_name
         except KeyError:
             return []
 
-	permsys = PermissionSystem(self.ENV)
-	perm_list = permsys.get_all_permissions()
+        permsys = PermissionSystem(self.ENV)
+        perm_list = permsys.get_all_permissions()
 
-	if self.DEBUG:
-		fp = open('/var/tmp/unixgroups', 'a+')
+        if self.DEBUG:
+            fp = open('/var/tmp/unixgroups', 'a+')
 
-	groups = list()
-	if not perm_list:
-		return [maingroup]
-	else:
-		for perm in perm_list:
-			if not perm[0] in groups:
-				groups.append(perm[0])
-				if self.DEBUG:
-					fp.write('appending group: %s\n' %(perm[0]))
+        groups = list()
+        if not perm_list:
+            return [maingroup]
+        else:
+            for perm in perm_list:
+                if not perm[0] in groups:
+                    groups.append(perm[0])
+                    if self.DEBUG:
+                        fp.write('appending group: %s\n' % (perm[0]))
 
+        secgroups = list()
+        for grp in groups:
+            if self.DEBUG:
+                fp.write('check group: %s\n' % (grp))
+            try:
+                members = getgrnam(grp)[3]
+            except KeyError, detail:
+                continue
 
-	secgroups = list()
-	for grp in groups:
-		if self.DEBUG:
-			fp.write('check group: %s\n' %(grp))
-		try:
-			members =  getgrnam(grp)[3]
-		except KeyError, detail:
-			continue
+            if username in members:
+                secgroups.append(grp)
+                if self.DEBUG:
+                    fp.write('%s in group: %s\n' % (username, grp))
 
-		if username in members:
-			secgroups.append(grp)
-			if self.DEBUG:
-				fp.write('%s in group: %s\n' %(username,grp))
-	
-	if self.DEBUG:		
-		fp.close()
+        if self.DEBUG:
+            fp.close()
 
         return [maingroup] + secgroups
