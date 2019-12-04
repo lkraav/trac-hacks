536f0cea317432f52500f57013b67ea59a261615
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2019-12-04 13:46:35 +0000

    TracVisualization 0.2: Publish to PyPI
    Fixes #13684.
    
    
    git-svn-id: https://trac-hacks.org/svn@17613 7322e99d-02ea-0310-aa39-e9a107903beb

--- visualizationplugin/0.12/setup.py
+++ visualizationplugin/0.12/setup.py
@@ -11,15 +11,17 @@
 from setuptools import setup, find_packages
 
 PACKAGE = 'TracVisualization'
-VERSION = '0.0.1'
+VERSION = '0.2'
 
 setup(
     name=PACKAGE, version=VERSION,
     description='Graphs tables and other data using Google Visualization API',
-    author="Rob Guttman", author_email="guttman@alum.mit.edu",
+    author="Rob Guttman",
+    author_email="guttman@alum.mit.edu",
     license='3-Clause BSD',
-    url='http://trac-hacks.org/wiki/VisualizationPlugin',
-    packages = ['viz'],
-    package_data = {'viz':['templates/*.html','htdocs/*.css','htdocs/*.js']},
-    entry_points = {'trac.plugins':['viz.web_ui = viz.web_ui']}
+    url='https://trac-hacks.org/wiki/VisualizationPlugin',
+    classifiers = ['Framework :: Trac'],
+    packages=['viz'],
+    package_data={'viz':['templates/*.html','htdocs/*.css','htdocs/*.js']},
+    entry_points={'trac.plugins':['viz.web_ui = viz.web_ui']}
 )
