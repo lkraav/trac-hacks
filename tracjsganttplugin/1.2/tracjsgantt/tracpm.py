3304fdf573e5f4504de4f49bc3b99614132feb64
jun66j5 <jun66j5@7322e99d-02ea-0310-aa39-e9a107903beb>
2019-12-19 18:34:55 +0000

    Trac-jsGantt 1.2.0.0dev: make compatible with Trac 1.4 (closes #13713)
    git-svn-id: https://trac-hacks.org/svn@17638 7322e99d-02ea-0310-aa39-e9a107903beb

--- tracjsganttplugin/1.2/tracjsgantt/tracpm.py
+++ tracjsganttplugin/1.2/tracjsgantt/tracpm.py
@@ -83,15 +83,17 @@ class TracPM(Component):
     def environment_created(self):
         self.log.info('Creating environment for TracPM.')
         self.found_db_version = 0
-        self.upgrade_environment(self.env.get_db_cnx())
+        self.upgrade_environment()
 
-    def environment_needs_upgrade(self, db):
-        cursor = db.cursor()
-        cursor.execute('SELECT value FROM system WHERE name=%s',
-                       (db_default.name, ))
-        value = cursor.fetchone()
+    def environment_needs_upgrade(self, db=None):
+        for row in self.env.db_query('SELECT value FROM system WHERE name=%s',
+                                     (db_default.name, )):
+            version = row[0]
+            break
+        else:
+            version = 0
         try:
-            self.found_db_version = int(value[0])
+            self.found_db_version = int(version)
             if self.found_db_version < db_default.version:
                 self.log.info('TracPM environment out of date.')
                 return True
@@ -103,23 +105,22 @@ class TracPM(Component):
         # Environment version is current
         return False
 
-    def upgrade_environment(self, db):
+    def upgrade_environment(self, db=None):
         self.log.info('Upgrading environment for TracPM.')
-        db_manager, _ = DatabaseManager(self.env)._get_connector()
-        cursor = db.cursor()
+        db_manager, _ = DatabaseManager(self.env).get_connector()
+        with self.env.db_transaction as db:
+            # Add or update TracPM version in system table
+            if not self.found_db_version:
+                db("INSERT INTO system (name, value) VALUES (%s, %s)",
+                   (db_default.name, db_default.version))
+            else:
+                db("UPDATE system SET value=%s WHERE name=%s",
+                   (db_default.version, db_default.name))
 
-        # Add or update TracPM version in system table
-        if not self.found_db_version:
-            cursor.execute("INSERT INTO system (name, value) VALUES (%s, %s)",
-                           (db_default.name, db_default.version))
-        else:
-            cursor.execute("UPDATE system SET value=%s WHERE name=%s",
-                           (db_default.version, db_default.name))
-
-        # Create tables
-        for table in db_default.tables:
-            for sql in db_manager.to_sql(table):
-                cursor.execute(sql)
+            # Create tables
+            for table in db_default.tables:
+                for sql in db_manager.to_sql(table):
+                    db(sql)
 
     # Configurable data sources
     fields = None
