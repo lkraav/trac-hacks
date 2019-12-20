ccea886da83daba9f5ccba7ebdf3e930e51342e0
stoecker <stoecker@7322e99d-02ea-0310-aa39-e9a107903beb>
2019-12-20 20:45:54 +0000

    branch current trunk to 1.3
    git-svn-id: https://trac-hacks.org/svn@17640 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ traciniadminpanelplugin/1.3/inieditorpanel/texts/othertexts.py
@@ -0,0 +1,15 @@
+# store auto-generated texts here, which aren't translatable otherwise
+
+# data field types
+N_("bool")
+N_("int")
+N_("choice")
+
+# field access types
+N_('readonly')
+N_('hidden')
+N_('modifiable')
+
+# display field value when default
+N_('<default value>')
+N_('<none>')
---
+++ traciniadminpanelplugin/1.3/setup.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2006 Edgewall Software
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution. The terms
+# are also available at http://trac.edgewall.com/license.html.
+#
+# This software consists of voluntary contributions made by many
+# individuals. For the exact contribution history, see the revision
+# history and logs, available at http://projects.edgewall.com/trac/.
+
+from setuptools import setup, find_packages
+
+NAME = 'TracIniAdminPanel'
+PACKAGE = 'inieditorpanel'
+VERSION = '1.0.2'
+
+extra = {}
+try:
+    from trac.util.dist import get_l10n_cmdclass
+    cmdclass = get_l10n_cmdclass()
+    if cmdclass:
+        extra['cmdclass'] = cmdclass
+        extractors = [
+            ('**.py',                'trac.dist:extract_python', None),
+            ('**/templates/**.html', 'genshi', None)
+        ]
+        extra['message_extractors'] = {
+            PACKAGE: extractors,
+        }
+except ImportError:
+    pass
+
+setup(
+    name = NAME,
+    version = VERSION,
+
+    author = "Sebastian Krysmanski, Dirk Stöcker",
+    author_email = 'trac@dstoecker.de',
+    url = "https://trac-hacks.org/wiki/TracIniAdminPanelPlugin",
+    description = "An admin panel for editing trac.ini",
+    keywords = "trac plugin",
+    license = "BSD",
+    classifiers=[
+        'Framework :: Trac',
+        'License :: OSI Approved :: BSD License',
+    ],
+    install_requires = ['Trac'],
+    packages = find_packages(exclude=['*.texts*']),
+    package_data = { PACKAGE: [ 'templates/*', 'htdocs/*', 'locale/*/LC_MESSAGES/*.mo' ] },
+
+    entry_points = { 'trac.plugins': [
+        '%s.web_ui = %s.web_ui' % (PACKAGE, PACKAGE),
+        '%s.default_manager = %s.default_manager' % (PACKAGE, PACKAGE),
+        # Add additional components here
+      ] },
+    **extra
+)
