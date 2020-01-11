cc412603985ea25ee4c9d4a7d7290e9354b60315
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2020-01-11 07:24:28 +0000

    TracKeywordReplace 1.0dev: Branch for Trac 1.0
    Refs #13726.
    
    
    git-svn-id: https://trac-hacks.org/svn@17653 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ keywordreplaceplugin/1.0/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='TracKeywordReplace',
+      version='1.0',
+      packages=['keywordreplace'],
+      author='Bangyou Zheng',
+      description='Replace keywords with wiki format from a table in a Wiki page.',
+      url='http://trac-hacks.org/wiki/KeywordReplacePlugin',
+      license='BSD',
+      entry_points = {'trac.plugins': ['keywordreplace = keywordreplace']})
