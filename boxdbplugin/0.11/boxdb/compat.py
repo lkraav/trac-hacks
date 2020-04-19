8d80e4ada9be6b06368fa74c255dc92798457ba2
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2020-04-19 20:09:31 +0000

    Remove plugin code that has no project page
    git-svn-id: https://trac-hacks.org/svn@17696 7322e99d-02ea-0310-aa39-e9a107903beb

--- boxdbplugin/0.11/boxdb/compat.py
+++
@@ -1,9 +0,0 @@
-# Created by  on 2008-01-10.
-# Copyright (c) 2008 Noah Kantrowitz. All rights reserved.
-
-try:
-    import simplejson
-except ImportError:
-    import _simplejson as simplejson
-
-
