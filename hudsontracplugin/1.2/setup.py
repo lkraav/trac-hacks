882870412d5d8a2c6105ea16ffaf4fd9d1781fdd
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2020-04-29 20:44:36 +0000

    0.6dev: Require Trac >= 1.2
    Refs #13753.
    
    
    git-svn-id: https://trac-hacks.org/svn@17711 7322e99d-02ea-0310-aa39-e9a107903beb

--- hudsontracplugin/1.2/setup.py
+++ hudsontracplugin/1.2/setup.py
@@ -10,7 +10,7 @@ setup(
     description = "A trac plugin to add hudson build info to the trac timeline",
     license = "BSD",
     keywords = "trac builds hudson",
-    url = "http://trac-hacks.org/wiki/HudsonTracPlugin",
+    url = "https://trac-hacks.org/wiki/HudsonTracPlugin",
 
     packages = ['HudsonTrac'],
     package_data = {
