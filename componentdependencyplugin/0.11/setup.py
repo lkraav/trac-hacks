d928e87b1019456fe12571fcfc82e2c1159614bb
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2020-05-06 00:24:56 +0000

    TracComponentDependency 0.3: Add classifiers
    Version 0.3 has been released to PyPI.
    
    Fixes #13691.
    
    
    git-svn-id: https://trac-hacks.org/svn@17722 7322e99d-02ea-0310-aa39-e9a107903beb

--- componentdependencyplugin/0.11/setup.py
+++ componentdependencyplugin/0.11/setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 
 
 setup(name='TracComponentDependency',
-      version='0.2',
+      version='0.3',
       description="allows plugins to depend on other components being enabled",
       author='Jeff Hammel',
       author_email='jhammel@openplans.org',
@@ -14,6 +14,7 @@ setup(name='TracComponentDependency',
       license="BSD 3-Clause",
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests*']),
       include_package_data=True,
+      classifiers=['Framework :: Trac'],
       package_data={ 'componentdependencyplugin': ['templates/*', 'htdocs/*'] },
       zip_safe=False,
       entry_points = """
