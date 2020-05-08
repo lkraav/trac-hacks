d51acc00787b78de0e24d9512b5789817d0153f4
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2020-05-08 22:31:11 +0000

    TracFullBlog 0.2dev: Convert admin template to Jinja2
    * This eliminates the problem described in trac:#13281
    * Re-styled template for consistency with Trac admin panels
    * Added auto-preview of text area
    
    Refs #12601.
    
    
    git-svn-id: https://trac-hacks.org/svn@17731 7322e99d-02ea-0310-aa39-e9a107903beb

--- fullblogplugin/1.4/tracfullblog/admin.py
+++ fullblogplugin/1.4/tracfullblog/admin.py
@@ -10,7 +10,7 @@ License: BSD
 from trac.core import *
 from trac.admin import IAdminPanelProvider
 from trac.resource import Resource
-from trac.web.chrome import add_warning, Chrome
+from trac.web.chrome import Chrome, add_warning
 
 from core import FullBlogCore
 
@@ -27,24 +27,20 @@ class FullBlogAdminPanel(Component):
             yield 'blog', 'Blog', 'settings', 'Settings'
 
     def render_admin_panel(self, req, cat, page, path_info):
-        req.perm(Resource('blog', None)).require('BLOG_ADMIN')
+        req.perm(Resource('blog')).require('BLOG_ADMIN')
 
         blog_admin = {}
         blog_core = FullBlogCore(self.env)
 
         if req.method == 'POST':
-            if req.args.get('savesettings'):
-                self.config.set('fullblog', 'num_items_front',
-                    req.args.getint('numpostsfront'))
-                self.config.set('fullblog', 'default_postname',
-                    req.args.get('defaultpostname'))
-                self.config.save()
-            elif req.args.get('savebloginfotext'):
-                blog_core.set_bloginfotext(req.args.get('bloginfotext'))
-                req.redirect(req.href.admin(req.args['cat_id'],
-                                            req.args['panel_id']))
-            else:
-                self.log.warning('Unknown POST request: %s', req.args)
+            self.config.set('fullblog', 'num_items_front',
+                req.args.getint('numpostsfront'))
+            self.config.set('fullblog', 'default_postname',
+                req.args.get('defaultpostname'))
+            self.config.save()
+            blog_core.set_bloginfotext(req.args.get('bloginfotext'))
+            req.redirect(req.href.admin(req.args['cat_id'],
+                                        req.args['panel_id']))
 
         blog_admin['bloginfotext'] = blog_core.get_bloginfotext()
         blog_admin['numpostsfront'] = \
@@ -52,4 +48,8 @@ class FullBlogAdminPanel(Component):
         blog_admin['defaultpostname'] = \
                 self.config.get('fullblog', 'default_postname')
 
-        return 'fullblog_admin.html', {'blog_admin': blog_admin}, None
+        chrome = Chrome(self.env)
+        chrome.add_auto_preview(req)
+        chrome.add_wiki_toolbars(req)
+
+        return 'fullblog_admin.html', {'blog_admin': blog_admin}
