d2316c90fe0e92d3ad3b27cef8f572fe886323d9
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2020-05-27 21:33:14 +0000

    Hack TracDefaultTheme deleted by rjollos
    git-svn-id: https://trac-hacks.org/svn@17763 7322e99d-02ea-0310-aa39-e9a107903beb

--- tracdefaulttheme/0.10/defaulttheme/theme.py
+++
@@ -1,9 +0,0 @@
-from trac.core import *
-from themeengine.api import ThemeBase
-
-__all__ = ['DefaultTheme']
-
-class DefaultTheme(ThemeBase):
-    """Trac default theme"""
-    
-    htdocs = screenshot = True
