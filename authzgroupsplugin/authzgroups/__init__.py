6f2ced25bd7097aeebd9d5f4df985f688c5ae98b
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2020-05-28 01:03:55 +0000

    Add code attached to wiki page
    git-svn-id: https://trac-hacks.org/svn@17766 7322e99d-02ea-0310-aa39-e9a107903beb

--- authzgroupsplugin/authzgroups/__init__.py
+++ authzgroupsplugin/authzgroups/__init__.py
@@ -1,3 +1,6 @@
+# Copyright David Abrahams 2007. Distributed under the Boost
+# Software License, Version 1.0. (See accompanying
+# file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 from trac.core import *
 from trac.perm import IPermissionGroupProvider
 from trac.versioncontrol.svn_authz import SubversionAuthorizer, \
