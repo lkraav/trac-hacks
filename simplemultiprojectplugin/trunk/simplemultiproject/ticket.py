3f1685e8f80a0dae6d9af360ce31c1445fa7c2f4
Cinc-th <Cinc-th@7322e99d-02ea-0310-aa39-e9a107903beb>
2020-08-01 04:35:19 +0000

    SimpleMultiProjectPlugin: new rewrite ticket handling to use new permission system and for licensing reasons.
     * project filtering for ticket page implemented. 
     * version filtering for ticket page implemented. No additional filtering according to project selection yet. 
    
    Refs #13850
    Refs #13849
    
    git-svn-id: https://trac-hacks.org/svn@17816 7322e99d-02ea-0310-aa39-e9a107903beb

--- simplemultiprojectplugin/trunk/simplemultiproject/ticket.py
+++ simplemultiprojectplugin/trunk/simplemultiproject/ticket.py
@@ -6,9 +6,9 @@
 #
 from collections import defaultdict
 from trac.core import Component as TracComponent, implements
-from trac.ticket.model import Component
+from trac.ticket.model import Component, Version
 from trac.web.api import IRequestFilter
-from simplemultiproject.smp_model import PERM_TEMPLATE, SmpComponent, SmpProject
+from simplemultiproject.smp_model import PERM_TEMPLATE, SmpComponent, SmpProject, SmpVersion
 
 
 class SmpTicket(TracComponent):
@@ -18,6 +18,7 @@ class SmpTicket(TracComponent):
     def __init__(self):
         self.smp_project = SmpProject(self.env)
         self.smp_component = SmpComponent(self.env)
+        self.smp_version = SmpVersion(self.env)
 
     # IRequestFilter methods
 
@@ -34,10 +35,29 @@ class SmpTicket(TracComponent):
             else:
                 data['fields'][comp_field]['options'] = self.get_components_for_user(req)
 
+            # Create a new list of available projects for the ticket
+            try:
+                field = data['fields_map']['project']
+            except KeyError:
+                pass  # May have been filtered but one of the ticket field filter plugins
+            else:
+                data['fields'][field]['options'] = self.get_projects_for_user(req)
+
+            # Create a new list of available versions for the ticket
+            try:
+                field = data['fields_map']['version']
+            except KeyError:
+                pass  # May have been filtered but one of the ticket field filter plugins
+            else:
+                data['fields'][field]['options'] = self.get_versions_for_user(req)
+
         return template, data, content_type
 
     def get_components_for_user(self, req):
-        """Check if user has access tothis milestone. Returns True if access is possible otherwise False-"""
+        """Get all components the user has access to.
+        :param req: Trac Request object
+        :returns list of component names
+        """
         # dict with key: milestone, val: list of project ids
         components = defaultdict(list)
         for comp in self.smp_component.get_all_components_and_project_id():
@@ -56,3 +76,39 @@ class SmpTicket(TracComponent):
                 comps.append(comp.name)
 
         return comps
+
+    def get_projects_for_user(self, req):
+        """Get all projects user has access to.
+        :param req: Trac Request object
+        :returns list of project names
+        """
+        projects = self.smp_project.get_all_projects()
+        usr_projects = []
+        for project in projects:
+            if (PERM_TEMPLATE % project.id) in req.perm:
+                usr_projects.append(project.name)
+        return usr_projects
+
+    def get_versions_for_user(self, req):
+        """Get all versions the user has access to.
+        :param req: Trac Request object
+        :returns list of version names
+        """
+        # dict with key: milestone, val: list of project ids
+        versions = defaultdict(list)
+        for ver in self.smp_version.get_all_versions_and_project_id():
+            versions[ver[0]].append(ver[1])  # ver[0]: name, ver[1]: project id
+
+        vers = []
+        all_vers = Version.select(self.env)
+        for ver in all_vers:
+            if ver.name in versions:
+                project_ids = versions[ver.name]
+                for project in project_ids:
+                    if (PERM_TEMPLATE % project) in req.perm:
+                        vers.append(ver.name)
+                del versions[ver.name]
+            else:
+                vers.append(ver.name)
+
+        return vers
