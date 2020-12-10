bb872c1bcc17ac53a219b995a99512e695564e04
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2020-12-10 22:05:06 +0000

    Remove unnecessary files and convert line endings
    git-svn-id: https://trac-hacks.org/svn@17932 7322e99d-02ea-0310-aa39-e9a107903beb

--- tracbasicmacro/0.11/tracbmacros/wiki.py
+++ tracbasicmacro/0.11/tracbmacros/wiki.py
@@ -35,42 +35,42 @@ from genshi.builder import tag
 
 class WikiHistoryMacro(WikiMacroBase):
     r"""Embed information about wiki changelog in wiki pages.
-    If invoked without keyword arguments then full 
-    changelog is shown inside a table with columns 
-    `Fecha`, `Version`, `Descripcion, `Autor`. Column labels 
-    may be customized using `chgtable_titles` option in `wiki` 
-    section. CSS class used to render the table may be specified 
-    through `chgtable_class` option in `wiki` section 
-    (see TracIni if you need help). Please read below for further 
+    If invoked without keyword arguments then full
+    changelog is shown inside a table with columns
+    `Fecha`, `Version`, `Descripcion, `Autor`. Column labels
+    may be customized using `chgtable_titles` option in `wiki`
+    section. CSS class used to render the table may be specified
+    through `chgtable_class` option in `wiki` section
+    (see TracIni if you need help). Please read below for further
     details.
-    
+
     Usage is as follows (all fields are optional).
-    
+
     `[[WikiHistory(pagename, version, attr=attr_name, cols=columns)]]`
-    
-    ''pagename'' : the name of target Wiki page. If missing or empty 
-                   and a wiki page is being rendered then it defaults to 
-                   the current wiki page. If the page being rendered 
+
+    ''pagename'' : the name of target Wiki page. If missing or empty
+                   and a wiki page is being rendered then it defaults to
+                   the current wiki page. If the page being rendered
                    doesn't belong in the Wiki then 'WikiStart' is used instead.
     ''version'' :  consider changes prior to (and including) this version
-    ''attr'' :     if this keyword argument is present then only the value 
-                   of the version attribute identified by `attr_name` is 
+    ''attr'' :     if this keyword argument is present then only the value
+                   of the version attribute identified by `attr_name` is
                    rendered in textual form (and `cols` argument is ignored)
-                   Supported values are `time`, `version`, 
+                   Supported values are `time`, `version`,
                    `comment`, and `author`
-    ''cols'' :     colon separated list of identifiers used to select 
-                   specific columns (order matters). Identifiers are 
+    ''cols'' :     colon separated list of identifiers used to select
+                   specific columns (order matters). Identifiers are
                    the same supported for `attr` parameter.
     """
-    
-    table_class = Option('wiki', 'chgtable_class', 'listing', 
+
+    table_class = Option('wiki', 'chgtable_class', 'listing',
                                 doc=_("""CSS class to render wiki history table"""))
-    labels = ListOption('wiki', 'chgtable_titles', u'Version, Date, Author, Comment', 
+    labels = ListOption('wiki', 'chgtable_titles', u'Version, Date, Author, Comment',
                                 keep_empty=True,
                                 doc=_("""Comma separated list of column labels for """
                                 """version, time, author, and comment (in that order)"""),
                                 )
-    
+
     def expand_macro(self, formatter, name, content):
         args, kw = parse_args(content)
         attr, cols = [kw.get(k) for k in ('attr', 'cols')]
@@ -90,15 +90,15 @@ class WikiHistoryMacro(WikiMacroBase):
         ATTR_LABELS = self.labels
         ATTR_LABELS = (len(ATTR_LABELS) == len(ALL_ATTRS)) and \
                         ATTR_LABELS or [_('Version'), _('Date'), _('Author'), _('Comment')]
-        
+
         def format_verinfo(value, attridx):
             return {
-                       1 : lambda x : tag.i(format_datetime(x, '%d/%m/%Y %H:%M:%S', tzinfo=localtz)), 
-                       0 : lambda x : tag.b(to_unicode(x, 'utf-8')), 
+                       1 : lambda x : tag.i(format_datetime(x, '%d/%m/%Y %H:%M:%S', tzinfo=localtz)),
+                       0 : lambda x : tag.b(to_unicode(x, 'utf-8')),
                        3 : lambda x : to_unicode(x or _('(No comments)'), 'utf-8'),
                        2 : lambda x : tag.i(to_unicode(x, 'utf-8')),
                    }.get(attridx, lambda x : tag.i(_('Atributo desconocido')) ) (value)
-        
+
         if attr is None :
             if cols :
                 cols = [ALL_ATTRS.index(x) for x in cols.split(':') if x in ALL_ATTRS ]
@@ -114,7 +114,7 @@ class WikiHistoryMacro(WikiMacroBase):
                             tag.tbody([tag.tr(
                                         [tag.td(format_verinfo(x[c], c))
                                                 for c in cols]
-                                    ) for x in page.get_history()]), 
+                                    ) for x in page.get_history()]),
                             class_=self.table_class, align='center')
         else :
             try :
