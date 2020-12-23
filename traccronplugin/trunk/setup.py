9ee9cc4e0bde60457d83b9df399698ecf3a571d3
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2020-12-23 23:45:57 +0000

    Drop latest code from PyPI
    git-svn-id: https://trac-hacks.org/svn@17943 7322e99d-02ea-0310-aa39-e9a107903beb

--- traccronplugin/trunk/setup.py
+++ traccronplugin/trunk/setup.py
@@ -1,38 +1,67 @@
 # -*- encoding: UTF-8 -*-
-
-'''
-Created on 12 oct. 2010
-
+"""
+Created on 12 Oct. 2010
 @author: thierry
-'''
-import ez_setup
-ez_setup.use_setuptools()
+Updated on 4 Feb. 2013
+@maintainer: t2y
+"""
 
-from setuptools import find_packages, setup
+try:
+    import setuptools
+except ImportError:
+    import distribute_setup
+    distribute_setup.use_setuptools()
+
+import pkg_resources
+from setuptools import setup
+
+REQUIRES = [
+    'Trac >= 0.10',
+]
+
+CLASSIFIERS = [
+    'Framework :: Trac',
+    'Development Status :: 4 - Beta',
+    'Environment :: Web Environment',
+    'License :: OSI Approved :: BSD License',
+    'Intended Audience :: Developers',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Software Development',
+]
+
+try:
+    LONG_DESCRIPTION = "".join([
+        open("README.txt").read(),
+    ])
+except:
+    LONG_DESCRIPTION = ""
 
 setup(
-    name = 'TracCronPlugin',
-    version = '0.3',
-    package_dir = {'': 'src'},
-    packages= find_packages('src'),
-    package_data = { 'traccron': [ 'templates/*.*'] },
-    author = "Thierry Bressure",
-    author_email = 'thierry@bressure.net',
-    maintainer = 'Tetsuya Morimoto',
-    maintainer_email = 'tetsuya.morimoto@gmail.com',
-    description = "Trac task scheduler  plugin for Trac.",
-    license = "BSD",
-    url = "http://trac-hacks.org/wiki/TracCronPlugin",
-    keywords = "trac cron scheduler plugin",    
-    classifiers = [
-        'Framework :: Trac',
-    ],
-    install_requires = [],
-    entry_points = {'trac.plugins': [
-                                     'traccron.core = traccron.core',
-                                     'traccron.task = traccron.task',
-                                     'traccron.scheduler = traccron.scheduler',
-                                     'traccron.listener = traccron.listener',
-                                     'traccron.history = traccron.history'
-                                     ]},
+    name='TracCronPlugin',
+    version='0.3.1',
+    description='Task scheduler plugin for Trac',
+    long_description=LONG_DESCRIPTION,
+    packages=['traccron'],
+    package_data={'traccron': ['templates/*.*']},
+    author='Thierry Bressure',
+    author_email='thierry@bressure.net',
+    maintainer='Tetsuya Morimoto',
+    maintainer_email='tetsuya.morimoto@gmail.com',
+    license='BSD',
+    platforms=['unix', 'linux', 'osx', 'windows'],
+    url='http://trac-hacks.org/wiki/TracCronPlugin',
+    keywords='trac cron scheduler plugin',
+    tests_require=['tox', 'pytest', 'pytest-pep8', 'pytest-capturelog'],
+    classifiers=CLASSIFIERS,
+    install_requires=REQUIRES,
+    entry_points={
+        'trac.plugins': [
+            'traccron.core = traccron.core',
+            'traccron.task = traccron.task',
+            'traccron.scheduler = traccron.scheduler',
+            'traccron.listener = traccron.listener',
+            'traccron.history = traccron.history',
+        ]
+    },
 )
