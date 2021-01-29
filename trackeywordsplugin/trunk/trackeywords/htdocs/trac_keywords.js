e30952e133d4a275b1e1b6ce5a1adbeb8a782146
clemens <clemens@7322e99d-02ea-0310-aa39-e9a107903beb>
2021-01-29 22:20:59 +0000

    One shall check the result validity when calling `getElementById()`!
    fixes #13949
    
    git-svn-id: https://trac-hacks.org/svn@17980 7322e99d-02ea-0310-aa39-e9a107903beb

--- trackeywordsplugin/trunk/trackeywords/htdocs/trac_keywords.js
+++ trackeywordsplugin/trunk/trackeywords/htdocs/trac_keywords.js
@@ -1,5 +1,7 @@
+// TracKeywordsPlugin 
 jQuery(function($) {
 
+  // the ID of the ticket "keywords" field
   field_id = document.getElementById('tags') ? "tags" : "field-keywords";
 
   // Remove or add a keyword tag to the field.
@@ -27,19 +29,29 @@ jQuery(function($) {
     el.value = newval.join(' ').replace(/^[;,\s]+|[;,\s]+$/, '');
     // trigger change event on the edit field in order to evoke ticket preview
     el.dispatchEvent(new Event('change'));
-
   }
 
   // Avoid duplicate insertion of keyword section.
-  if ($('#trac-keywords').length > 0) return;
+  if ($('#trac-keywords').length > 0) 
+    return;
+  
+  // prepare the keyword list which will be inserted into the DOM later.
   var $fieldset = $('<fieldset id="trac-keywords" />');
   $fieldset.append('<legend>Add Keywords</legend>')
   $ul = $('<ul></ul>');
   $fieldset.append($ul)
+
+
+  // get current  state of the "keywords" ticket field
+  var field = document.getElementById(field_id);
+  if (field == null)
+    return; 
+
   // Case-insensitive: compare to lower case.
-  var field = document.getElementById(field_id).value.toLowerCase();
+  var field_value = field.value.toLowerCase();
   // Regex splits the content of the field at separator characters.
-  var resource_keywords = field.split(/[;,\s]+/);
+  var resource_keywords = field_value.split(/[;,\s]+/);
+  
   // Iterate over each keyword defined in the Trac ini-file.
   $.each(trac_keywords, function(i, item) {
     $ul.append($('<li><a href="#">' + item[0] + '</a></li> ')
