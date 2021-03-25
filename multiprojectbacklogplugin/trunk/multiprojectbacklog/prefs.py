557837a1a09205cff4385a44e88e365bc15d9cce
Cinc-th <Cinc-th@7322e99d-02ea-0310-aa39-e9a107903beb>
2021-03-25 16:57:36 +0000

    MultiProjectBacklogPlugin: created Jinja2 template for backlog preference panel.
    Refs #13979
    
    git-svn-id: https://trac-hacks.org/svn@18096 7322e99d-02ea-0310-aa39-e9a107903beb

--- multiprojectbacklogplugin/trunk/multiprojectbacklog/prefs.py
+++ multiprojectbacklogplugin/trunk/multiprojectbacklog/prefs.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 #
 # Copyright (C) 2009, 2011, 2013 John Szakmeister
-# Copyright (C) 2016 Cinc
+# Copyright (C) 2016-2021 Cinc
 # All rights reserved.
 #
 # This software is licensed as described in the file COPYING, which
 # you should have received as part of this distribution.
 
+from pkg_resources import get_distribution, parse_version
 from trac.core import Component, implements
 from trac.prefs import IPreferencePanelProvider
 from trac.ticket.api import TicketSystem
@@ -16,6 +17,10 @@ from trac.web.chrome import add_notice, add_stylesheet
 
 class MultiProjectBacklogPrefPanel(Component):
     implements(IPreferencePanelProvider)
+
+    # Api changes regarding Genshi started after v1.2. This not only affects templates but also fragment
+    # creation using trac.util.html.tag and friends
+    pre_1_3 = parse_version(get_distribution("Trac").version) < parse_version('1.3')
 
     _ticket_fields = [
         (u'id', u'Id'), (u'summary', u'Summary'), (u'component', u'Component'),
@@ -30,17 +35,20 @@ class MultiProjectBacklogPrefPanel(Component):
 
     def render_preference_panel(self, req, panel):
         if req.method == 'POST':
-            fields = req.args.get('backlog_fields')
+            fields = req.args.getlist('backlog_fields')
             req.session['backlog_fields'] = fields
-            add_notice(req, _("Your backlog preferences have been saved."))
+            add_notice(req, _("Your preferences have been saved."))
             req.redirect(req.href.prefs(panel or None))
 
         custom_fields = [(cf['name'], cf['label']) for cf in
                          TicketSystem(self.env).get_custom_fields()]
         add_stylesheet(req, 'mpbacklog/css/backlog.css')
-        return 'prefs_backlog.html', {
+        data = {
             'fields': self._ticket_fields + custom_fields,
             'shown_fields':
-                req.session.get('backlog_fields') or [field[0] for field in
-                                                      self._ticket_fields]
-        }
+                req.session.get('backlog_fields') or [field[0] for field in self._ticket_fields]
+            }
+        if self.pre_1_3:
+            return 'prefs_backlog.html', data
+        else:
+            return 'mp_prefs_backlog.html', data
