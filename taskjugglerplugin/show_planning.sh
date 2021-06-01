3dc830a6b6c0f3ab81b5bea927060c57772be45b
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2021-06-01 03:31:24 +0000

    Hack TaskjugglerPlugin deleted by rjollos
    git-svn-id: https://trac-hacks.org/svn@18388 7322e99d-02ea-0310-aa39-e9a107903beb

--- taskjugglerplugin/show_planning.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-wget --no-check-certificate https://__yourLogin__:__yourPassword__@www.__yourHost__/.../trac2TaskJuggler.php -O /__path_of_this_file__/trac.tji
-TaskJugglerUI /__path_of_this_file__/trac.tjp
