0677db05eee92a71d839882aa3ca12a4851e91ab
stoecker <stoecker@7322e99d-02ea-0310-aa39-e9a107903beb>
2023-11-13 16:42:05 +0000

    CcSelectorPlugin: clone trunk to 1.4 branch
    git-svn-id: https://trac-hacks.org/svn@18593 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ ccselectorplugin/branches/1.4/cc_selector/cc_selector.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+
+import re
+import trac
+
+from pkg_resources import resource_exists, resource_filename
+
+from trac.config import BoolOption, Option
+from trac.core import *
+from trac.perm import PermissionSystem
+from trac.web.api import IRequestFilter, IRequestHandler
+from trac.web.chrome import Chrome, ITemplateProvider, add_script
+
+# Import i18n methods.  Fallback modules maintain compatibility to Trac 0.11
+# by keeping Babel optional here.
+try:
+    from trac.util.translation import domain_functions
+    add_domain, _ = \
+        domain_functions('cc_selector', ('add_domain', '_'))
+except ImportError:
+    from trac.util.translation import gettext
+    _ = gettext
+    def add_domain(a, b, c=None):
+        pass
+
+
+class TicketWebUiAddon(Component):
+    implements(IRequestFilter, ITemplateProvider, IRequestHandler)
+
+    show_fullname = BoolOption(
+        'cc_selector', 'show_fullname', False,
+        doc="Display full names instead of usernames if available.")
+
+    username_blacklist = Option(
+        'cc_selector', 'username_blacklist', '',
+        doc="Usernames separated by comma, that should never get listed.")
+
+    def __init__(self):
+        # bind the 'cc_selector' catalog to the specified locale directory
+        locale_dir = resource_filename(__name__, 'locale')
+        add_domain(self.env.path, locale_dir)
+
+    # IRequestFilter methods
+
+    def pre_process_request(self, req, handler):
+        return handler
+
+    def post_process_request(self, req, template, data, content_type):
+        if re.search('ticket', req.path_info):
+            add_script(req, 'cc_selector/cc_selector.js')
+            if req.locale is not None and \
+                    resource_exists('cc_selector',
+                                    'htdocs/lang_js/%s.js' % req.locale):
+                add_script(req, 'cc_selector/lang_js/%s.js' % req.locale)
+        return template, data, content_type
+
+    # ITemplateProvider methods
+
+    def get_htdocs_dirs(self):
+        return [('cc_selector', resource_filename(__name__, 'htdocs'))]
+
+    def get_templates_dirs(self):
+        return [resource_filename(__name__, 'templates')]
+
+    # IRequestHandler methods
+
+    def match_request(self, req):
+        match = re.match(r'^/cc_selector', req.path_info)
+        return False if match is None else True
+
+    def process_request(self, req):
+        add_script(req, 'cc_selector/cc_selector.js')
+
+        blacklist = self.username_blacklist.split(', ')
+        privileged_users = PermissionSystem(
+            self.env).get_users_with_permission('TICKET_VIEW')
+        all_users = self.env.get_known_users()
+
+        developers = filter(lambda u: u[0] in privileged_users, all_users)
+        cc_developers = list(filter(lambda u: not u[0] in blacklist, developers))
+        # TRANSLATOR: text added per list item in popup window like
+        #   [_] CC to username
+        cc_to = _("Cc to")
+        data = {
+            'cc_developers': cc_developers,
+            'cc_to': cc_to,
+            'show_fullname': self.show_fullname
+        }
+        if hasattr(Chrome, 'jenv'):
+            return 'cc_selector_jinja.html', data
+        else:
+            return 'cc_selector.html', data, None
---
+++ ccselectorplugin/branches/1.4/setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+extra = {}
+
+PACKAGE = 'cc_selector'
+
+try:
+    from trac.util.dist import get_l10n_js_cmdclass
+    cmdclass = get_l10n_js_cmdclass()
+    if cmdclass:
+        # includes some 'install_lib' overrides,
+        # i.e. 'compile_catalog' before 'bdist_egg'
+        extra['cmdclass'] = cmdclass
+        extractors = [
+            ('**.py', 'python', None),
+            ('**/templates/**.html', 'genshi', None),
+        ]
+        extra['message_extractors'] = {
+            PACKAGE: extractors,
+        }
+# i18n is implemented to be optional here
+except ImportError:
+    pass
+
+
+setup(
+    name='TracCcSelector',
+    version='0.2.1',
+    description='Visual Cc ticket field editor for Trac',
+    keywords='trac cc ticket editor',
+    url='https://trac-hacks.org/wiki/CcSelectorPlugin',
+    author='Vladislav Naumov',
+    author_email='vnaum@vnaum.com',
+    license='GPL',
+    maintainer='Steffen Hoffmann',
+    maintainer_email='hoff.st@web.de',
+    packages=[PACKAGE],
+    package_data={PACKAGE: [
+        'htdocs/*.js', 'htdocs/lang_js/*.js', 'locale/*/LC_MESSAGES/*.mo',
+        'locale/.placeholder', 'templates/*.html'
+        ]},
+    zip_safe=True,
+    entry_points={
+        'trac.plugins': [
+            'cc_selector.cc_selector=cc_selector.cc_selector'
+        ]},
+    install_requires=['Trac'],
+    extras_require={'Babel': 'Babel>=0.9.5'},
+    **extra
+)
