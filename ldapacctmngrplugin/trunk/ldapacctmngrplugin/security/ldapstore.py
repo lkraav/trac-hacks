61a023ca8dd3cb086010e3b21696a93c2a77f573
jun66j5 <jun66j5@7322e99d-02ea-0310-aa39-e9a107903beb>
2023-12-15 15:12:17 +0000

    Add option for user field and make compatible with Python 3 (closes #13487)
    git-svn-id: https://trac-hacks.org/svn@18603 7322e99d-02ea-0310-aa39-e9a107903beb

--- ldapacctmngrplugin/trunk/ldapacctmngrplugin/security/ldapstore.py
+++ ldapacctmngrplugin/trunk/ldapacctmngrplugin/security/ldapstore.py
@@ -1,67 +1,100 @@
-import ldap
-import ldap.filter
+# -*- coding: utf-8 -*-
 
-from trac.core import *
-from trac.config import *
+import ldap
+from ldap.filter import filter_format
+
+from trac.core import Component, implements
+from trac.config import BoolOption, Option
 from acct_mgr.api import IPasswordStore
 
-class LDAPStore (Component):
-    bind_server = Option('ldap', 'bind_server', 'ldap://localhost:389', doc='LDAP server for authentication. The format is "ldap[s]://host[:port]", e.g., "ldap://localhost:389".')
-    bind_anonymous = Option('ldap', 'bind_anonymous', 'yes', doc='If server accepts anonymous bind (yes or no)')
-    bind_dn = Option('ldap', 'bind_dn', '', doc='For server not accepting anonymous bind, specify a bind_dn and password.')
-    bind_passwd = Option('ldap', 'bind_passwd', '', doc='For server not accepting anonymous bind, specify a bind_dn and password.')
-    user_searchbase = Option('ldap', 'user_searchbase', 'dc=company,dc=com', doc='Where to look for users. It is usually "dc=your_company_name,dc=com". Please consult the structure of your LDAP tree.')
-    user_searchfilter = Option('ldap', 'user_searchfilter', 'objectClass=inetOrgPerson', doc='Filter for listing valid users. The default ("objectClass=inetOrgPerson") should work for most of the cases.')
-    user_matchfilter = Option('ldap', 'user_matchfilter', 'uid=%s', doc='The LDAP field for matching username when authenticating. The query is almost always "uid=%s".')
+
+class LDAPStore(Component):
 
     implements(IPasswordStore)
 
+    bind_server = Option(
+        'ldap', 'bind_server', 'ldap://localhost:389',
+        doc='LDAP server for authentication. The format is '
+            'ldap[s]://host[:port]", e.g., "ldap://localhost:389".')
+
+    bind_anonymous = BoolOption(
+        'ldap', 'bind_anonymous', 'enabled',
+        doc='If server accepts anonymous bind (yes or no)')
+
+    bind_dn = Option(
+        'ldap', 'bind_dn', '',
+        doc='For server not accepting anonymous bind, specify a bind_dn and '
+            'password.')
+
+    bind_passwd = Option(
+        'ldap', 'bind_passwd', '',
+        doc='For server not accepting anonymous bind, specify a bind_dn and '
+            'password.')
+
+    user_searchbase = Option(
+        'ldap', 'user_searchbase', 'dc=company,dc=com',
+        doc='Where to look for users. It is usually '
+            '"dc=your_company_name,dc=com". Please consult the structure of '
+            'your LDAP tree.')
+
+    user_searchfilter = Option(
+        'ldap', 'user_searchfilter', 'objectClass=inetOrgPerson',
+        doc='Filter for listing valid users. The default '
+            '("objectClass=inetOrgPerson") should work for most of the cases.')
+
+    user_field = Option(
+        'ldap', 'user_field', 'uid',
+        doc="""The LDAP field to match the username and authenticate.""")
+
+    # IPasswordStore methods
+
     def check_password(self, user, password):
+        if not user or not password:
+            return None
+
         # Authenticate a user by checking password
-        con = None
-
-        # nested "try:" for python2.4
+        con = self.init_connection()
         try:
+            resp = self._search_users(con, user)
+
+            # Added to prevent empty password authentication (some server allows that)
+            if not resp:
+                return None
+
             try:
-                con = self.init_connection()
-                resp = self._ldap_search_user(con, user, ['dn'])
-
-                # Added to prevent empty password authentication (some server allows that)
-                if not len(resp) :
-                    return None
-
-                resp = con.simple_bind_s(resp[0][0], password)
-                return True
+                dn, attrs = resp[0]
+                con.simple_bind_s(dn, password)
             except ldap.INVALID_CREDENTIALS:
-                self.log.debug('Invalid credentials: user %s not authenticated', user)
+                self.log.warning('Invalid credentials: user %s not authenticated', user)
                 return False
+            else:
+                return True
         finally:
-            if con != None:
-                con.unbind()
+            con.unbind()
 
     def get_users(self):
         # Get list of users from LDAP server
-        con = None
-        base = self.user_searchbase
-        filter = self.user_searchfilter
-        resp = None
-
+        user_field = self.user_field
+        con = self.init_connection()
         try:
-            con = self.init_connection()
-            resp = con.search_s(base, ldap.SCOPE_SUBTREE, filter, ['dn','uid'])
+            resp = self._search_users(con)
         finally:
-            if con != None:
-                con.unbind()
+            con.unbind()
 
-        self.log.debug('List users: get %d users' % (len(resp)))
-        for entry in resp:
-            if entry[1]['uid'][0]:
-                yield entry[1]['uid'][0]
+        for dn, attrs in resp:
+            if not dn:
+                continue
+            usernames = attrs.get(user_field)
+            if usernames:
+                username = usernames[0]
+                if isinstance(username, bytes):
+                    username = username.decode('utf-8')
+                yield username
 
     def has_user(self, user):
         con = self.init_connection()
         try:
-            resp = self._ldap_search_user(con, user, ['dn'])
+            resp = self._search_users(con, user)
             return len(resp) != 0
         finally:
             con.unbind()
@@ -69,18 +102,23 @@ class LDAPStore (Component):
     def init_connection(self):
         # Initialize LDAP connection
         connection = ldap.initialize(self.bind_server)
+        connection.set_option(ldap.OPT_REFERRALS, 0)
 
         if self.bind_server.startswith('ldaps'):
             # Not verifying the certificate for TLS
             ldap.set_option(ldap.OPT_X_TLS_REQUIRE_CERT, ldap.OPT_X_TLS_NEVER)
             #connection.start_tls_s()
 
-        if self.bind_anonymous.lower() != 'yes':
-            resp = connection.simple_bind_s(self.bind_dn, self.bind_passwd)
+        if not self.bind_anonymous:
+            connection.simple_bind_s(self.bind_dn, self.bind_passwd)
 
         return connection
 
-    def _ldap_search_user(self, con, user, attrs):
-        filter_ = ldap.filter.filter_format(self.user_matchfilter, [user])
+    def _search_users(self, con, user=None):
+        filters = [self.user_searchfilter]
+        if user:
+            filters.append(filter_format('{0}=%s'.format(self.user_field),
+                                         [user]))
+        filter_ = '(&%s)' % ''.join('(%s)' % f for f in filters)
         return con.search_s(self.user_searchbase, ldap.SCOPE_SUBTREE, filter_,
-                            attrs)
+                            ['dn', self.user_field])
--- ldapacctmngrplugin/trunk/ldapacctmngrplugin/setup.py
+++ ldapacctmngrplugin/trunk/ldapacctmngrplugin/setup.py
@@ -1,20 +1,23 @@
 #!/usr/bin/env python
-# -*- coding: iso-8859-1 -*-
-
-import sys
+# -*- coding: utf-8 -*-
 
 from setuptools import setup, find_packages
 
 setup(
     name = 'LDAPAcctMngrPlugin',
-    version = '0.9',
+    version = '0.10',
     packages = find_packages(),
 
     author = 'Adam Li',
     author_email = 'adamli528@gmail.com',
     description = 'A LDAP authentication plugin for AccountManager',
     license = 'Apache License 2.0',
-    install_requires = ['TracAccountManager'],
+    classifiers=[
+        'Framework :: Trac',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+    ],
+    install_requires = ['TracAccountManager', 'python-ldap'],
     entry_points = {
         'trac.plugins': [
             'security.ldapstore = security.ldapstore'
