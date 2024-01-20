e208d59bd93b51132b853720ed63eb0c0f7a3922
jun66j5 <jun66j5@7322e99d-02ea-0310-aa39-e9a107903beb>
2024-01-20 03:22:45 +0000

    TracTicketReferencePlugin: modify Python in the classifiers with 2.7 and 3, and enable `[bdist_wheel] universal` option (refs #14274)
    git-svn-id: https://trac-hacks.org/svn@18617 7322e99d-02ea-0310-aa39-e9a107903beb

--- tracticketreferenceplugin/1.0/setup.py
+++ tracticketreferenceplugin/1.0/setup.py
@@ -31,7 +31,8 @@ CLASSIFIERS = [
     "License :: OSI Approved :: Apache Software License",
     "Intended Audience :: Developers",
     "Operating System :: OS Independent",
-    "Programming Language :: Python",
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
     "Topic :: Software Development",
 ]
 
