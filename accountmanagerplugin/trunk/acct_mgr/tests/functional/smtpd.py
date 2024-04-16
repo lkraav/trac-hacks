fc4a50553e882da086e9fe513b08674b25c61171
jun66j5 <jun66j5@7322e99d-02ea-0310-aa39-e9a107903beb>
2024-04-16 03:16:25 +0000

    TracAccountManager 0.6dev: allow to run tests with Python 3.12 using aiosmtpd
    git-svn-id: https://trac-hacks.org/svn@18632 7322e99d-02ea-0310-aa39-e9a107903beb

--- accountmanagerplugin/trunk/acct_mgr/tests/functional/smtpd.py
+++ accountmanagerplugin/trunk/acct_mgr/tests/functional/smtpd.py
@@ -10,9 +10,17 @@
 
 from __future__ import absolute_import
 
-import asyncore
-import smtpd
 import threading
+
+try:
+    import aiosmtpd
+except ImportError:
+    import asyncore, smtpd
+    aiosmtpd = None
+else:
+    import asyncio
+    from aiosmtpd.controller import UnthreadedController
+    from aiosmtpd.handlers import Message
 
 
 class NonForgetingSMTPServerStore(object):
@@ -40,7 +48,7 @@ class NonForgetingSMTPServerStore(object):
     def message(self):
         return self.last_message.get('message')
 
-    def process_message(self, peer, mailfrom, rcpttos, data, **kwargs):
+    def process_message(self, mailfrom, rcpttos, data, **kwargs):
         message = {'recipients': rcpttos, 'sender': mailfrom, 'message': data}
         self.messages.update((recipient, message) for recipient in rcpttos)
         self.last_message = message
@@ -50,43 +58,16 @@ class NonForgetingSMTPServerStore(object):
         self.last_message.clear()
 
 
-class SMTPServer(smtpd.SMTPServer):
-
-    store = None
-
-    def __init__(self, localaddr, store):
-        smtpd.SMTPServer.__init__(self, localaddr, None)
-        self.store = store
-
-    def process_message(self, peer, mailfrom, rcpttos, data, **kwargs):
-        self.store.process_message(peer, mailfrom, rcpttos, data, **kwargs)
-
-
-class SMTPThreadedServer(threading.Thread):
+class SMTPThreadedServerMethods(object):
     """
     Run a SMTP server for a single connection, within a dedicated thread
     """
     host = 'localhost'
 
-    def __init__(self, port):
-        self.port = port
-        self.store = NonForgetingSMTPServerStore()
-        super(SMTPThreadedServer, self).__init__(target=asyncore.loop,
-                                                 args=(0.1, True))
-        self.daemon = True
-
-    def start(self):
-        self.server = SMTPServer((self.host, self.port), self.store)
-        super(SMTPThreadedServer, self).start()
-
-    def stop(self):
-        self.server.close()
-        self.join()
-
     def get_sender(self, recipient=None):
-        """Return the sender of a message. If recipient is passed, return
-        the sender for the message sent to that recipient, else, send
-        the sender for last message"""
+        """Return the sender of a message. If recipient is passed, return the
+        sender for the message sent to that recipient, else, send the sender
+        for last message"""
         try:
             return self.store.messages[recipient]['sender']
         except KeyError:
@@ -102,18 +83,18 @@ class SMTPThreadedServer(threading.Thread):
             return self.store.recipients
 
     def get_message(self, recipient=None):
-        """Return the message of a message. If recipient is passed, return
-        the actual message for the message sent to that recipient, else, send
-        the last message"""
+        """Return the message of a message. If recipient is passed, return the
+        actual message for the message sent to that recipient, else, send the
+        last message"""
         try:
             return self.store.messages[recipient]['message']
         except KeyError:
             return self.store.message
 
     def get_message_parts(self, recipient):
-        """Return the message parts(dict). If recipient is passed, return
-        the parts for the message sent to that recipient, else, send the parts
-        for last message"""
+        """Return the message parts(dict). If recipient is passed, return the
+        parts for the message sent to that recipient, else, send the parts for
+        last message"""
         try:
             return self.store.messages[recipient]
         except KeyError:
@@ -121,3 +102,84 @@ class SMTPThreadedServer(threading.Thread):
 
     def full_reset(self):
         self.store.full_reset()
+
+
+if aiosmtpd:
+    class Handler(Message):
+
+        store = None
+
+        def __init__(self, store):
+            self.store = store
+
+        def handle_message(self, message):
+            pass
+
+        def prepare_message(self, session, envelope):
+            content = envelope.content
+            if content.endswith(b'\r\n'):
+                content = content[:-2]
+            data = content.decode('utf-8')
+            mailfrom = envelope.mail_from
+            recipients = envelope.rcpt_tos
+            self.store.process_message(mailfrom, recipients, data)
+            return None
+
+    class Controller(UnthreadedController):
+
+        def _create_server(self):
+            return self.loop.create_server(
+                self._factory_invoker, host=self.hostname, port=self.port,
+                ssl=self.ssl_context, reuse_address=True)
+
+    class SMTPThreadedServer(threading.Thread, SMTPThreadedServerMethods):
+
+        def __init__(self, port):
+            self.port = port
+            self.store = NonForgetingSMTPServerStore()
+            loop = asyncio.new_event_loop()
+            handler = Handler(self.store)
+            controller = Controller(handler, loop=loop, hostname=self.host,
+                                    port=port)
+            self.loop = loop
+            self.controller = controller
+            controller.begin()
+            super().__init__(target=loop.run_forever)
+            self.daemon = True
+
+        def stop(self):
+            if self.loop.is_running():
+                self.loop.call_soon_threadsafe(self.loop.stop)
+            while self.is_alive():
+                self.join(0.1)
+            self.controller.end()
+            self.loop.close()
+
+else:
+    class SMTPServer(smtpd.SMTPServer):
+
+        store = None
+
+        def __init__(self, localaddr, store):
+            smtpd.SMTPServer.__init__(self, localaddr, None)
+            self.store = store
+
+        def process_message(self, peer, mailfrom, rcpttos, data, **kwargs):
+            self.store.process_message(mailfrom, rcpttos, data)
+
+    class SMTPThreadedServer(threading.Thread, SMTPThreadedServerMethods):
+
+        def __init__(self, port):
+            self.port = port
+            self.store = NonForgetingSMTPServerStore()
+            super(SMTPThreadedServer, self).__init__(target=asyncore.loop,
+                                                     args=(0.1, True))
+            self.daemon = True
+
+        def start(self):
+            self.server = SMTPServer((self.host, self.port), self.store)
+            super(SMTPThreadedServer, self).start()
+
+        def stop(self):
+            self.server.close()
+            self.join()
